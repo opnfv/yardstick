2d131cd335b35914133d3dd8d1d8f5741fc38eb0
Hans Feldt <hans.feldt@ericsson.com>
2015-05-12 18:25:42 +0200

    add README and scripts for build and test
    README.rst is work in progress and probably not proper rst format
    yet but should still contain valuable information.
    
    run_tests.sh is a script that runs unit and style tests on the
    code. It can be used as a gate check in gerrit. Similar scripts
    are standard practice in other open source projects.
    
    JIRA: -
    Change-Id: I5e586b346ff45f1151960a0e7fda2fe6072422c0
    Signed-off-by: Hans Feldt <hans.feldt@ericsson.com>

---
+++ run_tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# Run yardstick's test suite(s)
+
+run_flake8() {
+    echo -n "Running flake8 ... "
+    logfile=pep8.log
+    flake8 yardstick > $logfile
+    if [ $? -ne 0 ]; then
+        echo "FAILED, result in $logfile"
+    else
+        echo "OK, result in $logfile"
+    fi
+}
+
+run_tests() {
+    echo -n "Running unittest ... "
+    logfile=test.log
+    python -m unittest discover -s yardstick/tests &> $logfile
+    if [ $? -ne 0 ]; then
+        echo "FAILED, result in $logfile"
+    else
+        echo "OK, result in $logfile"
+    fi
+}
+
+run_flake8
+run_tests
+
