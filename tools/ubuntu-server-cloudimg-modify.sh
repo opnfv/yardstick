364e88d962e45d6e923c8c1664692553e2b5e7a4
Hans Feldt <hans.feldt@ericsson.com>
2015-06-11 14:33:10 +0200

    Add support for building images
    Two scripts are added. One that will be installed in user's PATH
    and one that is an example of how to modify an image from within.
    
    See README for example and script for more info
    
    Change-Id: Iab743f6e9105d5ba872ffba0512ffee954c6d830
    JIRA: YARDSTICK-28
    Signed-off-by: Hans Feldt <hans.feldt@ericsson.com>

---
+++ tools/ubuntu-server-cloudimg-modify.sh
@@ -0,0 +1,36 @@
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+#!/bin/bash
+
+# installs required packages
+# must be run from inside the image (either chrooted or running)
+
+set -ex
+
+if [ $# -eq 1 ]; then
+    nameserver_ip=$1
+
+    # /etc/resolv.conf is a symbolic link to /run, restore at end
+    rm /etc/resolv.conf
+    echo "nameserver $nameserver_ip" > /etc/resolv.conf
+fi
+
+# iperf3 only available for trusty in backports
+grep trusty /etc/apt/sources.list && \
+    echo "deb http://archive.ubuntu.com/ubuntu/ trusty-backports main restricted universe multiverse" >> /etc/apt/sources.list
+apt-get update
+apt-get install -y \
+    iperf3 \
+    lmbench \
+    stress
+
+# restore symlink
+ln -sf /run/resolvconf/resolv.conf /etc/resolv.conf
+
