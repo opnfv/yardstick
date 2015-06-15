17e94ff1e86c2a98895910d63828081ad5867052
Jo¶rgen Karlsson <jorgen.w.karlsson@ericsson.com>
2015-06-15 16:15:24 +0200

    add unit test for ping
    Running of unittest from run_test.sh is NOT enabled.
    
    JIRA:-
    Change-Id: I741d63e8ab37570c3cf67353fafc7f5210382219
    Signed-off-by: Jo¶rgen Karlsson <jorgen.w.karlsson@ericsson.com>

--- run_tests.sh
+++ run_tests.sh
@@ -25,8 +25,7 @@ run_flake8() {
 
 run_tests() {
     echo -n "Running unittest ... "
-    logfile=test.log
-    python -m unittest discover -s yardstick/tests &> $logfile
+    python -m unittest discover -s tests/unit
     if [ $? -ne 0 ]; then
         echo "FAILED, result in $logfile"
         exit 1
