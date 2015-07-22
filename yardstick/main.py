fd18ee7adc41de5a23f6b9903324f7d5cf40e941
Kristian Hunt <kristian.hunt@gmail.com>
2015-07-22 10:30:49 +0200

    Update example invocation of yardstick in main.py
    Example invocation in main.py is in accordance with change 40f49a4
    
    JIRA:-
    
    Change-Id: Id3b07e83db768c6d43f6a5bddc954448c9f1e6c4
    Signed-off-by: Kristian Hunt <kristian.hunt@gmail.com>

--- yardstick/main.py
+++ yardstick/main.py
@@ -12,15 +12,15 @@
 """ yardstick - command line tool for managing benchmarks
 
     Example invocation:
-    $ yardstick samples/ping-task.yaml
+    $ yardstick task start samples/ping.yaml
 
-    Servers are the same as VMs (Nova call them servers in the API)
+    Servers are the same as VMs (Nova calls them servers in the API)
 
     Many tests use a client/server architecture. A test client is configured
     to use a specific test server e.g. using an IP address. This is true for
     example iperf. In some cases the test server is included in the kernel
     (ping, pktgen) and no additional software is needed on the server. In other
-    cases (iperf) a server process needs to be installed and started
+    cases (iperf) a server process needs to be installed and started.
 
     One server is required to host the test client program (such as ping or
     iperf). In the task file this server is called host.
