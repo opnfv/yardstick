6631caf35e08f82a2267f4ea7fe8c157e3f5ad27
QiLiang <liangqi1@huawei.com>
2015-07-25 08:37:45 +0000

    Update pbr requirement version in setup.py
    JIRA: YARDSTICK-45
    
    Change-Id: Ic7e918b6d287725e2451da93aa8a36ec2b09ca0a
    Signed-off-by: QiLiang <liangqi1@huawei.com>

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     install_requires=["backport_ipaddress",  # remove with python3
                       "flake8",
                       "PyYAML>=3.10",
-                      "pbr!=0.7,<1.0,>=0.6",
+                      "pbr<2.0,>=1.3",
                       "python-glanceclient>=0.12.0",
                       "python-heatclient>=0.2.12",
                       "python-keystoneclient>=0.11.1",
