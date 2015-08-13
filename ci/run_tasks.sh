35af15283186e80b9d55f1529d2b9ea88302a353
Kristian Hunt <kristian.hunt@gmail.com>
2015-08-13 14:48:30 +0200

    Add script for running yardstick tasks back-to-back
    This script enables to run multiple yardstick tasks in the yardstick
    daily build without having to change the Jenkins job in the releng
    repository.
    
    This task should be executed from the main yardstick directory just
    as run_test.sh.
    
    JIRA: YARDSTICK-106
    
    Change-Id: I8a6bb7e6c03ec551709ff66a45aad7257fb36e92
    Signed-off-by: Kristian Hunt <kristian.hunt@gmail.com>

---
+++ ci/run_tasks.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# Run yardstick tasks back-to-back
+# This script is called from yardstick-{pod} job and decides which tasks
+# are executed as part of that job.
+
+
+# verify that virtual environment is activated
+# assumes the virtual environment has been created as described in README.rst
+if [[ ! $(which python | grep venv) ]]; then
+    echo "Unable to activate venv...Exiting"
+    exit 1
+fi
+
+# Define tasks to be run
+TASK_FILE_NAMES[0]='samples/ping.yaml'
+TASK_FILE_NAMES[1]='samples/iperf3.yaml'
+TASK_FILE_NAMES[2]='samples/pktgen.yaml'
+TASK_FILE_NAMES[3]='samples/fio.yaml'
+
+# Execute tasks
+for TASK_FILE in ${TASK_FILE_NAMES[@]}
+do
+    echo "Executing task from file: $TASK_FILE"
+    yardstick -d task start $TASK_FILE
+done
