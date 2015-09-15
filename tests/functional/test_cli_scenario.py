531ac9968cf8703ad7a2c70f45837ce0e81dbec9
kubi <jean.gaoliang@huawei.com>
2015-09-15 03:37:14 -0400

    Add functional tests in verify and merge
    As Ana said ,"The first functional test should be as simple as a "Hello world",
     it shall be possible to run the "Hello world" test without using OpenStack."
    so i just finish functional test framework and do functional test for subcommand
    "runner"and"scenario" without using Openstack.
    
    JIRA:YARDSTICK-103
    
    Change-Id: I673ae61f9922536a685d32ae62e5ad5165472f9d
    Signed-off-by: kubi <jean.gaoliang@huawei.com>

---
+++ tests/functional/test_cli_scenario.py
@@ -0,0 +1,61 @@
+##############################################################################
+# Copyright (c) 2015 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+
+import unittest
+
+from tests.functional import utils
+
+
+class ScenarioTestCase(unittest.TestCase):
+
+    def setUp(self):
+        super(ScenarioTestCase, self).setUp()
+        self.yardstick = utils.Yardstick()
+
+    def test_scenario_list(self):
+        res = self.yardstick("scenario list")
+
+        self.assertIn("Lmbench", res)
+        self.assertIn("Perf", res)
+        self.assertIn("Fio", res)
+        self.assertIn("Ping", res)
+        self.assertIn("Iperf3", res)
+        self.assertIn("Pktgen", res)
+
+    def test_scenario_show_Lmbench(self):
+        res = self.yardstick("scenario show Lmbench")
+        lmbench = "Execute lmbench memory read latency benchmark in a host" in res
+        self.assertTrue(lmbench)
+
+    def test_scenario_show_Perf(self):
+        res = self.yardstick("scenario show Perf")
+        perf = "Execute perf benchmark in a host" in res
+        self.assertTrue(perf)
+
+    def test_scenario_show_Fio(self):
+        res = self.yardstick("scenario show Fio")
+        fio = "Execute fio benchmark in a host" in res
+        self.assertTrue(fio)
+
+    def test_scenario_show_Ping(self):
+        res = self.yardstick("scenario show Ping")
+        ping = "Execute ping between two hosts" in res
+        self.assertTrue(ping)
+
+    def test_scenario_show_Iperf3(self):
+        res = self.yardstick("scenario show Iperf3")
+        iperf3 = "Execute iperf3 between two hosts" in res
+        self.assertTrue(iperf3)
+
+    def test_scenario_show_Pktgen(self):
+        res = self.yardstick("scenario show Pktgen")
+        pktgen = "Execute pktgen between two hosts" in res
+        self.assertTrue(pktgen)
+
