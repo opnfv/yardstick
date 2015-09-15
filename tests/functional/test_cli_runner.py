4cb31c9468f746c10c7135fbbd7f4f7ddeff56ce
kubi <jean.gaoliang@huawei.com>
2015-09-15 03:37:14 -0400

    Add functional tests in verify and merge
    As Ana said ,"The first functional test should be as simple as a "Hello world",
     it shall be possible to run the "Hello world" test without using OpenStack."
    so i just finish functional test framework and do functional test for subcommand
    "runner"and"scenario" without using Openstack.
    
    JIRA:YARDSTICK-103
    
    Change-Id: I673ae61f9922536a685d32ae62e5ad5165472f9d
    Signed-off-by: kubi <jean.gaoliang@huawei.com>

---
+++ tests/functional/test_cli_runner.py
@@ -0,0 +1,49 @@
+##############################################################################
+# Copyright (c) 2015 Huawei Technologies Co.,Ltd.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+
+import unittest
+
+from tests.functional import utils
+
+
+class RunnerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        super(RunnerTestCase, self).setUp()
+        self.yardstick = utils.Yardstick()
+
+    def test_runner_list(self):
+        res = self.yardstick("runner list")
+
+        self.assertIn("Duration", res)
+        self.assertIn("Arithmetic", res)
+        self.assertIn("Iteration", res)
+        self.assertIn("Sequence", res)
+
+    def test_runner_show_Duration(self):
+        res = self.yardstick("runner show Duration")
+        duration = "duration - amount of time" in res
+        self.assertTrue(duration)
+
+    def test_runner_show_Arithmetic(self):
+        res = self.yardstick("runner show Arithmetic")
+        arithmetic = "Run a scenario arithmetically" in res
+        self.assertTrue(arithmetic)
+
+    def test_runner_show_Iteration(self):
+        res = self.yardstick("runner show Iteration")
+        iteration = "iterations - amount of times" in res
+        self.assertTrue(iteration)
+
+    def test_runner_show_Sequence(self):
+        res = self.yardstick("runner show Sequence")
+        sequence = "sequence - list of values which are executed" in res
+        self.assertTrue(sequence)
+
