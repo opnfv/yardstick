4ce03c6cdc85c6b09c25c588650a609a4cd7a1c6
QiLiang <liangqi1@huawei.com>
2015-10-15 14:24:27 +0800

    Heat context code refactor
    Heat context code refactor to cater for the evolution of the
    Yardstick framework.
    
    This is the initial version, very rough with many TODOs in the code,
    but show the idea of how the new yardstick context will look like.
    
    In test_case.yaml context segment will add "context_type" to indicate
    the context type, default is Heat.
    
    Welcome your review and comments.
    
    JIRA: YARDSTICK-168
    
    Change-Id: Ida0ce12c17cd9b88d7acfb4c9eb1ac6986394b38
    Signed-off-by: QiLiang <liangqi1@huawei.com>

---
+++ tests/unit/benchmark/contexts/test_model.py
@@ -0,0 +1,1 @@
+# TODO refactor test cases later
---
+++ yardstick/benchmark/contexts/base.py
@@ -0,0 +1,70 @@
+##############################################################################
+# Copyright (c) 2015 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+import abc
+import six
+
+import yardstick.common.utils as utils
+
+
+@six.add_metaclass(abc.ABCMeta)
+class Context(object):
+    '''Class that represents a context in the logical model'''
+    list = []
+
+    def __init__(self):
+        Context.list.append(self)
+
+    @abc.abstractmethod
+    def init(self, attrs):
+        "TODO add comments"
+
+    @staticmethod
+    def get_cls(context_type):
+        '''Return class of specified type.'''
+        for context in utils.itersubclasses(Context):
+            if context_type == context.__context_type__:
+                return context
+        raise RuntimeError("No such context_type %s" % context_type)
+
+    @staticmethod
+    def get(context_type):
+        """Returns instance of a context for context type.
+        """
+        return Context.get_cls(context_type)()
+
+    @abc.abstractmethod
+    def deploy(self):
+        '''TODO add comments'''
+
+    @abc.abstractmethod
+    def undeploy(self):
+        '''TODO add comments'''
+
+    @abc.abstractmethod
+    def _get_server(self, attr_name):
+        '''get server object by name from context
+        '''
+
+    @staticmethod
+    def get_server(attr_name):
+        '''lookup server object by name from context
+        attr_name: either a name for a server created by yardstick or a dict
+        with attribute name mapping when using external heat templates
+        '''
+        server = None
+        for context in Context.list:
+            server = context._get_server(attr_name)
+            if server is not None:
+                break
+
+        if server is None:
+            raise ValueError("context not found for server '%s'" %
+                             attr_name["name"])
+
+        return server
