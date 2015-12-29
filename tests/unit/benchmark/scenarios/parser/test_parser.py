7f78826fd53ad303f62254de386aa7e3bf0c0fd6
kubi <jean.gaoliang@huawei.com>
2015-12-29 11:04:20 +0800

    running Parser Yang-to-Tosca module as a tool
    this is part2 of patch, it need to wait a new docker images to uplooad
    which with libxml2-dev libxslt1-dev,so FT will fail, it will be ok when
    we have a new image
    validating output against expected outcome
    
    Change-Id: If50d241a5338888f14fd11a752dc72678e0c569b
    JIRA:YARDSTICK-224
    Signed-off-by: kubi <jean.gaoliang@huawei.com>

---
+++ tests/unit/benchmark/scenarios/parser/test_parser.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+##############################################################################
+# Copyright (c) 2015 Huawei Technologies Co.,Ltd and other.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# Unittest for yardstick.benchmark.scenarios.parser.Parser
+
+import mock
+import unittest
+import json
+
+from yardstick.benchmark.scenarios.parser import parser
+
+@mock.patch('yardstick.benchmark.scenarios.parser.parser.subprocess')
+class ParserTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_parser_successful_setup(self, mock_subprocess):
+
+        p = parser.Parser({}, {})
+        mock_subprocess.call().return_value = 0
+        p.setup()
+        self.assertEqual(p.setup_done, True)
+
+    def test_parser_successful(self, mock_subprocess):
+        args = {
+            'options': {'yangfile':'/root/yardstick/samples/yang.yaml',
+            'toscafile':'/root/yardstick/samples/tosca.yaml'},
+        }
+        p = parser.Parser(args, {})
+        result = {}
+        mock_subprocess.call().return_value = 0
+        sample_output = '{"yangtotosca": "success"}'
+
+        p.run(result)
+        expected_result = json.loads(sample_output)
+
+    def test_parser_teardown_successful(self, mock_subprocess):
+
+        p = parser.Parser({}, {})
+        mock_subprocess.call().return_value = 0
+        p.teardown()
+        self.assertEqual(p.teardown_done, True)
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
---
+++ yardstick/benchmark/scenarios/parser/parser.py
@@ -0,0 +1,80 @@
+##############################################################################
+# Copyright (c) 2015 Huawei Technologies Co.,Ltd and other.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+import pkg_resources
+import logging
+import subprocess
+from yardstick.benchmark.scenarios import base
+
+LOG = logging.getLogger(__name__)
+
+
+class Parser(base.Scenario):
+    """running Parser Yang-to-Tosca module as a tool
+    validating output against expected outcome
+
+    more info https://wiki.opnfv.org/parser
+    """
+    __scenario_type__ = "Parser"
+
+    SETUP_SCRIPT = "parser_setup.sh"
+    TEARDOWN_SCRIPT = "parser_teardown.sh"
+    PARSER_SCRIPT = "parser.sh"
+
+    def __init__(self, scenario_cfg, context_cfg):
+        self.scenario_cfg = scenario_cfg
+        self.context_cfg = context_cfg
+        self.setup_done = False
+
+    def setup(self):
+        """scenario setup"""
+        self.setup_script = pkg_resources.resource_filename(
+            "yardstick.benchmark.scenarios.parser",
+            Parser.SETUP_SCRIPT)
+        cmd = "%s" % (self.setup_script)
+
+        subprocess.call(cmd, shell=True)
+
+        self.setup_done = True
+
+    def run(self, result):
+        """execute the translation"""
+        options = self.scenario_cfg['options']
+        yangfile = options.get("yangfile", '~/yardstick/samples/yang.yaml')
+        toscafile = options.get("toscafile", '~/yardstick/samples/tosca.yaml')
+
+        self.parser_script = pkg_resources.resource_filename(
+            "yardstick.benchmark.scenarios.parser",
+            Parser.PARSER_SCRIPT)
+
+        if not self.setup_done:
+            self.setup()
+
+        cmd1 = "%s %s %s" % (self.parser_script, yangfile, toscafile)
+        cmd2 = "chmod 777 %s" % (self.parser_script)
+        subprocess.call(cmd2, shell=True)
+        output = subprocess.call(cmd1, shell=True, stdout=subprocess.PIPE)
+        print "yangtotosca finished"
+
+        result['yangtotosca'] = "success" if output == 0 else "fail"
+
+    def teardown(self):
+        ''' for scenario teardown remove parser and pyang '''
+        self.teardown_script = pkg_resources.resource_filename(
+            "yardstick.benchmark.scenarios.parser",
+            Parser.TEARDOWN_SCRIPT)
+        subprocess.call(self.teardown_script, shell=True)
+        self.teardown_done = True
+
+
+def _test():
+    '''internal test function'''
+    pass
+
+if __name__ == '__main__':
+    _test()
