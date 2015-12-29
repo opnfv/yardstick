242a7e954999c40182cf329e82c6f93b26af20f0
kubi <jean.gaoliang@huawei.com>
2015-12-29 11:04:20 +0800

    running Parser Yang-to-Tosca module as a tool
    validating output against expected outcome
    
    Change-Id: If50d241a5338888f14fd11a752dc72678e0c569b
    JIRA:YARDSTICK-224
    Signed-off-by: kubi <jean.gaoliang@huawei.com>

---
+++ yardstick/benchmark/scenarios/parser/parser_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright (c) 2015 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+apt-get update
+apt-get install -y \
+    python-lxml\
+
+git clone https://github.com/mbj4668/pyang.git /opt/temp/pyang
+cd /opt/temp/pyang
+python setup.py install
+git clone https://gerrit.opnfv.org/gerrit/parser /opt/temp/parser
