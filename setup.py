975ba018f8fb5c5be3882d49c68def07a4da6e23
Vincenzo Riccobene <vincenzox.m.riccobene@intel.com>
2016-01-11 12:20:56 +0000

    Implementation of yardstick-tc021 in Yardstick
    Includes
     - Yardstick Scenario for integration with ApexLake
     - Yardstick Task .yaml file
     - Documentation
    
    JIRA: YARDSTICK-219
    
    Change-Id: I7b7f7967410aa0ea558b0632fbb27f583dd6e86e
    Signed-off-by: Vincenzo Riccobene <vincenzox.m.riccobene@intel.com>

--- setup.py
+++ setup.py
@@ -39,7 +39,8 @@ setup(
                       "scp",
                       "six",
                       "testrepository>=0.0.18",
-                      "testtools>=1.4.0"
+                      "testtools>=1.4.0",
+                      "nose"
                       ],
     extras_require={
         'plot': ["matplotlib>=1.4.2"]
