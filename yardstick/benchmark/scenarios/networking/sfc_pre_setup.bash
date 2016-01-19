f2e8eb245f01f3bc4dd7ef71168a872ce7f49ad8
Ricardo Noriega <ricardo.noriega.de.soto@ericsson.com>
2016-01-19 17:41:22 +0100

    Service Function Chaining test cases
    NOTE: Working on progress, please do not merge.
    
    Change-Id: Id4c8b1bc05e768bdb2b8f17798f13f4d83dcd9d6
    Signed-off-by: Ricardo Noriega <ricardo.noriega.de.soto@ericsson.com>

---
+++ yardstick/benchmark/scenarios/networking/sfc_pre_setup.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# download and create image
+source /opt/admin-openrc.sh 
+wget https://www.dropbox.com/s/focu44sh52li7fz/sfc_cloud.qcow2
+glance image-create --name sfc --disk-format qcow2 --container-format bare --file sfc_cloud.qcow2
+
+#create flavor
+nova flavor-create --is-public true sfc_custom 666 1000 5 2
