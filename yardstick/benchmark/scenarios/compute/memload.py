a0142dad89c0eb6d9700a5846048722c674a03f7
JingLu5 <lvjing5@huawei.com>
2016-05-06 09:58:41 +0800

    add memory_load scenario
    JIRA: YARDSTICK-259
    
    This scenario reads memory usage statistics on a Linux host.
    memory usage statistics are read using the utility 'free'.
    
    Signed-off-by: JingLu5 <lvjing5@huawei.com>
    Change-Id: I677aa65ca264dc77a963bf8b6913a729fbf031be

---
+++ yardstick/benchmark/scenarios/compute/memload.py
@@ -0,0 +1,122 @@
+##############################################################################
+# Copyright (c) 2015 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+"""Memory load and statistics."""
+
+import logging
+import yardstick.ssh as ssh
+
+from yardstick.benchmark.scenarios import base
+
+LOG = logging.getLogger(__name__)
+
+
+class MEMLoad(base.Scenario):
+    """Collect memory statistics and system load.
+
+    This scenario reads memory usage statistics on a Linux host.
+
+    memory usage statistics are read using the utility 'free'.
+
+    Parameters
+        interval - Time interval to measure memory usage.
+        Continuously display the result delay interval seconds apart.
+        type:       [int]
+        unit:       seconds
+        default:    1
+
+        count - specifies a # of measurments for each test
+        type:       [int]
+        unit:       N/A
+        default:    1
+    """
+    __scenario_type__ = "MEMORYload"
+
+    def __init__(self, scenario_cfg, context_cfg):
+        """Scenario construction."""
+        self.scenario_cfg = scenario_cfg
+        self.context_cfg = context_cfg
+        self.setup_done = False
+
+    def setup(self):
+        """Scenario setup."""
+        host = self.context_cfg['host']
+        user = host.get('user', 'ubuntu')
+        ip = host.get('ip', None)
+        key_filename = host.get('key_filename', '~/.ssh/id_rsa')
+
+        LOG.info("user:%s, host:%s", user, ip)
+        self.client = ssh.SSH(user, ip, key_filename=key_filename)
+        self.client.wait(timeout=600)
+
+        self.setup_done = True
+
+    def _execute_command(self, cmd):
+        """Execute a command on server."""
+        LOG.info("Executing: %s" % cmd)
+        status, stdout, stderr = self.client.execute(cmd)
+        if status:
+            raise RuntimeError("Failed executing command: ",
+                               cmd, stderr)
+        return stdout
+
+    def _filtrate_result(self, result):
+        fields = []
+        free = {}
+        ite = 0
+        average = {'total': 0, 'used': 0, 'free': 0, 'cached': 0, 'shared': 0,
+                   'buffers': 0}
+        maximum = {'total': 0, 'used': 0, 'free': 0, 'cached': 0, 'shared': 0,
+                   'buffers': 0}
+
+        for row in result.split('\n'):
+            line = row.split()
+
+            if line and line[0] == 'total':
+                # header fields
+                fields = line[:]
+            elif line and line[0] == 'Mem:':
+                memory = 'memory' + str(ite)
+                ite += 1
+                values = line[1:]
+                if values and len(values) == len(fields):
+                    free[memory] = dict(zip(fields, values))
+
+        for entry in free:
+            for item in average:
+                average[item] += int(free[entry][item])
+
+            for item in maximum:
+                if int(free[entry][item]) > maximum[item]:
+                    maximum[item] = int(free[entry][item])
+
+        for item in average:
+            average[item] = average[item] / len(free)
+
+        return {'free': free, 'average': average, 'max': maximum}
+
+    def _get_mem_usage(self):
+        """Get memory usage using free."""
+        options = self.scenario_cfg['options']
+        interval = options.get("interval", 1)
+        count = options.get("count", 1)
+
+        cmd = "free -s %s -c %s" % (interval, count)
+
+        result = self._execute_command(cmd)
+        filtrated_result = self._filtrate_result(result)
+
+        return filtrated_result
+
+    def run(self, result):
+        """Read processor statistics."""
+        if not self.setup_done:
+            self.setup()
+
+        result.update(self._get_mem_usage())
