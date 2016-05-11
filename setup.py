1354337e37667c45249f16e9278f7a62494b084f
rexlee8776 <limingjiang@huawei.com>
2016-05-11 07:20:47 +0000

    fix bug when run tc019(conf file copy missing in setup.py)
    JIRA: YARDSTICK-261
    
    Change-Id: I142977923935a86ee88f4bcbdc859197cbb18fb8
    Signed-off-by: rexlee8776 <limingjiang@huawei.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ setup(
     include_package_data=True,
     package_data={
         'yardstick': [
+            'benchmark/scenarios/availability/*.yaml',
             'benchmark/scenarios/availability/attacker/*.yaml',
+            'benchmark/scenarios/availability/ha_tools/*.bash',
             'benchmark/scenarios/availability/attacker/scripts/*.bash',
             'benchmark/scenarios/availability/monitor/*.yaml',
             'benchmark/scenarios/availability/monitor/script_tools/*.bash',
