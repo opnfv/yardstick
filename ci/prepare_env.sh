fa764905f8fc8b08d58c4c6d809ef65aeb5399e5
MatthewLi <matthew.lijun@huawei.com>
2016-05-19 22:06:11 -0400

    check if the env variable is valid or not before fetch_os_creds
    JIRA: YARDSTICK-265
    
    Change-Id: I7761344b95aefb1e46d0e68ef8a7e97066508917
    Signed-off-by: MatthewLi <matthew.lijun@huawei.com>

--- ci/prepare_env.sh
+++ ci/prepare_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ##############################################################################
-# Copyright (c) 2015 Ericsson AB and others.
+# Copyright (c) 2015 Ericsson AB, Huawei Technologies Co.,Ltd and others.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
@@ -10,8 +10,6 @@
 
 # Perepare the environment to run yardstick ci
 
-: ${INSTALLER_TYPE:='fuel'}
-: ${INSTALLER_IP:='10.20.0.2'}
 : ${DEPLOY_TYPE:='bm'} # Can be any of 'bm' (Bare Metal) or 'virt' (Virtual)
 
 : ${NODE_NAME:='unknown'}
@@ -22,12 +20,25 @@
 #  e.g. EXTERNAL_NETWORK='ext-net;flat;192.168.0.2;192.168.0.253;192.168.0.1;192.168.0.0/24'
 export EXTERNAL_NETWORK=$(echo $EXTERNAL_NETWORK | cut -f1 -d \;)
 
-echo
-echo "INFO: Creating openstack credentials .."
-
 # Create openstack credentials
+echo "INFO: Creating openstack credentials .."
 OPENRC=/home/opnfv/openrc
+INSTALLERS=(apex compass fuel joid)
+
 if [ ! -f $OPENRC ]; then
+    # credentials file is not given, check if environment variables are set
+    # to get the creds using fetch_os_creds.sh later on
+    echo "INFO: Checking environment variables INSTALLER_TYPE and INSTALLER_IP"
+    if [ -z ${INSTALLER_TYPE} ]; then
+        echo "environment variable 'INSTALLER_TYPE' is not defined."
+        exit 1
+    elif [[ ${INSTALLERS[@]} =~ ${INSTALLER_TYPE} ]]; then
+        echo "INSTALLER_TYPE env variable found: ${INSTALLER_TYPE}"
+    else
+        echo "Invalid env variable INSTALLER_TYPE=${INSTALLER_TYPE}"
+        exit 1
+    fi
+
     if [ "$DEPLOY_TYPE" == "virt" ]; then
         FETCH_CRED_ARG="-v -d $OPENRC -i ${INSTALLER_TYPE} -a ${INSTALLER_IP}"
     else
