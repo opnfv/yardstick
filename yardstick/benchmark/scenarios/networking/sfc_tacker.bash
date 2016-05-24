f0cac6d242b4cf208c1148170d5990255449a9d7
Manuel Buil <manuel.buil@ericsson.com>
2016-05-24 16:11:53 +0200

    SFC Yardstick test (do not merge yet)
    Modifications of the SFC Yardstick test so that it works
    
    WORK ON-GOING, please wait until end
    
    Change-Id: Ide6588a682f3491ab58c47ee7335205868c109fc
    Signed-off-by: Manuel Buil <manuel.buil@ericsson.com>

--- yardstick/benchmark/scenarios/networking/sfc_tacker.bash
+++ yardstick/benchmark/scenarios/networking/sfc_tacker.bash
@@ -1,19 +1,31 @@
 #!/bin/bash
-set -e
-BASEDIR= `pwd`
+BASEDIR=`pwd`
 
 #import VNF descriptor
-tacker vnfd-create --vnfd-file ${BASEDIR}/test-vnfd.yaml
+tacker vnfd-create --vnfd-file ${BASEDIR}/test-vnfd1.yaml
+tacker vnfd-create --vnfd-file ${BASEDIR}/test-vnfd2.yaml
 
 #create instances of the imported VNF
-tacker vnf-create --name testVNF1 --vnfd-name test-vnfd
-tacker vnf-create --name testVNF2 --vnfd-name test-vnfd
+tacker vnf-create --name testVNF1 --vnfd-name test-vnfd1
+tacker vnf-create --name testVNF2 --vnfd-name test-vnfd2
+
+key=true
+while $key;do
+        sleep 3
+        active=`tacker vnf-list | grep -E 'PENDING|ERROR'`
+        echo -e "checking if SFs are up:  $active"
+        if [ -z "$active" ]; then
+                key=false
+        fi
+done
 
 #create service chain
-tacker sfc-create --name chainA --chain testVNF1
-tacker sfc-create --name chainB --chain testVNF2
+tacker sfc-create --name red --chain testVNF1
+tacker sfc-create --name blue --chain testVNF2
 
 #create classifier
-tacker sfc-classifier-create --name myclassA --chain chainA --match dest_port=80,protocol=6
-tacker sfc-classifier-create --name myclassB --chain chainB --match dest_port=22,protocol=6
+tacker sfc-classifier-create --name red_http --chain red --match source_port=0,dest_port=80,protocol=6
+tacker sfc-classifier-create --name red_ssh --chain red --match source_port=0,dest_port=22,protocol=6
 
+tacker sfc-list
+tacker sfc-classifier-list
