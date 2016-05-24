51db1fa6a427e548657eb0958dad4d10cd80e9f4
Manuel Buil <manuel.buil@ericsson.com>
2016-05-24 16:11:53 +0200

    SFC Yardstick test
    Modifications of the SFC Yardstick test
    
    The test creates two chains. One chain blocks HTTP the other one blocks SSH.
    We doublecheck that HTTP works in one but not in the other and the same for
    SSH.
    
    There are some things that must be modified manually as ODL is not yet ready
    for ovs 2.5.90. Here are the instructions:
    
    https://wiki.opnfv.org/display/sfc/Yardstick
    
    Change-Id: Ide6588a682f3491ab58c47ee7335205868c109fc
    Signed-off-by: Manuel Buil <manuel.buil@ericsson.com>

--- yardstick/benchmark/scenarios/networking/sfc_teardown.bash
+++ yardstick/benchmark/scenarios/networking/sfc_teardown.bash
@@ -1,17 +1,18 @@
 #!/bin/bash
-set -e
+#set -e
 
 #delete classifier
-tacker sfc-classifier-create myclassA
-tacker sfc-classifier-create myclassB
+tacker sfc-classifier-delete red_http
+tacker sfc-classifier-delete red_ssh
 
 #delete service chain
-tacker sfc-delete chainA
-tacker sfc-delete chainB
+tacker sfc-delete red
+tacker sfc-delete blue
 
 #delete VNFs
 tacker vnf-delete testVNF1
 tacker vnf-delete testVNF2
 
 #delete VNF descriptor
-tacker vnfd-delete test-vnfd
+tacker vnfd-delete test-vnfd1
+tacker vnfd-delete test-vnfd2
