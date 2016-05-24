1c6daa7ae1d3f67b0d03ceb93a568c9226756c39
Manuel Buil <manuel.buil@ericsson.com>
2016-05-24 16:11:53 +0200

    SFC Yardstick test
    Modifications of the SFC Yardstick test
    
    The test creates two chains. One chain blocks HTTP the other one blocks SSH.
    We doublecheck that HTTP works in one but not in the other and the same for
    SSH.
    
    There are some things that must be modified manually as ODL is not yet ready
    for ovs 2.5.90. Here are the instructions:
    
    https://wiki.opnfv.org/display/sfc/Yardstick
    
    Change-Id: Ide6588a682f3491ab58c47ee7335205868c109fc
    Signed-off-by: Manuel Buil <manuel.buil@ericsson.com>

--- yardstick/benchmark/scenarios/networking/sfc_server.bash
+++ yardstick/benchmark/scenarios/networking/sfc_server.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 
-service iptables stop
-python -m SimpleHTTPServer 80
+#service iptables stop
+python -m SimpleHTTPServer 80 > /dev/null 2>&1 &
+touch index.html
+echo "WORKED" >> index.html
