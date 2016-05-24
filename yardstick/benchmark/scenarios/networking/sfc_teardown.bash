f7d341b943d6c971bcc8b3e5d795c50cb4eab06e
Manuel Buil <manuel.buil@ericsson.com>
2016-05-24 16:11:53 +0200

    SFC Yardstick test (do not merge yet)
    Modifications of the SFC Yardstick test so that it works
    
    WORK ON-GOING, please wait until end
    
    Change-Id: Ide6588a682f3491ab58c47ee7335205868c109fc
    Signed-off-by: Manuel Buil <manuel.buil@ericsson.com>

--- yardstick/benchmark/scenarios/networking/sfc_teardown.bash
+++ yardstick/benchmark/scenarios/networking/sfc_teardown.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -e
+#set -e
 
 #delete classifier
-tacker sfc-classifier-create myclassA
-tacker sfc-classifier-create myclassB
+tacker sfc-classifier-delete sfc1_http
+tacker sfc-classifier-delete sfc1_ssh
 
 #delete service chain
 tacker sfc-delete chainA
@@ -14,4 +14,5 @@ tacker vnf-delete testVNF1
 tacker vnf-delete testVNF2
 
 #delete VNF descriptor
-tacker vnfd-delete test-vnfd
+tacker vnfd-delete test-vnfd1
+tacker vnfd-delete test-vnfd2
