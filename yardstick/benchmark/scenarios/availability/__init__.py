1d6b2af637b06f815248003d85e0352512c9b374
lihuan <lihuansse@tongji.edu.cn>
2016-07-05 20:40:10 +0800

    Creating result checker classes
    JIRA: YARDSTICK-276
    
    Change-Id: I63c4f2c36108e95f5d3b7da42e66cb8c9b16c817
    Signed-off-by: lihuan <lihuansse@tongji.edu.cn>

---
+++ yardstick/benchmark/scenarios/availability/__init__.py
@@ -0,0 +1,25 @@
+##############################################################################
+# Copyright (c) 2016 Juan Qiu and others
+# juan_ qiu@tongji.edu.cn
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+
+class ActionType:
+    ATTACKER = "attacker"
+    MONITOR = "monitor"
+    RESULTCHECKER = "resultchecker"
+    RESULTCOMPARER = "comparer"
+    OPERATION = "operation"
+
+
+class Condition:
+    EQUAL = "eq"
+    GREATERTHAN = "gt"
+    GREATERTHANEQUAL = "gt_eq"
+    LESSTHAN = "lt"
+    LESSTHANEQUAL = "lt_eq"
+    IN = "in"
