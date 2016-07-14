151e2655c678e3241ac08d049945a0f35083d3af
Yaoguang Wang <sunshine.wang@huawei.com>
2016-07-14 19:59:12 +0800

    StorPerf integration into Yardstick as a pulgin.
    This task will install *StorPerf* on the Jump Host.
    StorPerf will run in docker container on the Jump Host.
    
    JIRA: YARDSTICK-303
    
    Change-Id: Ia592522a169f021adecc7d89d70079980be69665
    Signed-off-by: Yaoguang Wang <sunshine.wang@huawei.com>

---
+++ yardstick/resources/script/install/storperf.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright (c) 2016 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# StorPerf plugin installation script
+# After installation, it will run StorPerf container on Jump Host
+# Requirements:
+#     1. docker has been installed on the Jump Host
+#     2. Openstack environment file for storperf, '~/storperf_admin-rc', is ready.
+
+set -e
+
+mkdir -p /tmp/storperf-yardstick
+
+docker pull opnfv/storperf
+docker run -t \
+--env-file ~/storperf_admin-rc \
+-p 5000:5000 -p 8000:8000 \
+-v /tmp/storperf-yardstick/carbon:/opt/graphite/storage/whisper \
+--name storperf-yardstick opnfv/storperf &
