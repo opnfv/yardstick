359df8d838ca74c5b0c2687b170573f4655fe24c
kubi <jean.gaoliang@huawei.com>
2016-07-15 14:54:41 +0800

    Support Network Capacity Testing
    two metrics:Number of Connection, Number of Frame
    
    JIRA:YARDSTICK-283
    
    Change-Id: I71f1de25667437bbeac7c06749ff8fb38f41a791
    Signed-off-by: kubi <jean.gaoliang@huawei.com>

---
+++ tests/unit/benchmark/scenarios/networking/test_networkcapacity.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+##############################################################################
+# Copyright (c) 2016 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# Unittest for yardstick.benchmark.scenarios.networking.networkcapacity.NetworkCapacity
+
+import mock
+import unittest
+import os
+import json
+
+from yardstick.benchmark.scenarios.networking import networkcapacity
+
+SAMPLE_OUTPUT = '{"Number of connections":"308","Number of frames received": "166503"}'
+
+@mock.patch('yardstick.benchmark.scenarios.networking.networkcapacity.ssh')
+class NetworkCapacityTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.ctx = {
+            'nodes': {
+                'host1': {
+                    'ip': '172.16.0.137',
+                    'user': 'cirros',
+                    'key_filename': "mykey.key",
+                    'password': "root"
+                },
+            }
+        }
+
+        self.result = {}
+
+    def test_capacity_successful_setup(self, mock_ssh):
+        c = networkcapacity.NetworkCapacity({}, self.ctx)
+        mock_ssh.SSH().execute.return_value = (0, '', '')
+
+        c.setup()
+        self.assertIsNotNone(c.client)
+        self.assertTrue(c.setup_done)
+
+    def test_capacity_successful(self, mock_ssh):
+        c = networkcapacity.NetworkCapacity({}, self.ctx)
+
+        mock_ssh.SSH().execute.return_value = (0, SAMPLE_OUTPUT, '')
+        c.run(self.result)
+        expected_result = json.loads(SAMPLE_OUTPUT)
+        self.assertEqual(self.result, expected_result)
+
+    def test_capacity_unsuccessful_script_error(self, mock_ssh):
+        c = networkcapacity.NetworkCapacity({}, self.ctx)
+
+        mock_ssh.SSH().execute.return_value = (1, '', 'FOOBAR')
+        self.assertRaises(RuntimeError, c.run, self.result)
---
+++ yardstick/benchmark/scenarios/networking/networkcapacity.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright (c) 2016 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# Measure compute capacity and scale of a host
+
+set -e
+OUTPUT_FILE=/tmp/netperf-out.log
+
+# run capacity test
+run_capacity()
+{
+    netstat -s > $OUTPUT_FILE
+}
+
+# write the result to stdout in json format
+output_json()
+{
+    CONNECTIONS=$(awk '/active/{print $1}' $OUTPUT_FILE)
+    FRAMES=$(awk '/total/{print $1}' $OUTPUT_FILE)
+    echo -e "{ \
+        \"Number of connections\":\"$CONNECTIONS\", \
+        \"Number of frames received\": \"$FRAMES\" \
+    }"
+}
+
+main()
+{
+    run_capacity
+
+    output_json
+}
+
+main
