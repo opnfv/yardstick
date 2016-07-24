b3fe2550c2edb22a8b2c3222babfc5fc53ad74e5
tjuyinkanglin <14_ykl@tongji.edu.cn>
2016-07-24 23:33:55 +0800

    Create Bash Sheel Utilities for TC054
    JIRA: YARDSTICK-277
    
    Change-Id: I723bc6e0d054020831a8e9f743f20e51d937e4dc
    Signed-off-by: tjuyinkanglin <14_ykl@tongji.edu.cn>

---
+++ yardstick/benchmark/scenarios/availability/ha_tools/network/ping.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright (c) 2015 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# this script check the ip status via ping
+# Usage:
+#   ping <ip>
+# parameter ip - ip address
+
+ping $1 -c 1
