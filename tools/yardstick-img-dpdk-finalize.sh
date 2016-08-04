56bb5376fed94accbc6df72c02f005ae0e5ca3a5
wu.zhihui <wu.zhihui1@zte.com.cn>
2016-08-04 10:11:16 +0800

    [Yardstick-233]DPDK image for TC042
    These file is based on Akos's code.
    (https://gerrit.opnfv.org/gerrit/#/c/16095/)
    
    The different places:
    1. create flavor and import yardstick_key.pub with heat template.
    2. remove handle code about eth0 in user_data.
    3. update heat cli and delete step "nova stop" which is not necessary.
    4. clean the stack in final.
    5. add eth2 for vm.
    
    JIRA: YARDSTICK-233
    
    Change-Id: I04f2051fb4bcfeed2df4868d758fd92011c62233
    Signed-off-by: wu.zhihui <wu.zhihui1@zte.com.cn>

---
+++ tools/yardstick-img-dpdk-finalize.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# installs dpdk and pktgen packages on modified image
+
+# PREREQUISITES
+# modified image (yardstick-wily-server) must be uploaded to OpenStack
+# heat must be installed: apt-get install python-heatclient, python-glanceclient, python-nova
+# must have a public yardstick-key uploaded in openstack
+# must have a proper flavor for the image (i.e. m1.small)
+
+
+stackname="yardstick-modify-stack"
+template=dpdk_install.yml
+new_image_name="yardstick-image-pktgen-ready"
+
+openstack stack create $stackname -f yaml -t $template
+progress="WARMING_UP"
+
+while [ "$progress" != "CREATE_COMPLETE" ]
+do
+  sleep 10
+  echo "check stack status......."
+  show_output=$(openstack stack show $stackname)
+  progress=$(echo $show_output | sed 's/^.*stack_status . \([^ ]*\).*$/\1/')
+  echo "$progress"
+  if [ "$progress" == "CREATE_FAILED" ];then
+    echo "create $stackname failed"
+    exit 1
+  fi
+done
+
+status=$(nova image-create --poll $stackname $new_image_name)
+if [[ "$status" =~ "Finished" ]];then
+  echo "$new_image_name finished"
+fi
+
+openstack stack delete --yes $stackname
