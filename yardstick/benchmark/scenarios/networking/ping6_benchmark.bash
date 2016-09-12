ff4a1c76e6053888a152b189c761bb5333ffb844
MatthewLi <matthew.lijun@huawei.com>
2016-09-12 06:53:54 -0400

    add ping6 parameters
    JIRA: YARDSTICK-315
    
    1) make packetsize workable
    2) add ping_count parameter
    
    Change-Id: Ice2235fc5744b94df6a3f981c23159ca8280d876
    Signed-off-by: MatthewLi <matthew.lijun@huawei.com>

--- yardstick/benchmark/scenarios/networking/ping6_benchmark.bash
+++ yardstick/benchmark/scenarios/networking/ping6_benchmark.bash
@@ -11,6 +11,7 @@
 
 # Run a single ping6 command towards a ipv6 router
 set -e
+ping6_options=$1
 source /opt/admin-openrc.sh
 chmod 600 vRouterKey
 # TODO find host
@@ -29,4 +30,4 @@ wait_vm_ok() {
 }
 wait_vm_ok
 sleep 360
-sudo ip netns exec qdhcp-$(neutron net-list | grep -w ipv4-int-network1 | awk '{print $2}') ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i vRouterKey fedora@20.0.0.4 "ping6 -c 1 2001:db8:0:1::1 | grep ttl | awk -F [=\ ] '{printf \$10}'"
+sudo ip netns exec qdhcp-$(neutron net-list | grep -w ipv4-int-network1 | awk '{print $2}') ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i vRouterKey fedora@20.0.0.4 "ping6 $ping6_options 2001:db8:0:1::1 | grep ttl | awk -F [=\ ] '{printf \$10}'"
