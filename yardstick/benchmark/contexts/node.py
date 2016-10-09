c76f37bf1fd6ab4bf3b4e5d9506dfe2932d6ecb8
chenjiankun <chenjiankun1@huawei.com>
2016-10-09 10:11:24 +0000

    Bugfix: no pod.yaml file error when run test case not in the root path
    JIRA: YARDSTICK-364
    
    Change-Id: I8c460064c058f77971140f69689ba638179f89d9
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

--- yardstick/benchmark/contexts/node.py
+++ yardstick/benchmark/contexts/node.py
@@ -12,6 +12,7 @@ import yaml
 import logging
 
 from yardstick.benchmark.contexts.base import Context
+from yardstick.definitions import ROOT_PATH
 
 LOG = logging.getLogger(__name__)
 
@@ -33,7 +34,8 @@ class NodeContext(Context):
     def init(self, attrs):
         '''initializes itself from the supplied arguments'''
         self.name = attrs["name"]
-        self.file_path = attrs.get("file", "/etc/yardstick/nodes/pod.yaml")
+        self.file_path = attrs.get("file", "")
+        self.file_path = ROOT_PATH + self.file_path
 
         LOG.info("Parsing pod file: %s", self.file_path)
 
