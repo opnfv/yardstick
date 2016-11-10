b8e7f4515bea28e54cec9211467e674df2d1b308
chenjiankun <chenjiankun1@huawei.com>
2016-11-10 07:24:15 +0000

    Yardstick installation unified (pip/setup)
    JIRA: YARDSTICK-394
    
    Change-Id: I486aa24121b2ad8d66cd6df97ca86ef826862c91
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

---
+++ install.sh
@@ -0,0 +1,42 @@
+# install tools
+apt-get update && apt-get install -y \
+    wget \
+    expect \
+    curl \
+    git \
+    sshpass \
+    qemu-utils \
+    kpartx \
+    libffi-dev \
+    libssl-dev \
+    python \
+    python-dev \
+    libxml2-dev \
+    libxslt1-dev \
+    python-setuptools && \
+    easy_install -U setuptools
+
+apt-get -y autoremove && apt-get clean
+
+
+# fit for arm64
+if [ -z "$(grep '^deb \[' /etc/apt/sources.list)" ];then
+    source_file=/etc/apt/sources.list
+    touch "${source_file}"
+    sed -i -e 's/^deb /deb [arch=amd64] /g' "${source_file}"
+    sed -i -e 's/^deb-src /# deb-src /g' "${source_file}"
+    source_file=/etc/apt/sources.list.d/yardstick.list
+    echo -e "\n"
+    echo -e "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ trusty main universe multiverse restricted" >> "${source_file}"
+    echo -e "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ trusty-updates main universe multiverse restricted" >> "${source_file}"
+    echo -e "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ trusty-security main universe multiverse restricted" >> "${source_file}"
+    echo -e "deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ trusty-proposed main universe multiverse restricted" >> "${source_file}"
+    echo "vm.mmap_min_addr = 0" > /etc/sysctl.d/mmap_min_addr.conf
+    dpkg --add-architecture arm64
+    apt-get install -y qemu-user-static libc6:arm64
+fi
+
+# install yardstick + dependencies
+easy_install -U pip
+pip install -r requirements.txt
+pip install .
