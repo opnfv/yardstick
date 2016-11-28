c383ae3fa6ccb865575eacf78209fdd3ac7efa69
chenjiankun <chenjiankun1@huawei.com>
2016-11-28 01:51:47 +0000

    Create API and command to create a influxDB container
    JIRA: YARDSTICK-425
    
    This API is used to create a influxDB Container
    Add command line to create a influxDB Container, too
    
    Change-Id: If9c2d04b779924d492a5d5ea91f7968fa959570e
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

--- api/views.py
+++ api/views.py
@@ -14,6 +14,7 @@ from flask_restful import Resource
 from api.utils import common as common_utils
 from api.actions import test as test_action
 from api.actions import result as result_action
+from api.actions import env as env_action
 
 logger = logging.getLogger(__name__)
 
@@ -38,5 +39,17 @@ class Result(Resource):
 
         try:
             return getattr(result_action, action)(args)
+        except AttributeError:
+            return common_utils.error_handler('Wrong action')
+
+
+class Env(Resource):
+    def post(self):
+        action = common_utils.translate_to_str(request.json.get('action', ''))
+        args = common_utils.translate_to_str(request.json.get('args', {}))
+        logger.debug('Input args is: action: %s, args: %s', action, args)
+
+        try:
+            return getattr(env_action, action)(args)
         except AttributeError:
             return common_utils.error_handler('Wrong action')
---
+++ tests/unit/common/test_httpClient.py
@@ -0,0 +1,33 @@
+##############################################################################
+# Copyright (c) 2016 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+import unittest
+import mock
+import json
+
+from yardstick.common import httpClient
+
+
+class HttpClientTestCase(unittest.TestCase):
+
+    @mock.patch('yardstick.common.httpClient.requests')
+    def test_post(self, mock_requests):
+        url = 'http://localhost:5000/hello'
+        data = {'hello': 'world'}
+        headers = {'Content-Type': 'application/json'}
+        httpClient.HttpClient().post(url, data)
+        mock_requests.post.assert_called_with(url, data=json.dumps(data),
+                                              headers=headers)
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
