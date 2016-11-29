6d202ca9b981934313821bf725c9a002982a99a7
Ross Brattain <ross.b.brattain@intel.com>
2016-11-28 16:35:35 -0800

    centralize logging into root logger
    If we setup root logger correctly and have each
    module logger propogate we shouldn't need individual
    logger configuration
    
    updates:
        lower paramiko to WARN level
        dispatcher/file.py was missing logging.handlers import
        purge all existing handlers and add our own handlers
        move everything back into yardstick/__init__.py so API can use it
        make _LOG_STREAM_HDLR global, so we can set loglevel on it whenever
    
    Change-Id: If000799590379d3407655a7d54378481a96ea3d4
    Signed-off-by: Ross Brattain <ross.b.brattain@intel.com>

--- yardstick/__init__.py
+++ yardstick/__init__.py
@@ -8,18 +8,40 @@
 ##############################################################################
 
 import logging
-import logging.config
-import sys
 import os
+import sys
+
 import yardstick.vTC.apexlake as apexlake
 
 # Hack to be able to run apexlake unit tests
 # without having to install apexlake.
 sys.path.append(os.path.dirname(apexlake.__file__))
 
-logging.basicConfig(
-    level=logging.WARNING,
-    format='[%(asctime)s] %(name)-20s %(filename)s:%(lineno)d '
-        '%(levelname)s %(message)s',  # noqa
-    datefmt='%m/%d/%y %H:%M:%S')
-logging.getLogger(__name__).setLevel(logging.INFO)
+LOG_FILE = '/tmp/yardstick.log'
+LOG_FORMATTER = ('%(asctime)s '
+                 '%(name)s %(filename)s:%(lineno)d '
+                 '%(levelname)s %(message)s')
+
+_LOG_FORMATTER = logging.Formatter(LOG_FORMATTER)
+_LOG_STREAM_HDLR = logging.StreamHandler()
+_LOG_FILE_HDLR = logging.FileHandler(LOG_FILE)
+
+def _init_logging():
+
+    _LOG_STREAM_HDLR.setFormatter(_LOG_FORMATTER)
+
+    # don't append to log file, clobber
+    _LOG_FILE_HDLR.setFormatter(_LOG_FORMATTER)
+    _LOG_FILE_HDLR.setLevel(logging.DEBUG)
+
+    del logging.root.handlers[:]
+    logging.root.setLevel(logging.DEBUG)
+    logging.root.addHandler(_LOG_STREAM_HDLR)
+    logging.root.addHandler(_LOG_FILE_HDLR)
+    logging.debug("logging.root.handlers = %s", logging.root.handlers)
+
+    CI_DEBUG = os.environ.get('CI_DEBUG')
+    if CI_DEBUG is not None and CI_DEBUG.lower() == "true":
+        _LOG_STREAM_HDLR.setLevel(logging.DEBUG)
+    else:
+        _LOG_STREAM_HDLR.setLevel(logging.INFO)
