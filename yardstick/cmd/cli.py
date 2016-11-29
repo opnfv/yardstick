7d228736fbd8222333bf8d17a5bcfcdedd76b7a3
Ross Brattain <ross.b.brattain@intel.com>
2016-11-28 16:35:35 -0800

    centralize logging into root logger
    If we setup root logger correctly and have each
    module logger propogate we shouldn't need individual
    logger configuration
    
    updates:
        lower paramiko to WARN level
        dispatcher/file.py was missing logging.handlers import
        purge all existing handlers and add our own handlers
        move everything back into yardstick/__init__.py so API can use it
        make _LOG_STREAM_HDLR global, so we can set loglevel on it whenever
        added api/server.py call to _init_logging
        removed old LOG_FORMATTER from cli.py
    
    Change-Id: If000799590379d3407655a7d54378481a96ea3d4
    Signed-off-by: Ross Brattain <ross.b.brattain@intel.com>

--- yardstick/cmd/cli.py
+++ yardstick/cmd/cli.py
@@ -19,6 +19,7 @@ from pkg_resources import get_distribution
 from argparse import RawDescriptionHelpFormatter
 from oslo_config import cfg
 
+from yardstick import _LOG_STREAM_HDLR, _init_logging
 from yardstick.cmd.commands import task
 from yardstick.cmd.commands import runner
 from yardstick.cmd.commands import scenario
@@ -127,15 +128,12 @@ class YardstickCLI():
 
     def _handle_global_opts(self):
 
-        # handle global opts
-        logger = logging.getLogger('yardstick')
-        logger.setLevel(logging.WARNING)
-
+        _init_logging()
         if CONF.verbose:
-            logger.setLevel(logging.INFO)
+            _LOG_STREAM_HDLR.setLevel(logging.INFO)
 
         if CONF.debug:
-            logger.setLevel(logging.DEBUG)
+            _LOG_STREAM_HDLR.setLevel(logging.DEBUG)
 
     def _dispath_func_notask(self):
 
