68e7b6dd37e79b90251104e8638dc6c83d87f4f7
chenjiankun <chenjiankun1@huawei.com>
2016-12-02 00:30:58 +0000

    Add API to create Grafana container
    JIRA: YARDSTICK-441
    
    Change-Id: Ia848c4af072915ef252e8e03100dd7a4e4a6c3c2
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

--- api/actions/env.py
+++ api/actions/env.py
@@ -14,27 +14,95 @@ from docker import Client
 
 from yardstick.common import constants as config
 from yardstick.common import utils as yardstick_utils
+from yardstick.common.httpClient import HttpClient
 from api import conf as api_conf
-from api.utils import common as common_utils
 from api.utils import influx
 
 logger = logging.getLogger(__name__)
 
 
-def createInfluxDBContainer(args):
-    try:
-        container = _create_influxdb_container()
-        _config_output_file()
-        thread = threading.Thread(target=_config_influxdb)
-        thread.start()
-        return common_utils.result_handler('success', container)
-    except Exception as e:
-        message = 'Failed to create influxdb container: %s' % e
-        return common_utils.error_handler(message)
+def createGrafanaContainer(args):
+    thread = threading.Thread(target=_create_grafana)
+    thread.start()
 
 
-def _create_influxdb_container():
+def _create_grafana():
     client = Client(base_url=config.DOCKER_URL)
+
+    if not _check_grafana_iamge_exists(client):
+        client.pull('grafana/grafana')
+
+    _create_grafana_container(client)
+
+    time.sleep(5)
+
+    _create_data_source()
+
+
+def _create_data_source():
+    url = 'http://admin:admin@%s:3000/api/datasources' % api_conf.GATEWAY_IP
+    data = {
+        "name": "yardstick",
+        "type": "influxdb",
+        "access": "proxy",
+        "url": "http://%s:8086" % api_conf.GATEWAY_IP,
+        "password": "root",
+        "user": "root",
+        "database": "yardstick",
+        "basicAuth": True,
+        "basicAuthUser": "admin",
+        "basicAuthPassword": "admin",
+        "isDefault": False,
+    }
+    HttpClient().post(url, data)
+
+
+def _create_grafana_container(client):
+    ports = [3000]
+    port_bindings = {k: k for k in ports}
+    host_config = client.create_host_config(port_bindings=port_bindings)
+
+    container = client.create_container(image='grafana/grafana',
+                                        ports=ports,
+                                        detach=True,
+                                        tty=True,
+                                        host_config=host_config)
+    client.start(container)
+
+
+def _check_grafana_iamge_exists(client):
+    l = filter(lambda a: a['RepoTags'] and a['RepoTags'][0].find(
+        'grafana/grafana') != -1, client.images())
+    return len(l) > 0
+
+
+def createInfluxDBContainer(args):
+    thread = threading.Thread(target=_create_influxdb)
+    thread.start()
+
+
+def _create_influxdb():
+    client = Client(base_url=config.DOCKER_URL)
+
+    if not _check_influxdb_image_exists(client):
+        client.pull('tutum/influxdb')
+
+    _create_influxdb_container(client)
+
+    _config_output_file()
+
+    time.sleep(5)
+
+    _config_influxdb()
+
+
+def _check_influxdb_image_exists(client):
+    l = filter(lambda a: a['RepoTags'] and a['RepoTags'][0].find(
+        'tutum/influxdb') != -1, client.images())
+    return len(l) > 0
+
+
+def _create_influxdb_container(client):
 
     ports = [8083, 8086]
     port_bindings = {k: k for k in ports}
@@ -46,11 +114,9 @@ def _create_influxdb_container():
                                         tty=True,
                                         host_config=host_config)
     client.start(container)
-    return container
 
 
 def _config_influxdb():
-    time.sleep(20)
     try:
         client = influx.get_data_db_client()
         client.create_user(config.USER, config.PASSWORD, config.DATABASE)
