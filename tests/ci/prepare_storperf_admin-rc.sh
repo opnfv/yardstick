38f2fdc9dea5788e2ce8ce04eef657c12d8d4dd7
JingLu5 <lvjing5@huawei.com>
2016-12-16 14:38:16 +0800

    Use openstack command instead keystone command in prepare_storperf_admin-rc.sh
    JIRA: YARDSTICK-476
    
    In Openstack Newton, the ”tenant“ is deprecated and "project" is used.
    The prepare_storperf_admin-rc.sh uses keystone command to get "tenant id".
    So for newton, we need to use openstack command to get "project id".
    
    Change-Id: Ic69cb07f684ccb557866a4141e9d2724fc054f29
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

--- tests/ci/prepare_storperf_admin-rc.sh
+++ tests/ci/prepare_storperf_admin-rc.sh
@@ -15,8 +15,7 @@ PASSWORD=${OS_PASSWORD:-console}
 TENANT_NAME=${OS_TENANT_NAME:-admin}
 VOLUME_API_VERSION=${OS_VOLUME_API_VERSION:-2}
 PROJECT_NAME=${OS_PROJECT_NAME:-$TENANT_NAME}
-TENANT_ID=`keystone tenant-get admin|grep 'id'|awk -F '|' '{print $3}'|sed -e 's/^[[:space:]]*//'`
-
+TENANT_ID=`openstack project show admin|grep '\bid\b' |awk -F '|' '{print $3}'|sed -e 's/^[[:space:]]*//'`
 
 rm -f ~/storperf_admin-rc
 touch ~/storperf_admin-rc
