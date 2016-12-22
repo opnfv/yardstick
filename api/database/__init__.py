8d702f720aeb9566adb258e886c12a348bedc84b
chenjiankun <chenjiankun1@huawei.com>
2016-12-22 08:41:08 +0000

    Add sqlite and SQLAlchemy support for API
    JIRA: YARDSTICK-505
    
    Now yardstick API need a database to store API data.
    And for future the yardstick GUI also need a self database.
    So I choose a light-weight database sqlite.
    And use SQLAlchemy to do ORM.
    
    Change-Id: I1edc350ec6f57ad67785de549c2135c86ea60a4a
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

---
+++ api/database/__init__.py
@@ -0,0 +1,29 @@
+##############################################################################
+# Copyright (c) 2016 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+import logging
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+engine = create_engine('sqlite:////tmp/yardstick.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+def init_db():
+    subclasses = [subclass.__name__ for subclass in Base.__subclasses__()]
+    logger.debug('Import models: %s', subclasses)
+    Base.metadata.create_all(bind=engine)
