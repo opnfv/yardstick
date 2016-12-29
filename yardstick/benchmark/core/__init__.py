a2485ae32e9059178ad6a849543e6e8a6b18ff48
chenjiankun <chenjiankun1@huawei.com>
2016-12-29 11:45:12 +0000

    Split Yardstick CLI with Yardstick core logic
    JIRA: YARDSTICK-511
    
    We need to unify yardstick entry. Now the solution is using CLI call API
    as nova do.
    This is the first step: coupling the yardstick core logic from CLI.
    Moving the core logic to yardstick/benchmark/core and the CLI using a
    object to call yardstick core logic.
    
    Change-Id: I84f10d2134635880c281cc63212a8533f2dd7d4e
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

---
+++ yardstick/benchmark/core/__init__.py
@@ -0,0 +1,38 @@
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+from __future__ import print_function
+
+
+class Param(object):
+
+    def __init__(self, kwargs):
+        # list
+        self.inputfile = kwargs.get('inputfile')
+        self.task_args = kwargs.get('task-args')
+        self.task_args_file = kwargs.get('task-args-file')
+        self.keep_deploy = kwargs.get('keep-deploy')
+        self.parse_only = kwargs.get('parse-only')
+        self.output_file = kwargs.get('output-file', '/tmp/yardstick.out')
+        self.suite = kwargs.get('suite')
+
+        # list
+        self.input_file = kwargs.get('input_file')
+
+        # list
+        self.casename = kwargs.get('casename')
+
+        # list
+        self.type = kwargs.get('type')
+
+
+def print_hbar(barlen):
+    '''print to stdout a horizontal bar'''
+    print("+"),
+    print("-" * barlen),
+    print("+")
