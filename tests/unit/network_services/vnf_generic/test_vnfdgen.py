9bc1fad18c8f200567abad5b8bcccfe92295ab57
Deepak S <deepak.s@linux.intel.com>
2016-12-30 09:21:22 -0800

    Introducing Generic framework to do pre-deployment VNF & Network service testing
    This patch introduces the framework which is aligned with ETSI-TST001
    
    This patch adds:
    1. NetworkServiceTestCase introduces following functions
          --> setup
                --> Verify if infrastructure mapping can meet topology
                --> Load VNF models
                --> Fill traffic profile with information from topology
                --> Provision VNFs
                --> Run experiment (traffic)
          --> run -> Yardstick calls run() at intervals defined in the yaml
                     and produces timestamped samples
          --> teardown --> Stop  VNFs
    
    2. TrafficProfile is a generic class to get traffic profile for a given
       testcase and select the traffic generator for testcase.
    
    3. QueueFileWrapper is a class to send/recive cmds to vnf
    
    4. GenericVNF is a generic class to instantiate VNF
    
    5. GenericTrafficGen is a generic class to run/listen/verify traffic.
    
    JIRA: YARDSTICK-483
    Change-Id: Ic453c917d34dcb508a7f3afb459011da85f6402e
    Signed-off-by: Deepak S <deepak.s@linux.intel.com>

---
+++ tests/unit/network_services/vnf_generic/test_vnfdgen.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2016-2017 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Unittest for yardstick.network_services.vnf_generic.vnfdgen
+
+from __future__ import absolute_import
+import unittest
+from six.moves import range
+
+from yardstick.network_services.vnf_generic import vnfdgen
+
+TREX_VNFD_TEMPLATE = """
+vnfd:vnfd-catalog:
+    vnfd:
+    -   id: TrexTrafficGen  # ISB class mapping
+        name: trexgen
+        short-name: trexgen
+        description: TRex stateless traffic generator for RFC2544
+        mgmt-interface:
+            vdu-id: trexgen-baremetal
+            user: {{user}}  # Value filled by vnfdgen
+            password: {{password}}  # Value filled by vnfdgen
+            ip: {{ip}}  # Value filled by vnfdgen
+        connection-point:
+        -   name: xe0
+            type: VPORT
+        -   name: xe1
+            type: VPORT
+        vdu:
+        -   id: trexgen-baremetal
+            name: trexgen-baremetal
+            description: TRex stateless traffic generator for RFC2544
+            external-interface:
+            -   name: xe0
+                virtual-interface:
+                    type: PCI-PASSTHROUGH
+                    vpci: '{{ interfaces.xe0.vpci}}'
+                    local_ip: '{{ interfaces.xe0.local_ip }}'
+                    dst_ip: '{{ interfaces.xe0.dst_ip }}'
+                    local_mac: '{{ interfaces.xe0.local_mac }}'
+                    dst_mac: '{{ interfaces.xe0.dst_mac }}'
+                    bandwidth: 10 Gbps
+                vnfd-connection-point-ref: xe0
+            -   name: xe1
+                virtual-interface:
+                    type: PCI-PASSTHROUGH
+                    vpci: '{{ interfaces.xe1.vpci }}'
+                    local_ip: '{{ interfaces.xe1.local_ip }}'
+                    dst_ip: '{{ interfaces.xe1.dst_ip }}'
+                    local_mac: '{{ interfaces.xe1.local_mac }}'
+                    dst_mac: '{{ interfaces.xe1.dst_mac }}'
+                    bandwidth: 10 Gbps
+                vnfd-connection-point-ref: xe1
+
+        benchmark:
+            kpi:
+                - rx_throughput_fps
+                - tx_throughput_fps
+                - tx_throughput_mbps
+                - rx_throughput_mbps
+                - tx_throughput_pc_linerate
+                - rx_throughput_pc_linerate
+                - min_latency
+                - max_latency
+                - avg_latency
+"""
+
+COMPLETE_TREX_VNFD = \
+    {'vnfd:vnfd-catalog':
+     {'vnfd':
+      [{'benchmark':
+        {'kpi':
+         ['rx_throughput_fps',
+          'tx_throughput_fps',
+          'tx_throughput_mbps',
+          'rx_throughput_mbps',
+          'tx_throughput_pc_linerate',
+          'rx_throughput_pc_linerate',
+          'min_latency',
+          'max_latency',
+          'avg_latency']},
+        'connection-point': [{'name': 'xe0',
+                              'type': 'VPORT'},
+                             {'name': 'xe1',
+                              'type': 'VPORT'}],
+        'description': 'TRex stateless traffic generator for RFC2544',
+        'id': 'TrexTrafficGen',
+        'mgmt-interface': {'ip': '1.1.1.1',
+                           'password': 'berta',
+                           'user': 'berta',
+                           'vdu-id': 'trexgen-baremetal'},
+        'name': 'trexgen',
+        'short-name': 'trexgen',
+        'vdu': [{'description': 'TRex stateless traffic generator for RFC2544',
+                 'external-interface':
+                 [{'name': 'xe0',
+                   'virtual-interface': {'bandwidth': '10 Gbps',
+                                         'dst_ip': '1.1.1.1',
+                                         'dst_mac': '00:01:02:03:04:05',
+                                         'local_ip': '1.1.1.2',
+                                         'local_mac': '00:01:02:03:05:05',
+                                         'type': 'PCI-PASSTHROUGH',
+                                         'vpci': '0000:00:10.2'},
+                   'vnfd-connection-point-ref': 'xe0'},
+                  {'name': 'xe1',
+                   'virtual-interface': {'bandwidth': '10 Gbps',
+                                         'dst_ip': '2.1.1.1',
+                                         'dst_mac': '00:01:02:03:04:06',
+                                         'local_ip': '2.1.1.2',
+                                         'local_mac': '00:01:02:03:05:06',
+                                         'type': 'PCI-PASSTHROUGH',
+                                         'vpci': '0000:00:10.1'},
+                   'vnfd-connection-point-ref': 'xe1'}],
+                 'id': 'trexgen-baremetal',
+                 'name': 'trexgen-baremetal'}]}]}}
+
+NODE_CFG = {'ip': '1.1.1.1',
+            'name': 'demeter',
+            'password': 'berta',
+            'role': 'TrafficGen',
+            'user': 'berta',
+            'interfaces': {'xe0': {'dpdk_port_num': 1,
+                                   'dst_ip': '1.1.1.1',
+                                   'dst_mac': '00:01:02:03:04:05',
+                                   'local_ip': '1.1.1.2',
+                                   'local_mac': '00:01:02:03:05:05',
+                                   'vpci': '0000:00:10.2'},
+                           'xe1': {'dpdk_port_num': 0,
+                                   'dst_ip': '2.1.1.1',
+                                   'dst_mac': '00:01:02:03:04:06',
+                                   'local_ip': '2.1.1.2',
+                                   'local_mac': '00:01:02:03:05:06',
+                                   'vpci': '0000:00:10.1'}}}
+
+
+TRAFFIC_PROFILE_TPL = """
+private:
+    - ipv4:
+        outer_l2:
+            framesize:
+                64B: "{{ get(imix, 'private.imix_small', 10) }}"
+                128B: "{{ get(imix, 'private.imix_128B', 10) }}"
+                256B: "{{ get(imix, 'private.imix_256B', 10) }}"
+                373B: "{{ get(imix, 'private.imix_373B', 10) }}"
+                570B: "{{get(imix, 'private.imix_570B', 10) }}"
+                1400B: "{{get(imix, 'private.imix_1400B', 10) }}"
+                1518B: "{{get(imix, 'private.imix_1500B', 40) }}"
+"""
+
+TRAFFIC_PROFILE = {
+    "private": [{"ipv4": {"outer_l2":
+                          {"framesize": {"64B": '10', "128B": '10',
+                                         "256B": '10', "373B": '10',
+                                         "570B": '10', "1400B": '10',
+                                         "1518B": '40'}}}}]}
+
+
+class TestVnfdGen(unittest.TestCase):
+    """ Class to verify VNFS testcases """
+
+    def test_generate_vnfd(self):
+        """ Function to verify vnfd generation based on template """
+        self.maxDiff = None
+        generated_vnfd = vnfdgen.generate_vnfd(TREX_VNFD_TEMPLATE, NODE_CFG)
+        self.assertDictEqual(COMPLETE_TREX_VNFD, generated_vnfd)
+
+    def test_generate_tp_no_vars(self):
+        """ Function to verify traffic profile generation without imix """
+
+        self.maxDiff = None
+        generated_tp = vnfdgen.generate_vnfd(TRAFFIC_PROFILE_TPL, {"imix": {}})
+        self.assertDictEqual(TRAFFIC_PROFILE, generated_tp)
+
+    def test_dict_flatten_empty_dict(self):
+        self.assertEqual(vnfdgen.dict_key_flatten({}), {})
+
+    def test_dict_flatten_int(self):
+        d = {'a': 1, 'b': 2}
+        self.assertEqual(vnfdgen.dict_key_flatten(d), d)
+
+    def test_dict_flatten_str(self):
+        d = {'a': "1", 'b': '2'}
+        self.assertEqual(vnfdgen.dict_key_flatten(d), d)
+
+    def test_dict_flatten_list(self):
+        d = {'a': 1, 'b': list(range(2))}
+        self.assertEqual(vnfdgen.dict_key_flatten(d),
+                         {'a': 1, 'b0': 0, 'b1': 1})
+
+    def test_dict_flatten_dict(self):
+        d = {'a': 1, 'b': {x: x for x in list(range(2))}}
+        self.assertEqual(vnfdgen.dict_key_flatten(d),
+                         {'a': 1, 'b.0': 0, 'b.1': 1})
+
+    def test_generate_tp_single_var(self):
+        """ Function to verify traffic profile generation with imix """
+
+        generated_tp = \
+            vnfdgen.generate_vnfd(TRAFFIC_PROFILE_TPL,
+                                  {"imix": {"private": {"imix_small": '20'}}})
+        self.maxDiff = None
+        tp2 = dict(TRAFFIC_PROFILE)
+        tp2["private"][0]["ipv4"]["outer_l2"]["framesize"]["64B"] = '20'
+        self.assertDictEqual(tp2, generated_tp)
