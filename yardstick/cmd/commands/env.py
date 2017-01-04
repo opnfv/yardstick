1bd94f6e849116897bbae9598acde03cdaa07c7b
chenjiankun <chenjiankun1@huawei.com>
2017-01-04 17:41:18 +0000

    Add API to get the status of async task
    JIRA: YARDSTICK-526
    
    Currently there are many API run a task using sub thread.
    But we don't know the status of this task.
    So we need to offer a API to query the status of this task.
    
    Change-Id: I8d2cc558750bf9270aed4a7abb8bf35d17894d83
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

--- yardstick/cmd/commands/env.py
+++ yardstick/cmd/commands/env.py
@@ -6,13 +6,11 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
-import logging
+from __future__ import print_function
+import time
 
 from yardstick.common.httpClient import HttpClient
-from yardstick.common import constants
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+from yardstick.common import constants as consts
 
 
 class EnvCommand(object):
@@ -21,19 +19,46 @@ class EnvCommand(object):
         Set of commands to prepare environment
     '''
     def do_influxdb(self, args):
-        url = constants.YARDSTICK_ENV_ACTION_API
         data = {'action': 'createInfluxDBContainer'}
-        HttpClient().post(url, data)
-        logger.debug('Now creating and configing influxdb')
+        task_id = self._start_async_task(data)
+
+        self._check_status(task_id)
 
     def do_grafana(self, args):
-        url = constants.YARDSTICK_ENV_ACTION_API
         data = {'action': 'createGrafanaContainer'}
-        HttpClient().post(url, data)
-        logger.debug('Now creating and configing grafana')
+        task_id = self._start_async_task(data)
+
+        self._check_status(task_id)
 
     def do_prepare(self, args):
-        url = constants.YARDSTICK_ENV_ACTION_API
         data = {'action': 'prepareYardstickEnv'}
-        HttpClient().post(url, data)
-        logger.debug('Now preparing environment')
+        task_id = self._start_async_task(data)
+
+        self._check_status(task_id)
+
+    def _start_async_task(self, data):
+        url = consts.ENV_ACTION_API
+        return HttpClient().post(url, data)['result']['task_id']
+
+    def _check_status(self, task_id):
+        url = '{}?task_id={}'.format(consts.ASYNC_TASK_API, task_id)
+
+        CHECK_STATUS_RETRY = 20
+        CHECK_STATUS_DELAY = 5
+
+        for retry in xrange(CHECK_STATUS_RETRY):
+            response = HttpClient().get(url)
+            status = response['status']
+
+            if status:
+                break
+
+            # wait until the async task finished
+            time.sleep(CHECK_STATUS_DELAY * (retry + 1))
+
+        switcher = {
+            0: 'Timeout',
+            1: 'Finished',
+            2: 'Error:{}'.format(response['result'])
+        }
+        print (switcher[status], flush=True)
