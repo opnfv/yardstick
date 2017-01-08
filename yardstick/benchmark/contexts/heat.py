adb75cb5685579334c1deab0d17b40a47470249d
Ross Brattain <ross.b.brattain@intel.com>
2017-01-08 00:17:32 -0800

    heat: convert open to context manager
    Change-Id: I5ce6c77839f4104d305d66238bb2c6cbdc39a28c
    Signed-off-by: Ross Brattain <ross.b.brattain@intel.com>

--- yardstick/benchmark/contexts/heat.py
+++ yardstick/benchmark/contexts/heat.py
@@ -94,9 +94,10 @@ class HeatContext(Context):
 
         rsa_key = paramiko.RSAKey.generate(bits=2048, progress_func=None)
         rsa_key.write_private_key_file(self.key_filename)
-        open(self.key_filename + ".pub", "w").write("%s %s\n" %
-                                                    (rsa_key.get_name(),
-                                                     rsa_key.get_base64()))
+        print("Writing %s ..." % self.key_filename)
+        with open(self.key_filename + ".pub", "w") as pubkey_file:
+            pubkey_file.write(
+                "%s %s\n" % (rsa_key.get_name(), rsa_key.get_base64()))
         del rsa_key
 
     @property
