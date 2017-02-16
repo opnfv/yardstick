6ca362ea3c7b45c885ffbd7058a796b4604c28a1
Deepak S <deepak.s@linux.intel.com>
2017-02-16 20:10:51 +0530

    Update missing license headers
    Change-Id: I063fd37fe25754c94d164ae5a209d15b69322093
    Signed-off-by: Deepak S <deepak.s@linux.intel.com>

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
+##############################################################################
+# Copyright (c) 2017 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
 # install tools
 apt-get update && apt-get install -y \
     wget \
