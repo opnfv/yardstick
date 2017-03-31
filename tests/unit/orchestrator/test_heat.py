a16524e9675a903372b18a753af13498b912d638
Jing Zhang <jing.c.zhang@nokia.com>
2017-03-31 09:20:46 -0400

    Add SRIOV support
    A generic provider network solution is introduced, where any existing provider network can be used. To identify whether a network specified in the test case is a provider network new attributes are introduced in the test case network section:
    networks:
    test-net1:
    cidr: '192.168.1.0/24'
    provider: sriov
    net: sriov-net1
    subnet: sriov-subnet1
    test-net2:
    cidr: '192.168.2.0/24'
    provider: sriov
    net: sriov-net2
    subnet: sriov-subnet2
    If the provider attribute is present, the network is an existing provider network. If the value is sriov, binding:vnic_type=direct is added to the interface in the heat deployment template. In orchestrator/heat.py, the interface creating functions are given a new parameter that tells if the network in use is a provider network. The benchmark/contexts/model.py is changed to store the value of the provider attribute from the test case and function calls to port creation is updated with the provider parameter. The same change is made in contexts/heat.py as well. Also calls for creating a new (tenant) network was surpressed if the provider attribute is present.
    
    Update-1: Change test_model.py
    Update-2: Per comment, change comment style to """"
    Update-3: Change test_heat.py
    Update-4: Add unit test cases
    Update-5: Add SRIOV provider network example in opnfv_yardstick_tc008.yaml
    Update-6: Per comment, remove empty line in orchestrator/test_heat.py
    
    Change-Id: I76004c4fcc9bffcfd8ed021fd647e0cecb346ef4
    JIRA: YARDSTICK-612
    Signed-off-by: Jing Zhang <jing.c.zhang@nokia.com>

--- tests/unit/orchestrator/test_heat.py
+++ tests/unit/orchestrator/test_heat.py
@@ -13,6 +13,7 @@
 
 import unittest
 import uuid
+import mock
 
 from yardstick.orchestrator import heat
 
@@ -24,3 +25,33 @@ class HeatContextTestCase(unittest.TestCase):
         k = heat.get_short_key_uuid(u)
         self.assertEqual(heat.HEAT_KEY_UUID_LENGTH, len(k))
         self.assertIn(k, str(u))
+
+class HeatTemplateTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_context = mock.Mock()
+        self.mock_context.name = 'bar'
+
+    def test_add_servergroup(self):
+        template = heat.HeatTemplate('test')
+        template.add_servergroup('some-server-group', 'anti-affinity')
+
+        self.assertEqual(template.resources['some-server-group']['type'], 'OS::Nova::ServerGroup')
+        self.assertEqual(template.resources['some-server-group']['properties']['policies'], ['anti-affinity'])
+
+class HeatStackTestCase(unittest.TestCase):
+
+    def test_delete_calls__delete_multiple_times(self):
+        stack = heat.HeatStack('test')
+        stack.uuid = 1
+        with mock.patch.object(stack, "_delete") as delete_mock:
+            stack.delete()
+        # call once and then call again if uuid is not none
+        self.assertGreater(delete_mock.call_count, 1)
+
+    def test_delete_all_calls_delete(self):
+        stack = heat.HeatStack('test')
+        stack.uuid = 1
+        with mock.patch.object(stack, "delete") as delete_mock:
+            stack.delete_all()
+        self.assertGreater(delete_mock.call_count, 0)
