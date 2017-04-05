8bc016436be5dfc46bd0847e95ed152b53a88d63
DanielMartinBuckley <daniel.m.buckley@intel.com>
2017-04-05 11:20:46 +0100

    DRAFT: Create flavor from heat context
    JIRA: YARDSTICK-582
    
    Create a customizable flavor via heat context. All heat parameters
    are configurable including Core Affinity. The default flavor name
    is XXXX-flavor where XXXX is stackname. Flavor attributes are taken
    from the heat context file. If a flavor attribute is not used it
    takes default attribute value. If flavor name is not specified it
    uses the server name + "-flavor" or stack-name + "-flavor".
    
    Compute node specific attributes are configurable via "extra_specs"
    attribute. See
    https://docs.openstack.org/admin-guide/compute-flavors.html for
    details.
    
    Change-Id: If4015970b889b0b95bfa8eba9491ebf31e92f2c7
    Signed-off-by: DanielMartinBuckley <daniel.m.buckley@intel.com>

--- tests/unit/benchmark/contexts/test_heat.py
+++ tests/unit/benchmark/contexts/test_heat.py
@@ -108,6 +108,50 @@ class HeatContextTestCase(unittest.TestCase):
             "foo-key",
             "2f2e4997-0a8e-4eb7-9fa4-f3f8fbbc393b")
         mock_template.add_security_group.assert_called_with("foo-secgroup")
+        mock_template.add_flavor("test")
+        mock_template.add_flavor(vcpus=4, ram=2048)
+
+    @mock.patch('yardstick.benchmark.contexts.heat.HeatTemplate')
+    def test__add_resources_to_template_misc(self, mock_template):
+
+        self.test_context.keypair_name = "foo-key"
+        self.test_context.secgroup_name = "foo-secgroup"
+        self.test_context.key_uuid = "2f2e4997-0a8e-4eb7-9fa4-f3f8fbbc393b"
+
+        self.test_context._add_resources_to_template(mock_template)
+        mock_template.add_keypair.assert_called_with(
+            "foo-key",
+            "2f2e4997-0a8e-4eb7-9fa4-f3f8fbbc393b")
+        mock_template.add_security_group.assert_called_with("foo-secgroup")
+        mock_template.add_flavor("test")
+        mock_template.add_flavor(vcpus=4, ram=2048)
+        mock_template.add_flavor(vcpus=4, ram=2048, extra_specs={"cat": 1, "dog": 2})
+        mock_template.add_network("network1")
+        mock_template.add_network("network2")
+        mock_template.add_security_group("sec_group1", "nopolicy")
+        mock_template.add_security_group("sec_group2", ["policy1", "policy2"])
+        mock_template.add_subnet("subnet1", "network1", "cidr1")
+        mock_template.add_subnet("subnet2", "network2", "cidr2")
+        mock_template.add_router("router1", "gw1", "subnet1")
+        mock_template.add_router_interface("router_if1", "router1" ,"subnet1")
+        mock_template.add_port("port1", "network1", "subnet1")
+        mock_template.add_port("port2", "network2", "subnet2", sec_group_id="sec_group1")
+        mock_template.add_floating_ip("floating_ip1", "network1", "port1", "router_if1")
+        mock_template.add_floating_ip("floating_ip2", "network2", "port2", "router_if2","foo-secgroup")
+        mock_template.add_floating_ip_association("floating_ip1_association", "floating_ip1", "port1")
+        mock_template.add_servergroup("server_grp1","nopolicy")
+        mock_template.add_servergroup("server_grp2", "affinity")
+        mock_template.add_servergroup("server_grp3", "anti-affinity")
+        mock_template.add_security_group("security_group")
+        mock_template.add_server(name="server1", image="image1", flavor="flavor1")
+        mock_template.add_server(name="server2", image="image1", flavor="flavor1", ports=["port1","port2"],
+                                    networks=["network1","network2"], scheduler_hints="hints1", user="user1",
+                                    key_name="foo-key", user_data="user", metadata={"cat":1, "doc": 2},
+                                    additional_properties={"prop1": 1, "prop2": 2})
+        mock_template.add_server(image="image1", flavor="flavor1", ports=["port1", "port2"],
+                                networks=["network1", "network2"], scheduler_hints="hints1", user="user1",
+                                key_name="foo-key", user_data="user", metadata={"cat": 1, "doc": 2},
+                                additional_properties={"prop1": 1, "prop2": 2})
 
     @mock.patch('yardstick.benchmark.contexts.heat.HeatTemplate')
     def test_deploy(self, mock_template):
