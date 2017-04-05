b2fb23ca8ccc1acedaa9156552af4ba347f24103
JingLu5 <lvjing5@huawei.com>
2017-04-05 06:11:16 +0000

    Bugfix: fix query job status in TC074
    JIRA: YARDSTICK-621
    The storperf job status query in TC074 didn' work properly. The cause is the
    "type: status" is not passed in the URL. This patch also update storperf VM
    image to xenial.
    
    Change-Id: Idd9f501416b24612f6045a57ba2a95e2ed3a9572
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

--- tests/ci/clean_images.sh
+++ tests/ci/clean_images.sh
@@ -19,7 +19,7 @@ cleanup()
         return
     fi
 
-    for image in $(openstack image list | grep -e cirros-0.3.5 -e yardstick-image -e Ubuntu-14.04 \
+    for image in $(openstack image list | grep -e cirros-0.3.5 -e yardstick-image -e Ubuntu-16.04 \
         | awk '{print $2}'); do
         echo "Deleting image $image..."
         openstack image delete $image || true
