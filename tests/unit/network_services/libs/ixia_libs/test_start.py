04e2e563d668f1c980063d27d949130b30a0509d
Deepak S <deepak.s@intel.com>
2017-04-06 09:29:59 -0700

    DRAFT: Add ixnet support to enable rfc2544 test
    Change-Id: I84dfc6760c4f04e049eb9dfa757a077c6d16652e
    Signed-off-by: Deepak S <deepak.s@intel.com>

---
+++ tests/unit/network_services/libs/ixia_libs/test_start.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2016-2017 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Unittest for yardstick.network_services.libs.ixia_libs.IxNet
+
+from __future__ import absolute_import
+import unittest
+import mock
+
+from yardstick.network_services.libs.ixia_libs.IxNet.start import \
+    IxStart
+
+
+class TestIxStart(unittest.TestCase):
+    def test__start_traffic(self):
+        ixnet_gen = IxStart()
+
+        ixnet = mock.MagicMock()
+        ixnet.execute = mock.Mock()
+        ixnet.commit = mock.Mock()
+        ixnet.getList = mock.Mock(return_value=[0])
+        ixnet.getAttribute = mock.Mock(return_value='down')
+        result = ixnet_gen._start_traffic(ixnet)
+        self.assertIsNone(result)
---
+++ yardstick/network_services/libs/ixia_libs/IxNet/ipv4.py
@@ -0,0 +1,54 @@
+# Copyright (c) 2016-2017 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class IxIPv4():
+    def _add_ipv4_header(self, ixNet, params):
+        tis = ixNet.getList('/traffic', 'trafficItem')
+        for ti in tis:
+            eps = ixNet.getList(ti, "configElement")
+            for ep in eps:
+                if eps.index(ep) == 0:
+                    value = params["private"]
+                    index = [1, '256', '2048']
+                else:
+                    value = params["public"]
+                    index = [2, '2048', '256']
+                l3 = value['outer_l3']
+                srcip4 = l3['srcip4']
+                dstip4 = l3['dstip4']
+                for ip in ixNet.getList(ep, "stack"):
+                    for ipv4 in ixNet.getList(ip, "field"):
+                        if "srcIp" in ipv4:
+                            ixNet.setMultiAttribute(
+                                ipv4,
+                                '-seed', str(index[1]),
+                                '-fixedBits', '{0}'.format(srcip4),
+                                '-randomMask', '0.0.0.255',
+                                '-valueType', 'random',
+                                '-countValue', '{0}'.format(l3['count']))
+                        if "dstIp" in ipv4:
+                            ixNet.setMultiAttribute(
+                                ipv4,
+                                '-seed', str(index[2]),
+                                '-fixedBits', '{0}'.format(dstip4),
+                                '-randomMask', '0.0.0.255',
+                                '-valueType', 'random',
+                                '-countValue', '{0}'.format(l3['count']))
+        ixNet.commit()
