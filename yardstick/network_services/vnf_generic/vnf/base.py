f640c1506f65c50305bf3dfe4c57970e16268099
Deepak S <deepak.s@intel.com>
2017-04-09 07:59:14 -0700

    Adding support for ixload as traffic generator.
    added unittests
    fixed flake8
    added rel_bin_path partial function
    
    Change-Id: I8eac4f726be332323e9340f93973caf6bc005729
    Signed-off-by: Deepak S <deepak.s@intel.com>
    Signed-off-by: Ross Brattain <ross.b.brattain@intel.com>

--- yardstick/network_services/vnf_generic/vnf/base.py
+++ yardstick/network_services/vnf_generic/vnf/base.py
@@ -17,6 +17,8 @@ from __future__ import absolute_import
 import logging
 import ipaddress
 import yaml
+import os
+from functools import partial
 import six
 
 from yardstick.network_services.utils import get_nsb_option
@@ -75,6 +77,8 @@ class GenericVNF(object):
         self.runs_traffic = False
         self.name = "vnf__1"  # name in topology file
         self.bin_path = get_nsb_option("bin_path", "")
+        self.rel_bin_path = partial(os.path.join, self.bin_path)
+
 
     @classmethod
     def _get_kpi_definition(cls, vnfd):
