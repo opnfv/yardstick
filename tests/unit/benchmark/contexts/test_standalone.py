c8679e4109393b18c5c17cc9b92d2bf339b6d011
Neha Vadnere <neha.r.vadnere@intel.com>
2017-04-25 00:17:44 +0530

    Adding new SRIOV Standalone Context
    This patch adds new SRIOV context to run VNFs with
     - random uuid generation,
     - mac address generation,
     - getting dpdk_nic_bind path,
     - ssh key based authentication,
     - printing log messages,
     - added apache2 licence
    
    JIRA: YARDSTICK-480
    
    Change-Id: Ic8317eb9e7e4ecf270091c18be4782d1299ff087
    Signed-off-by: Neha Vadnere <neha.r.vadnere@intel.com>

--- tests/unit/benchmark/contexts/test_standalone.py
+++ tests/unit/benchmark/contexts/test_standalone.py
@@ -20,112 +20,239 @@
 from __future__ import absolute_import
 import os
 import unittest
-
+import mock
 from yardstick.benchmark.contexts import standalone
+from yardstick.benchmark.contexts import sriov
+
+MOCKS = {
+    'yardstick.benchmark.contexts': mock.MagicMock(),
+    'yardstick.benchmark.contexts.sriov': mock.MagicMock(),
+    'yardstick.benchmark.contexts.standalone': mock.MagicMock(),
+}
 
 
 class StandaloneContextTestCase(unittest.TestCase):
 
-    NODES_SAMPLE = "standalone_sample.yaml"
-    NODES_DUPLICATE_SAMPLE = "standalone_duplicate_sample.yaml"
+    NODES_SAMPLE = "nodes_sample_new.yaml"
+    NODES_DUPLICATE_SAMPLE = "nodes_duplicate_sample_new.yaml"
 
     def setUp(self):
         self.test_context = standalone.StandaloneContext()
 
     def test_construct(self):
-
         self.assertIsNone(self.test_context.name)
         self.assertIsNone(self.test_context.file_path)
         self.assertEqual(self.test_context.nodes, [])
         self.assertEqual(self.test_context.nfvi_node, [])
 
     def test_unsuccessful_init(self):
-
         attrs = {
             'name': 'foo',
             'file': self._get_file_abspath("error_file")
         }
-
         self.assertRaises(IOError, self.test_context.init, attrs)
 
     def test_successful_init(self):
-
         attrs = {
-            'name': 'foo',
+            'name': 'sriov',
             'file': self._get_file_abspath(self.NODES_SAMPLE)
         }
 
+        self.test_context.nfvi_node = [{
+            'name': 'sriov',
+            'vf_macs': ['00:00:00:71:7d:25', '00:00:00:71:7d:26'],
+            'ip': '10.123.123.122',
+            'role': 'Sriov',
+            'user': 'root',
+            'images': '/var/lib/libvirt/images/ubuntu1.img',
+            'phy_driver': 'i40e',
+            'password': 'password',
+            'phy_ports': ['0000:06:00.0', '0000:06:00.1']}]
+        self.test_context.get_nfvi_obj = mock.Mock()
         self.test_context.init(attrs)
-
-        self.assertEqual(self.test_context.name, "foo")
+        self.assertEqual(self.test_context.name, "sriov")
         self.assertEqual(len(self.test_context.nodes), 3)
-        self.assertEqual(len(self.test_context.nfvi_node), 1)
-        self.assertEqual(self.test_context.nfvi_node[0]["name"], "node2")
+        self.assertEqual(len(self.test_context.nfvi_node), 2)
+        self.assertEqual(self.test_context.nfvi_node[0]["name"], "sriov")
 
     def test__get_server_with_dic_attr_name(self):
-
         attrs = {
             'name': 'foo',
             'file': self._get_file_abspath(self.NODES_SAMPLE)
         }
-
+        self.test_context.nfvi_node = [{
+            'name': 'sriov',
+            'vf_macs': ['00:00:00:71:7d:25', '00:00:00:71:7d:26'],
+            'ip': '10.123.123.122',
+            'role': 'Sriov',
+            'user': 'root',
+            'images': '/var/lib/libvirt/images/ubuntu1.img',
+            'phy_driver': 'i40e',
+            'password': 'password',
+            'phy_ports': ['0000:06:00.0', '0000:06:00.1']}]
         self.test_context.init(attrs)
-
         attr_name = {'name': 'foo.bar'}
         result = self.test_context._get_server(attr_name)
-
         self.assertEqual(result, None)
 
     def test__get_server_not_found(self):
-
         attrs = {
             'name': 'foo',
             'file': self._get_file_abspath(self.NODES_SAMPLE)
         }
-
+        self.test_context.nfvi_node = [{
+            'name': 'sriov',
+            'vf_macs': ['00:00:00:71:7d:25', '00:00:00:71:7d:26'],
+            'ip': '10.123.123.122',
+            'role': 'Sriov',
+            'user': 'root',
+            'images': '/var/lib/libvirt/images/ubuntu1.img',
+            'phy_driver': 'i40e',
+            'password': 'password',
+            'phy_ports': ['0000:06:00.0', '0000:06:00.1']}]
         self.test_context.init(attrs)
-
         attr_name = 'bar.foo'
         result = self.test_context._get_server(attr_name)
-
         self.assertEqual(result, None)
 
     def test__get_server_duplicate(self):
-
         attrs = {
             'name': 'foo',
             'file': self._get_file_abspath(self.NODES_DUPLICATE_SAMPLE)
         }
-
+        self.test_context.nfvi_node = [{
+            'name': 'sriov',
+            'vf_macs': ['00:00:00:71:7d:25', '00:00:00:71:7d:26'],
+            'ip': '10.123.123.122',
+            'role': 'Sriov',
+            'user': 'root',
+            'images': '/var/lib/libvirt/images/ubuntu1.img',
+            'phy_driver': 'i40e',
+            'password': 'password',
+            'phy_ports': ['0000:06:00.0', '0000:06:00.1']}]
         self.test_context.init(attrs)
-
-        attr_name = 'node2.foo'
-
-        self.assertRaises(ValueError, self.test_context._get_server, attr_name)
+        attr_name = 'sriov.foo'
+        self.assertRaises(
+            ValueError,
+            self.test_context._get_server,
+            attr_name)
 
     def test__get_server_found(self):
+        attrs = {
+            'name': 'sriov',
+            'file': self._get_file_abspath(self.NODES_SAMPLE)
+        }
+        self.test_context.nfvi_node = [{
+            'name': 'sriov',
+            'vf_macs': ['00:00:00:71:7d:25', '00:00:00:71:7d:26'],
+            'ip': '10.123.123.122',
+            'role': 'Sriov',
+            'user': 'root',
+            'images': '/var/lib/libvirt/images/ubuntu1.img',
+            'phy_driver': 'i40e',
+            'password': 'password',
+            'phy_ports': ['0000:06:00.0', '0000:06:00.1']}]
+        self.test_context.init(attrs)
+        attr_name = 'sriov.sriov'
+        result = self.test_context._get_server(attr_name)
+        self.assertEqual(result['ip'], '10.123.123.122')
+        self.assertEqual(result['name'], 'sriov.sriov')
+        self.assertEqual(result['user'], 'root')
 
+    def test_deploy(self):
         attrs = {
             'name': 'foo',
             'file': self._get_file_abspath(self.NODES_SAMPLE)
         }
 
+        self.test_context.nfvi_node = [{
+            'name': 'sriov',
+            'vf_macs': ['00:00:00:71:7d:25', '00:00:00:71:7d:26'],
+            'ip': '10.123.123.122',
+            'role': 'Sriov',
+            'user': 'root',
+            'images': '/var/lib/libvirt/images/ubuntu1.img',
+            'phy_driver': 'i40e',
+            'password': 'password',
+            'phy_ports': ['0000:06:00.0', '0000:06:00.1']}]
+        self.test_context.get_nfvi_obj = mock.MagicMock()
         self.test_context.init(attrs)
-
-        attr_name = 'node1.foo'
-        result = self.test_context._get_server(attr_name)
-
-        self.assertEqual(result['ip'], '1.1.1.1')
-        self.assertEqual(result['name'], 'node1.foo')
-        self.assertEqual(result['user'], 'root')
-
-    def test_deploy(self):
+        self.test_context.nfvi_obj.ssh_remote_machine = mock.Mock()
+        self.test_context.nfvi_obj.first_run = True
+        self.test_context.nfvi_obj.get_nic_details = mock.Mock()
+        PORTS = ['0000:06:00.0', '0000:06:00.1']
+        NIC_DETAILS = {
+            'interface': {0: 'enp6s0f0', 1: 'enp6s0f1'},
+            'vf_macs': ['00:00:00:71:7d:25', '00:00:00:71:7d:26'],
+            'pci': ['0000:06:00.0', '0000:06:00.1'],
+            'phy_driver': 'i40e'}
+        DRIVER = 'i40e'
+        result = self.test_context.nfvi_obj.setup_sriov_context(
+            PORTS,
+            NIC_DETAILS,
+            DRIVER)
+        print("{0}".format(result))
         self.assertIsNone(self.test_context.deploy())
 
     def test_undeploy(self):
+        attrs = {
+            'name': 'foo',
+            'file': self._get_file_abspath(self.NODES_SAMPLE)
+        }
+        self.test_context.nfvi_node = [{
+            'name': 'sriov',
+            'vf_macs': ['00:00:00:71:7d:25', '00:00:00:71:7d:26'],
+            'ip': '10.123.123.122',
+            'role': 'Sriov',
+            'user': 'root',
+            'images': '/var/lib/libvirt/images/ubuntu1.img',
+            'phy_driver': 'i40e',
+            'password': 'password',
+            'phy_ports': ['0000:06:00.0', '0000:06:00.1']}]
+        self.test_context.get_nfvi_obj = mock.MagicMock()
+        self.test_context.init(attrs)
+        self.test_context.nfvi_obj.destroy_vm = mock.Mock()
         self.assertIsNone(self.test_context.undeploy())
+
+    def test_get_nfvi_obj(self):
+        with mock.patch('yardstick.benchmark.contexts.sriov'):
+            attrs = {
+                'name': 'sriov',
+                'file': self._get_file_abspath(self.NODES_SAMPLE)
+            }
+            self.test_context.init(attrs)
+            self.test_context.nfvi_obj.file_path = self._get_file_abspath(
+                self.NODES_SAMPLE)
+            self.test_context.nfvi_node = [{
+                'name': 'sriov',
+                'vf_macs': ['00:00:00:71:7d:25', '00:00:00:71:7d:26'],
+                'ip': '10.123.123.122',
+                'role': 'Sriov',
+                'user': 'root',
+                'images': '/var/lib/libvirt/images/ubuntu1.img',
+                'phy_driver': 'i40e',
+                'password': 'password',
+                'phy_ports': ['0000:06:00.0', '0000:06:00.1']}]
+            self.test_context.get_nfvi_obj = mock.MagicMock()
+            self.test_context.init(attrs)
+            self.test_context.get_context_impl = mock.Mock(
+                return_value=sriov.Sriov)
+            self.assertIsNotNone(self.test_context.get_nfvi_obj())
+
+    def test_get_context_impl_correct_obj(self):
+        with mock.patch.dict("sys.modules", MOCKS):
+            self.assertIsNotNone(self.test_context.get_context_impl('Sriov'))
+
+    def test_get_context_impl_wrong_obj(self):
+        with mock.patch.dict("sys.modules", MOCKS):
+            self.assertRaises(
+                ValueError,
+                lambda: self.test_context.get_context_impl('wrong_object'))
 
     def _get_file_abspath(self, filename):
         curr_path = os.path.dirname(os.path.abspath(__file__))
         file_path = os.path.join(curr_path, filename)
         return file_path
+
+if __name__ == '__main__':
+    unittest.main()
