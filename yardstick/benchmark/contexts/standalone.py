ccee2af95233d4499453ca6a8d38ee9c1010ba84
Neha Vadnere <neha.r.vadnere@intel.com>
2017-04-25 00:17:44 +0530

    DRAFT: Setup SRIOV Standalone Context - with random uuid generation, mac address generation, getting dpdk_nic_bind path, ssh key based authentication, printing log messages, added apache2 licence Change-Id: Ic8317eb9e7e4ecf270091c18be4782d1299ff087 Signed-off-by: Neha Vadnere <neha.r.vadnere@intel.com>

--- yardstick/benchmark/contexts/standalone.py
+++ yardstick/benchmark/contexts/standalone.py
@@ -21,6 +21,7 @@ import yaml
 
 from yardstick.benchmark.contexts.base import Context
 from yardstick.common.constants import YARDSTICK_ROOT_PATH
+from yardstick.common.utils import import_modules_from_package, itersubclasses
 
 LOG = logging.getLogger(__name__)
 
@@ -37,7 +38,8 @@ class StandaloneContext(Context):
         self.file_path = None
         self.nodes = []
         self.nfvi_node = []
-        super(StandaloneContext, self).__init__()
+        self.nfvi_obj = None
+        super(self.__class__, self).__init__()
 
     def read_config_file(self):
         """Read from config file"""
@@ -46,6 +48,14 @@ class StandaloneContext(Context):
             LOG.info("Parsing pod file: %s", self.file_path)
             cfg = yaml.load(stream)
         return cfg
+
+    def get_nfvi_obj(self):
+        print("{0}".format(self.nfvi_node[0]['role']))
+        context_type = self.get_context_impl(self.nfvi_node[0]['role'])
+        nfvi_obj = context_type()
+        nfvi_obj.__init__()
+        nfvi_obj.parse_pod_and_get_data(self.file_path)
+        return nfvi_obj
 
     def init(self, attrs):
         """initializes itself from the supplied arguments"""
@@ -64,8 +74,15 @@ class StandaloneContext(Context):
                 raise
 
         self.nodes.extend(cfg["nodes"])
-        self.nfvi_node.extend([node for node in cfg["nodes"]
-                               if node["role"] == "nfvi_node"])
+        for node in cfg["nodes"]:
+            if str(node["role"]) == "Sriov":
+                self.nfvi_node.extend([node for node in cfg["nodes"]
+                                       if str(node["role"]) == "Sriov"])
+            if str(node["role"]) == "ovs-dpdk":
+                LOG.info("{0}".format(node["role"]))
+            else:
+                LOG.debug("Node role is other than SRIOV and OVS")
+        self.nfvi_obj = self.get_nfvi_obj()
         LOG.debug("Nodes: %r", self.nodes)
         LOG.debug("NFVi Node: %r", self.nfvi_node)
 
@@ -73,13 +90,26 @@ class StandaloneContext(Context):
         """don't need to deploy"""
 
         # Todo: NFVi deploy (sriov, vswitch, ovs etc) based on the config.
+        self.nfvi_obj.ssh_remote_machine()
+        if self.nfvi_obj.first_run is True:
+            self.nfvi_obj.install_req_libs()
+
+        nic_details = self.nfvi_obj.get_nic_details()
+        print("{0}".format(nic_details))
+        self.nfvi_obj.setup_sriov_context(
+            self.nfvi_obj.sriov[0]['phy_ports'],
+            nic_details,
+            self.nfvi_obj.sriov[0]['phy_driver'])
         pass
 
     def undeploy(self):
         """don't need to undeploy"""
 
         # Todo: NFVi undeploy (sriov, vswitch, ovs etc) based on the config.
-        super(StandaloneContext, self).undeploy()
+        # self.nfvi_obj = self.get_nfvi_obj()
+        self.nfvi_obj.ssh_remote_machine()
+        self.nfvi_obj.destroy_vm()
+        pass
 
     def _get_server(self, attr_name):
         """lookup server info by name from context
@@ -114,3 +144,19 @@ class StandaloneContext(Context):
 
         node["name"] = attr_name
         return node
+
+    def get_context_impl(self, nfvi_type):
+        """ Find the implementing class from vnf_model["vnf"]["name"] field
+
+        :param vnf_model: dictionary containing a parsed vnfd
+        :return: subclass of GenericVNF
+        """
+        import_modules_from_package(
+            "yardstick.benchmark.contexts")
+        expected_name = nfvi_type
+        impl = [c for c in itersubclasses(StandaloneContext)
+                if c.__name__ == expected_name]
+        try:
+            return next(iter(impl))
+        except StopIteration:
+            raise ValueError("No implementation for %s", expected_name)
