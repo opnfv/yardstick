089555637193fcb97328f221da2ab3433d1da990
tjuyinkanglin <14_ykl@tongji.edu.cn>
2017-05-04 15:58:55 +0800

    Bugfix: Local Openstack Operation in HA test frameworks
    JIRA: YARDSTICK-635
    
    Change-Id: Ic27517714db9325e7a3b1ef623c49af61c36b2b5
    Signed-off-by: tjuyinkanglin <14_ykl@tongji.edu.cn>

--- yardstick/benchmark/scenarios/availability/operation/operation_general.py
+++ yardstick/benchmark/scenarios/availability/operation/operation_general.py
@@ -14,7 +14,8 @@ from yardstick.benchmark.scenarios.availability.operation.baseoperation \
     BaseOperation
 
 import yardstick.ssh as ssh
-from yardstick.benchmark.scenarios.availability.util import buildshellparams
+from yardstick.benchmark.scenarios.availability.util \
+    import buildshellparams, execute_shell_command
 
 LOG = logging.getLogger(__name__)
 
@@ -25,24 +26,29 @@ class GeneralOperaion(BaseOperation):
 
     def setup(self):
         LOG.debug("config:%s context:%s", self._config, self._context)
-        host = self._context.get(self._config['host'], None)
+        host = self._context.get(self._config.get('host', None), None)
 
-        self.connection = ssh.SSH.from_node(host, defaults={"user": "root"})
-        self.connection.wait(timeout=600)
-        LOG.debug("ssh host success!")
+        self.connection = None
+        if host:
+            self.connection = ssh.SSH.from_node(
+                host, defaults={"user": "root"})
+            self.connection.wait(timeout=600)
+            LOG.debug("ssh host success!")
 
         self.key = self._config['key']
         self.operation_key = self._config['operation_key']
 
         if "action_parameter" in self._config:
             actionParameter = self._config['action_parameter']
-            str = buildshellparams(actionParameter)
+            str = buildshellparams(
+                actionParameter, True if self.connection else False)
             l = list(item for item in actionParameter.values())
             self.action_param = str.format(*l)
 
         if "rollback_parameter" in self._config:
             rollbackParameter = self._config['rollback_parameter']
-            str = buildshellparams(rollbackParameter)
+            str = buildshellparams(
+                rollbackParameter, True if self.connection else False)
             l = list(item for item in rollbackParameter.values())
             self.rollback_param = str.format(*l)
 
@@ -55,15 +61,25 @@ class GeneralOperaion(BaseOperation):
 
     def run(self):
         if "action_parameter" in self._config:
-            with open(self.action_script, "r") as stdin_file:
-                exit_status, stdout, stderr = self.connection.execute(
-                    self.action_param,
-                    stdin=stdin_file)
+            if self.connection:
+                with open(self.action_script, "r") as stdin_file:
+                    exit_status, stdout, stderr = self.connection.execute(
+                        self.action_param,
+                        stdin=stdin_file)
+            else:
+                exit_status, stdout = \
+                    execute_shell_command(
+                        "/bin/bash {0} {1}".format(
+                            self.action_script, self.action_param))
         else:
-            with open(self.action_script, "r") as stdin_file:
-                exit_status, stdout, stderr = self.connection.execute(
-                    "/bin/sh -s ",
-                    stdin=stdin_file)
+            if self.connection:
+                with open(self.action_script, "r") as stdin_file:
+                    exit_status, stdout, stderr = self.connection.execute(
+                        "/bin/sh -s ",
+                        stdin=stdin_file)
+            else:
+                exit_status, stdout = execute_shell_command(
+                    "/bin/bash {0}".format(self.action_script))
 
         if exit_status == 0:
             LOG.debug("success,the operation's output is: %s", stdout)
@@ -74,12 +90,22 @@ class GeneralOperaion(BaseOperation):
 
     def rollback(self):
         if "rollback_parameter" in self._config:
-            with open(self.rollback_script, "r") as stdin_file:
-                exit_status, stdout, stderr = self.connection.execute(
-                    self.rollback_param,
-                    stdin=stdin_file)
+            if self.connection:
+                with open(self.rollback_script, "r") as stdin_file:
+                    exit_status, stdout, stderr = self.connection.execute(
+                        self.rollback_param,
+                        stdin=stdin_file)
+            else:
+                exit_status, stdout = \
+                    execute_shell_command(
+                        "/bin/bash {0} {1}".format(
+                            self.rollback_script, self.rollback_param))
         else:
-            with open(self.rollback_script, "r") as stdin_file:
-                exit_status, stdout, stderr = self.connection.execute(
-                    "/bin/sh -s ",
-                    stdin=stdin_file)
+            if self.connection:
+                with open(self.rollback_script, "r") as stdin_file:
+                    exit_status, stdout, stderr = self.connection.execute(
+                        "/bin/sh -s ",
+                        stdin=stdin_file)
+            else:
+                exit_status, stdout = execute_shell_command(
+                    "/bin/bash {0}".format(self.rollback_script))
