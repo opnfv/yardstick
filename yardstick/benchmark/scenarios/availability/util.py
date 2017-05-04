3c22db0d358a6c7c0e769b9556db890f05ab488d
tjuyinkanglin <14_ykl@tongji.edu.cn>
2017-05-04 15:58:55 +0800

    Bugfix: Local Openstack Operation in HA test frameworks
    JIRA: YARDSTICK-635
    
    Change-Id: Ic27517714db9325e7a3b1ef623c49af61c36b2b5
    Signed-off-by: tjuyinkanglin <14_ykl@tongji.edu.cn>

--- yardstick/benchmark/scenarios/availability/util.py
+++ yardstick/benchmark/scenarios/availability/util.py
@@ -6,14 +6,33 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+import logging
+import subprocess
+import traceback
 
+LOG = logging.getLogger(__name__)
 
-def buildshellparams(param):
+def buildshellparams(param, remote=True):
     i = 0
     values = []
-    result = '/bin/bash -s'
+    result = '/bin/bash -s' if remote else ''
     for key in param.keys():
         values.append(param[key])
         result += " {%d}" % i
         i = i + 1
     return result
+
+def execute_shell_command(command):
+    """execute shell script with error handling"""
+    exitcode = 0
+    output = []
+    try:
+        LOG.debug("the command is: %s", command)
+        output = subprocess.check_output(command, shell=True)
+    except Exception:
+        exitcode = -1
+        output = traceback.format_exc()
+        LOG.error("exec command '%s' error:\n ", command)
+        LOG.error(traceback.format_exc())
+
+    return exitcode, output
