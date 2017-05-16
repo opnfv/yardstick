4c260075a061fd27230fb41d727c5a642aeddb9c
Maciej Skrocki <maciej.skrocki@intel.com>
2017-05-16 13:58:41 -0700

    DRAFT: Updated Dockerfile for NSB
    - included TRex client libraries in Dockerfile
    - refactored TRex libraries imports
    
    Change-Id: I0949398f1e82d47aba6d226b4eef4c3404b2dbe6
    Signed-off-by: Maciej Skrocki <maciej.skrocki@intel.com>

--- yardstick/network_services/vnf_generic/vnf/tg_rfc2544_trex.py
+++ yardstick/network_services/vnf_generic/vnf/tg_rfc2544_trex.py
@@ -24,9 +24,9 @@ import yaml
 from yardstick import ssh
 from yardstick.network_services.vnf_generic.vnf.base import GenericTrafficGen
 from yardstick.network_services.utils import get_nsb_option
-from stl.trex_stl_lib.trex_stl_client import STLClient
-from stl.trex_stl_lib.trex_stl_client import LoggerApi
-from stl.trex_stl_lib.trex_stl_exceptions import STLError
+from trex_stl_lib.trex_stl_client import STLClient
+from trex_stl_lib.trex_stl_client import LoggerApi
+from trex_stl_lib.trex_stl_exceptions import STLError
 
 LOGGING = logging.getLogger(__name__)
 
