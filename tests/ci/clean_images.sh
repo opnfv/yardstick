81b1bab1a26c2b67060f692d1b1671114a1afd23
Alexandru Nemes <alexandru.nemes@enea.com>
2017-05-16 15:50:27 +0300

    Yardstick: Aarch64 jenkins slave support
    Change-Id: I2d58c38d75fc1fe036769340e9fb02932f543510
    Signed-off-by: Alexandru Nemes <alexandru.nemes@enea.com>

--- tests/ci/clean_images.sh
+++ tests/ci/clean_images.sh
@@ -19,7 +19,7 @@ cleanup()
         return
     fi
 
-    for image in $(openstack image list | grep -e cirros-0.3.5 -e yardstick-image -e Ubuntu-16.04 \
+    for image in $(openstack image list | grep -e cirros-0.3.5 -e cirros-d161201 -e yardstick-image -e Ubuntu-16.04 \
         | awk '{print $2}'); do
         echo "Deleting image $image..."
         openstack image delete $image || true
