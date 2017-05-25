a0c9e3066046fc369870dbffe67a0613d661cf80
qiujuan <juan_qiu@tongji.edu.cn>
2017-05-25 13:18:28 +0800

    Add intermediate variable for HA test
    JIRA: YARDSTICK-397
    
    Change-Id: I3489893caa5b8194b63cb844325ec0b2c554aecc
    Signed-off-by: qiujuan <juan_qiu@tongji.edu.cn>

--- tests/unit/benchmark/scenarios/availability/test_util.py
+++ tests/unit/benchmark/scenarios/availability/test_util.py
@@ -19,6 +19,25 @@ from yardstick.benchmark.scenarios.availability import util
 @mock.patch('yardstick.benchmark.scenarios.availability.util.subprocess')
 class ExecuteShellTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.param_config = {'serviceName': '$serviceName', 'value': 1}
+        self.intermediate_variables = {'$serviceName': 'nova-api'}
+        self.std_output = '| id       | 1                     |'
+        self.cmd_config = {'cmd':'ls','param':'-a'}
+
+    def test_util_build_command_shell(self,mock_subprocess):
+        result = util.build_shell_command(self.param_config, True,
+                                          self.intermediate_variables)
+        self.assertEqual("nova-api" in result, True)
+
+    def test_read_stdout_item(self,mock_subprocess):
+        result = util.read_stdout_item(self.std_output,'id')
+        self.assertEquals('1',result)
+
+    def test_buildshellparams(self,mock_subprocess):
+        result = util.buildshellparams(self.cmd_config,True)
+        self.assertEquals('/bin/bash -s {0} {1}', result)
+
     def test__fun_execute_shell_command_successful(self, mock_subprocess):
         cmd = "env"
         mock_subprocess.check_output.return_value = (0, 'unittest')
