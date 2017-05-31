16bb1589db614a6dfefbfa22de866043d6cf98c4
Deepak S <deepak.s@linux.intel.com>
2017-05-31 03:58:34 +0000

    Adding _get_context_from_server to get context info from server
    JIRA: YARDSTICK-672
    
    For some usecases, We might want use context info setting
    hardware specific paramters. Adding the context info to
    scenario config. This will specifically help in cases like scale-out
    and scale-up.
    
    Change-Id: I7760fb7c7513a59e4f0c9c981899a637f06e4a29
    Signed-off-by: Deepak S <deepak.s@linux.intel.com>
    Signed-off-by: Edward MacGillivray <edward.s.macgillivray@intel.com>

--- yardstick/benchmark/contexts/base.py
+++ yardstick/benchmark/contexts/base.py
@@ -1,5 +1,5 @@
 ##############################################################################
-# Copyright (c) 2015 Huawei Technologies Co.,Ltd and others.
+# Copyright (c) 2015-2017 Huawei Technologies Co.,Ltd and others.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
@@ -17,6 +17,15 @@ import yardstick.common.utils as utils
 class Context(object):
     """Class that represents a context in the logical model"""
     list = []
+
+    @staticmethod
+    def split_name(name, sep='.'):
+        try:
+            name_iter = iter(name.split(sep))
+        except AttributeError:
+            # name not a string
+            return None, None
+        return next(name_iter), next(name_iter, None)
 
     def __init__(self):
         Context.list.append(self)
@@ -56,20 +65,33 @@ class Context(object):
         """get server info by name from context
         """
 
+    @abc.abstractmethod
+    def _get_context_from_server(self, attr_name):
+        """get context info by name for given node name
+        """
+
     @staticmethod
     def get_server(attr_name):
         """lookup server info by name from context
         attr_name: either a name for a server created by yardstick or a dict
         with attribute name mapping when using external heat templates
         """
-        server = None
         for context in Context.list:
             server = context._get_server(attr_name)
             if server is not None:
-                break
+                return server
 
-        if server is None:
-            raise ValueError("context not found for server '%r'" %
-                             attr_name)
+        raise ValueError("context not found for server '%r'" % attr_name)
 
-        return server
+    @staticmethod
+    def get_context_from_server(attr_name):
+        """lookup context info by name from node config
+        attr_name: either a name of the node created by yardstick or a dict
+        with attribute name mapping when using external templates
+        """
+        for context in Context.list:
+            context_attrs = context._get_context_from_server(attr_name)
+            if context_attrs is not None:
+                return context_attrs
+
+        raise ValueError("context not found for name '%r'" % attr_name)
