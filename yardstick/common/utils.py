97004997c00dac2e0dcfeef303af4701e78bb909
Ross Brattain <ross.b.brattain@intel.com>
2017-06-21 19:25:22 -0700

    constants: cache YAML config values
    don't reparse the yardstick.yaml file each
    time we lookup an option.
    
    Since it is global data, just cache it in a global
    var
    
    Use pkg_resources.resource_filename
    to lookup the path of yardstick tests, intsead of using __file__
    
    pkg_resources is slightly more proper than __file__
    at least for packages
    
    Change-Id: I05d9748390a37bd45c53013fc084d23069ab7c51
    Signed-off-by: Ross Brattain <ross.b.brattain@intel.com>

--- yardstick/common/utils.py
+++ yardstick/common/utils.py
@@ -27,7 +27,6 @@ import collections
 import socket
 import random
 import ipaddress
-from functools import reduce
 from contextlib import closing
 
 import yaml
@@ -105,19 +104,6 @@ def parse_yaml(file_path):
             raise
     else:
         return value
-
-
-def get_param(key, default=''):
-
-    conf_file = os.environ.get('CONF_FILE', '/etc/yardstick/yardstick.yaml')
-
-    conf = parse_yaml(conf_file)
-    try:
-        return reduce(lambda a, b: a[b], key.split('.'), conf)
-    except KeyError:
-        if not default:
-            raise
-        return default
 
 
 def makedirs(d):
