38a52d93397fce50b47c7f280656fded1cad97b9
RajithaY <rajithax.yerrumsetty@intel.com>
2017-06-23 16:00:23 +0500

    Testing live migration using qemu This patch includes the scripts to execute the live migration using qemu and provide the information total time ,VM downtime information once live migration is completed
    Change-Id: Id5a57ddd1ec8ee79b1063f1fc63f94caafdf37ed
    Signed-off-by:RajithaY<rajithax.yerrumsetty@intel.com>

---
+++ yardstick/benchmark/scenarios/compute/qemu_migrate_benchmark.bash
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#############################################################################
+#Copyright (c) 2015 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+set -e
+
+# Commandline arguments
+
+src=$2
+dst_ip=$4
+migrate_to_port=$5
+max_down_time=$6
+
+OUTPUT_FILE=/tmp/output_qemu.log
+
+do_migrate()
+{
+#       local src=`echo $OPTIONS | cut -d ':' -f 2 | cut -d ',' -f 1`
+        echo "info status" | nc -U $src
+        # with no speed limit
+        echo "migrate_set_speed 0" |nc -U $src
+        # set the expected max downtime
+        echo "migrate_set_downtime ${max_down_time}" |nc -U $src
+        # start live migration
+        echo "migrate -d tcp:${dst_ip}:$migrate_to_port" |nc -U $src
+        # wait until live migration completed
+        status=""
+        while [  "${status}" == ""  ]
+        do
+                status=`echo "info migrate" | nc -U $src |grep completed | cut -d: -f2`
+                echo ${status}
+                sleep 1;
+        done
+} >/dev/null
+
+output_qemu()
+{
+        # print detail information
+        echo "info migrate" | nc -U $src
+        echo "quit" | nc -U $src
+        sleep 5
+
+} > $OUTPUT_FILE
+
+output_json()
+{
+total_time=`cat $OUTPUT_FILE |grep "total time" | cut -d' ' -f3`
+downtime=`cat $OUTPUT_FILE |grep "downtime" | cut -d' ' -f2`
+setuptime=`cat $OUTPUT_FILE|grep "setup" | cut -d' ' -f2`
+echo -e "{ \
+        \"total_time\":\"$total_time\", \
+        \"down_time\":\"$downtime\" \
+        \"setuptime\":\"$setuptime\", \
+         }"
+}
+# main entry
+main()
+{
+    do_migrate
+}
+main
