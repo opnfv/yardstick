b04755b5148daf36db436dc066e1bef2c93f1879
JingLu5 <lvjing5@huawei.com>
2017-06-26 03:18:56 +0000

    Bugfix: remove double quote around SECURE variable
    JIRA: YARDSTICK-689
    
    This patch remove the double quote around the SECURE varibale as it may cause the fowllowing error:
    $ openstack "${SECURE}" image list
    openstack: ' image list' is not an openstack command. See 'openstack --help'.
    Did you mean one of these?
      access token create
      address scope create
      address scope delete
      address scope list
      address scope set
      address scope show
      aggregate add host
      aggregate create
      ...
    
    Change-Id: Ibb22e85b3dd89b0e3b62821bd5ebe4f155886ffc
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

--- tests/ci/clean_images.sh
+++ tests/ci/clean_images.sh
@@ -21,18 +21,18 @@ cleanup()
         SECURE=""
     fi
 
-    if ! openstack "${SECURE}" image list; then
+    if ! openstack ${SECURE} image list; then
         return
     fi
 
-    for image in $(openstack "${SECURE}" image list | grep -e cirros-0.3.5 -e yardstick-image -e Ubuntu-16.04 \
+    for image in $(openstack ${SECURE} image list | grep -e cirros-0.3.5 -e yardstick-image -e Ubuntu-16.04 \
         | awk '{print $2}'); do
         echo "Deleting image $image..."
-        openstack "${SECURE}" image delete $image || true
+        openstack ${SECURE} image delete $image || true
     done
 
-    openstack "${SECURE}" flavor delete yardstick-flavor &> /dev/null || true
-    openstack "${SECURE}" flavor delete storperf &> /dev/null || true
+    openstack ${SECURE} flavor delete yardstick-flavor &> /dev/null || true
+    openstack ${SECURE} flavor delete storperf &> /dev/null || true
 }
 
 main()
