04ccaf677761c3268571c3d457589de6c7720d0d
chenjiankun <chenjiankun1@huawei.com>
2017-06-27 03:20:08 +0000

    Kubernetes (k8s) support
    JIRA: YARDSTICK-682
    
    We decide to support k8s in E release.
    We need to discuss with openretriver team and then rewrite the ping
    test case under k8s as the first step.
    
    Change-Id: I3f81ebca8de5c1f3a8b7d42581cd7342dc320239
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

---
+++ yardstick/common/kubernetes_utils.py
@@ -0,0 +1,122 @@
+import logging
+
+from kubernetes import client
+from kubernetes import config
+from kubernetes.client.rest import ApiException
+
+from yardstick.common import constants as consts
+
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.DEBUG)
+
+config.load_kube_config(config_file=consts.K8S_CONF_FILE)
+core_v1_api = client.CoreV1Api()
+
+
+def create_replication_controller(template,
+                                  namespace='default',
+                                  wait=False,
+                                  **kwargs):    # pragma: no cover
+
+    try:
+        core_v1_api.create_namespaced_replication_controller(namespace,
+                                                             template,
+                                                             **kwargs)
+    except ApiException:
+        LOG.exception('Create replication controller failed')
+        raise
+
+
+def delete_replication_controller(name,
+                                  namespace='default',
+                                  wait=False,
+                                  **kwargs):    # pragma: no cover
+
+    body = kwargs.get('body', client.V1DeleteOptions())
+    delete_key_from_dict(kwargs, 'body')
+    try:
+        core_v1_api.delete_namespaced_replication_controller(name,
+                                                             namespace,
+                                                             body,
+                                                             **kwargs)
+    except ApiException:
+        LOG.exception('Delete replication controller failed')
+        raise
+
+
+def delete_pod(name,
+               namespace='default',
+               wait=False,
+               **kwargs):    # pragma: no cover
+
+    body = kwargs.get('body', client.V1DeleteOptions())
+    delete_key_from_dict(kwargs, 'body')
+    try:
+        core_v1_api.delete_namespaced_pod(name,
+                                          namespace,
+                                          body,
+                                          **kwargs)
+    except ApiException:
+        LOG.exception('Delete pod failed')
+        raise
+
+
+def read_pod(name,
+             namespace='default',
+             **kwargs):  # pragma: no cover
+    try:
+        resp = core_v1_api.read_namespaced_pod(name, namespace, **kwargs)
+    except ApiException:
+        LOG.exception('Read pod failed')
+        raise
+    else:
+        return resp
+
+
+def read_pod_status(name, namespace='default', **kwargs):   # pragma: no cover
+    return read_pod(name).status.phase
+
+
+def create_config_map(name,
+                      data,
+                      namespace='default',
+                      wait=False,
+                      **kwargs):   # pragma: no cover
+    metadata = client.V1ObjectMeta(name=name)
+    body = client.V1ConfigMap(data=data, metadata=metadata)
+    try:
+        core_v1_api.create_namespaced_config_map(namespace, body, **kwargs)
+    except ApiException:
+        LOG.exception('Create config map failed')
+        raise
+
+
+def delete_config_map(name,
+                      namespace='default',
+                      wait=False,
+                      **kwargs):     # pragma: no cover
+    body = kwargs.get('body', client.V1DeleteOptions())
+    delete_key_from_dict(kwargs, 'body')
+    try:
+        core_v1_api.delete_namespaced_config_map(name,
+                                                 namespace,
+                                                 body,
+                                                 **kwargs)
+    except ApiException:
+        LOG.exception('Delete config map failed')
+        raise
+
+
+def get_pod_list(namespace='default'):      # pragma: no cover
+    try:
+        return core_v1_api.list_namespaced_pod(namespace=namespace)
+    except ApiException:
+        LOG.exception('Get pod list failed')
+        raise
+
+
+def delete_key_from_dict(dic, key):
+    try:
+        dic.pop(key)
+    except KeyError:
+        pass
