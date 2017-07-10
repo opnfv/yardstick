f456d48971142f0c105142388b2465ee94d91a66
JingLu5 <lvjing5@huawei.com>
2017-07-10 04:19:08 +0000

    Change storperf target IP
    As compass in master use OpenStack Ansible, the former IP "192.168.200.1" for
    Huawei pod-1 has been deprecated.
    
    Change-Id: Ia60885b36abec3455db2aa2939afbe2ef8bd79f5
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

--- tests/ci/scp_storperf_files.sh
+++ tests/ci/scp_storperf_files.sh
@@ -13,8 +13,8 @@
 
 ssh_options="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 sshpass -p root scp 2>/dev/null $ssh_options ~/storperf_admin-rc \
-        root@192.168.200.1:/root/ &> /dev/null
+        root@192.168.10.6:/root/ &> /dev/null
 sshpass -p root scp 2>/dev/null $ssh_options /home/opnfv/repos/storperf/docker-compose/docker-compose.yaml \
-        root@192.168.200.1:/root/ &> /dev/null
+        root@192.168.10.6:/root/ &> /dev/null
 sshpass -p root scp 2>/dev/null $ssh_options /home/opnfv/repos/storperf/docker-compose/nginx.conf \
-        root@192.168.200.1:/root/ &> /dev/null
+        root@192.168.10.6:/root/ &> /dev/null
