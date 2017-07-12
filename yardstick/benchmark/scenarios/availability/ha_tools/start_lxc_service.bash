6a1da9722082fca07d818f2994df31a8e64cbe55
JingLu5 <lvjing5@huawei.com>
2017-07-12 07:35:30 +0000

    Draft: HA testcase containerized Compass support
    Compass will be containerized in Euphrates release.
    This work is about support HA testcase in a containerized environment.
    
    Change-Id: Ibb668c81f046d6400293f8cd58181b9f20aeebe3
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

---
+++ yardstick/benchmark/scenarios/availability/ha_tools/start_lxc_service.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright (c) 2015 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# Start a service and check the service is started
+
+set -e
+
+service_name=$1
+
+lxc_filter=$(echo ${service_name} | sed 's/-/_/g')
+
+container=$(lxc-ls -1 --filter=${lxc_filter})
+
+Distributor=$(lxc-attach -n ${container} -- lsb_release -a | grep "Distributor ID" | awk '{print $3}')
+
+if [ "$Distributor" != "Ubuntu" -a "$service_name" != "keystone" -a "$service_name" != "neutron-server" -a "$service_name" != "haproxy" ]; then
+    service_name="openstack-"${service_name}
+elif [ "$Distributor" = "Ubuntu" -a "$service_name" = "keystone" ]; then
+    service_name="apache2"
+elif [ "$service_name" = "keystone" ]; then
+    service_name="httpd"
+fi
+
+if lxc-attach -n ${container} -- which systemctl 2>/dev/null; then
+    lxc-attach -n ${container} -- systemctl start $service_name
+else
+    lxc-attach -n ${container} -- service $service_name start
+fi
