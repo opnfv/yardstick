f86d20f9c83a43e087d9452a56d95ddf5d371997
JingLu5 <lvjing5@huawei.com>
2017-07-12 07:35:30 +0000

    Draft: HA testcase containerized Compass support
    Compass will be containerized in Euphrates release.
    This work is about support HA testcase in a containerized environment.
    
    Change-Id: Ibb668c81f046d6400293f8cd58181b9f20aeebe3
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

---
+++ yardstick/benchmark/scenarios/availability/ha_tools/fault_lxc_process_kill.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright (c) 2015 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# Stop process by process name
+
+set -e
+
+process_name=$1
+
+lxc_filter=${process_name//-/_}
+
+if [ "${lxc_filter}" = "glance_api" ]; then
+    lxc_filter="glance"
+fi
+
+container=$(lxc-ls -1 --filter="${lxc_filter}")
+
+if [ "${process_name}" = "haproxy" ]; then
+    for pid in $(pgrep -cf "/usr/.*/${process_name}");
+        do
+            kill -9 "${pid}"
+        done
+elif [ "${process_name}" = "keystone" ]; then
+    pids=$(lxc-attach -n "${container}" -- ps aux | grep "keystone" | grep -iv heartbeat | grep -iv monitor | grep -v grep | grep -v /bin/sh | awk '{print $2}')
+    for pid in "${pids}";
+        do
+            lxc-attach -n "${container}" -- kill -9 "${pid}"
+        done
+else
+    pids=$(lxc-attach -n "${container}" -- pgrep -f "/openstack/.*/${process_name}")
+    for pid in "${pids}";
+        do
+            lxc-attach -n "${container}" -- kill -9 "${pid}"
+        done
+fi
