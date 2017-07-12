c07beb263c01a4a3b4bdf572b28b82b15d111d5c
JingLu5 <lvjing5@huawei.com>
2017-07-12 07:35:30 +0000

    Draft: HA testcase containerized Compass support
    Compass will be containerized in Euphrates release.
    This work is about support HA testcase in a containerized environment.
    
    Change-Id: Ibb668c81f046d6400293f8cd58181b9f20aeebe3
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

---
+++ yardstick/benchmark/scenarios/availability/ha_tools/check_lxc_process_python.bash
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+##############################################################################
+# Copyright (c) 2015 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# check the status of a service
+
+set -e
+
+process_name=$1
+
+lxc_filter=$(echo "${process_name}" | sed 's/-/_/g')
+
+if [ "${lxc_filter}" = "glance_api" ]; then
+    lxc_filter="glance"
+elif [ "${lxc_filter}" = "nova_api" ]; then
+    nova_api_process_name1="nova-api-os-compute"
+    nova_api_process_name2="nova-api-metadata"
+fi
+
+container=$(lxc-ls -1 --filter="${lxc_filter}")
+
+if [ "${process_name}" = "haproxy" ]; then
+    ps aux | grep -e "/usr/.*/$process_name" | grep -v grep | grep -cv /bin/sh
+else
+    lxc-attach -n "${container}" -- ps aux | grep -e "${process_name}" | grep -v grep | grep -cv /bin/sh
+fi
