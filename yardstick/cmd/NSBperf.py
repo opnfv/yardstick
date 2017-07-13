1aaabe3107f57e48df742291eef3c9fd9e758b50
Ross Brattain <ross.b.brattain@intel.com>
2017-07-12 18:04:03 -0700

    NSBperf: fix bad sigint handler
    This is why we don't do things at module
    import time.
    
    when we import this module it makes a bad
    sigint handler.  Only load signal handlers
    at runtime.
    
    Also fix handler to take *args, **kwargs.
    
    Unittest failures:
    
    >     time.sleep(0.01)
    E     TypeError: handler() takes 0 positional arguments but 2 were given
    
    Change-Id: I4296a4bdef0e5f4d58b0503dcbc834f3bef0feeb
    Signed-off-by: Ross Brattain <ross.b.brattain@intel.com>

--- yardstick/cmd/NSBperf.py
+++ yardstick/cmd/NSBperf.py
@@ -39,12 +39,11 @@ if not PYTHONPATH or not VIRTUAL_ENV:
     raise SystemExit(1)
 
 
-def handler():
+def handler(*args, **kwargs):
     """ Capture ctrl+c and exit cli """
     subprocess.call(["pkill", "-9", "yardstick"])
     raise SystemExit(1)
 
-signal.signal(signal.SIGINT, handler)
 
 
 class YardstickNSCli(object):
@@ -214,5 +213,6 @@ class YardstickNSCli(object):
         self.run_test(args, test_path)
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, handler)
     NS_CLI = YardstickNSCli()
     NS_CLI.main()
