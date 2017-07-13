c4e55c19119b33110196c38b6821125ef9d76e8e
chenjiankun <chenjiankun1@huawei.com>
2017-07-13 02:26:19 +0000

    Add API to create environment
    JIRA: YARDSTICK-715
    
    API: /api/v2/yardstick/environments/action
    METHOD: POST
    PARAM:
    {
        'action': 'create_environment'
    }
    
    Change-Id: I58086be3c70cdc3c4c93516bcfd53ba725486600
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

---
+++ api/resources/v2/environments.py
@@ -0,0 +1,33 @@
+import uuid
+import logging
+
+from api import ApiResource
+from api.database.handlers import EnvironmentHandler
+from yardstick.common.utils import result_handler
+from yardstick.common import constants as consts
+
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.DEBUG)
+
+
+class V2Environments(ApiResource):
+
+    def post(self):
+        return self._dispatch_post()
+
+    def create_environment(self, args):
+        try:
+            name = args['name']
+        except KeyError:
+            return result_handler(consts.API_ERROR, 'name must be provided')
+
+        env_id = str(uuid.uuid4())
+
+        environment_handler = EnvironmentHandler()
+        env_init_data = {
+            'name': name,
+            'uuid': env_id
+        }
+        environment_handler.insert(env_init_data)
+
+        return result_handler(consts.API_SUCCESS, {'uuid': env_id})
