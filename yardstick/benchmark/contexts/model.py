66973733b2a08610431875f1617648f4790783fa
JingLu5 <lvjing5@huawei.com>
2017-07-24 02:44:26 +0000

    Heat: support vnic_type in heat type context
    JIRA: YARDSTICK-757
    
    Some test scenarios require VM with specific vnic type.
    This work is about supporting using different vnic types in heat type context.
    
    context:
      name: demo
      image: cirros-0.3.5
      flavor: yardstick-flavor
      user: cirros
    
      placement_groups:
        pgrp1:
          policy: "availability"
    
      servers:
        athena:
          floating_ip: true
          placement: "pgrp1"
        ares:
          placement: "pgrp1"
    
      networks:
        test:
          cidr: '10.0.1.0/24'
          vnic_type: "normal"
    
    Change-Id: Ia229fda72f47b04288ea107e2d58fd3e8ac91dd9
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

--- yardstick/benchmark/contexts/model.py
+++ yardstick/benchmark/contexts/model.py
@@ -110,6 +110,7 @@ class Network(Object):
         self.provider = attrs.get('provider')
         self.segmentation_id = attrs.get('segmentation_id')
         self.network_type = attrs.get('network_type')
+        self.vnic_type = attrs.get('vnic_type', 'normal')
         self.port_security_enabled = attrs.get('port_security_enabled', True)
         self.allowed_address_pairs = attrs.get('allowed_address_pairs', [])
         try:
@@ -255,7 +256,8 @@ class Server(Object):     # pragma: no cover
             # don't refactor to pass in network object, that causes JSON
             # circular ref encode errors
             template.add_port(port_name, network.stack_name, network.subnet_stack_name,
-                              sec_group_id=sec_group_id, provider=network.provider,
+                              network.vnic_type, sec_group_id=sec_group_id,
+                              provider=network.provider,
                               allowed_address_pairs=network.allowed_address_pairs)
             port_name_list.append(port_name)
 
