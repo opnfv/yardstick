998956a41c580381ff9ba6b54cedf8076ba21c7c
JingLu5 <lvjing5@huawei.com>
2017-08-02 07:16:27 +0000

    New storage test case: Bonnie++
    JIRA: YARDSTICK-770
    
    Bonnie++ is a disk and file system benchmarking tool for measuring I/O performance.
    With Bonnie++ you can quickly and easily produce a meaningful value to represent
    your current file system performance.
    
    This work is add new storage test case using Bonnie++.
    
    Change-Id: I752fee156707cda730962c68d17fda4d4e9cd472
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

---
+++ tests/unit/benchmark/scenarios/storage/test_bonnie.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+##############################################################################
+# Copyright (c) 2017 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# Unittest for yardstick.benchmark.scenarios.storage.bonnie.Bonnie
+
+from __future__ import absolute_import
+
+import unittest
+
+import mock
+
+from yardstick.common import utils
+from yardstick.benchmark.scenarios.storage import bonnie
+
+
+@mock.patch('yardstick.benchmark.scenarios.storage.bonnie.ssh')
+class BonnieTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.ctx = {
+            'host': {
+                'ip': '172.16.0.137',
+                'user': 'root',
+                'key_filename': "mykey.key"
+            }
+        }
+
+        self.result = {}
+
+    def test_bonnie_successful_setup(self, mock_ssh):
+
+        options = {
+            "file_size": "1024",
+            "ram_size": "512",
+            "test_dir": "/tmp",
+            "concurrency": "1",
+            "test_user": "root"
+        }
+        args = {"options": options}
+        b = bonnie.Bonnie(args, self.ctx)
+        mock_ssh.SSH.from_node().execute.return_value = (0, '', '')
+
+        b.setup()
+        self.assertIsNotNone(b.client)
+        self.assertTrue(b.setup_done, True)
+
+    def test_bonnie_successful__run(self, mock_ssh):
+
+        options = {
+            "file_size": "1024",
+            "ram_size": "512",
+            "test_dir": "/tmp",
+            "concurrency": "1",
+            "test_user": "root"
+        }
+        args = {"options": options}
+        b = bonnie.Bonnie(args, self.ctx)
+
+        sample_output = '1.97,1.97,bonnie,1,1501658820,1G,,804,65,193281,14,108497,8,4576,99,\
+                         192159,8,+++++,+++,16,,,,,+++++,+++,+++++,+++,+++++,+++,+++++,+++,\
+                         +++++,+++,+++++,+++,20287us,441ms,329ms,2408us,2581us,1820us,101us,\
+                         338us,425us,2185us,23us,149us\n'
+        mock_ssh.SSH.from_node().execute.return_value = (0, sample_output, '')
+        b.run(self.result)
+        expected_result = sample_output.split('\n')[-2]
+        self.assertEqual(self.result, expected_result)
+
+    def test_ramspeed_unsuccessful_script_error(self, mock_ssh):
+        options = {
+            "file_size": "1024",
+            "ram_size": "512",
+            "test_dir": "/tmp",
+            "concurrency": "1",
+            "test_user": "root"
+        }
+        args = {"options": options}
+        b = bonnie.Bonnie(args, self.ctx)
+
+        mock_ssh.SSH.from_node().execute.return_value = (1, '', 'FOOBAR')
+        self.assertRaises(RuntimeError, b.run, self.result)
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
