628b75b067943c7c6b018d0e82b0ccb1d4ed4616
tjuyinkanglin <14_ykl@tongji.edu.cn>
2017-08-09 23:06:56 +0800

    Add test case file, document and related scripts of yardstick tc057(HA_TC014)
    JIRA: YARDSTICK-779
    
    Change-Id: I6a812b1c88229b20a0dd0ce5bc135c9ba15266db
    Signed-off-by: tjuyinkanglin <14_ykl@tongji.edu.cn>

---
+++ yardstick/benchmark/scenarios/availability/ha_tools/pacemaker/get_resource_status_host.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##############################################################################
+# (c) OPNFV, Yin Kanglin and others.
+# 14_ykl@tongji.edu.cn
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# get pacemaker resource status of hosts
+# parameter: $1 - resource name $2 status
+
+pcs resource show | grep $1 -A 3 | grep $2
