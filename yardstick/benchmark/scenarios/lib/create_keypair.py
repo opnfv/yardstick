faa94361484811ad924a67fc1145d2eda65028c5
JingLu5 <lvjing5@huawei.com>
2017-08-11 06:16:19 +0000

    Add common openstack opertation scenarios: network
    JIRA: YARDSTICK-781
    
    This patch adds some common openstack opertation scenarios
    
    Change-Id: I854fc435a5c951245a5997cd4e3e63c5162030af
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

---
+++ yardstick/benchmark/scenarios/lib/create_keypair.py
@@ -0,0 +1,69 @@
+##############################################################################
+# Copyright (c) 2017 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+from __future__ import print_function
+from __future__ import absolute_import
+
+import logging
+import paramiko
+
+from yardstick.benchmark.scenarios import base
+import yardstick.common.openstack_utils as op_utils
+
+LOG = logging.getLogger(__name__)
+
+
+class CreateKeypair(base.Scenario):
+    """Create an OpenStack keypair"""
+
+    __scenario_type__ = "CreateKeypair"
+
+    def __init__(self, scenario_cfg, context_cfg):
+        self.scenario_cfg = scenario_cfg
+        self.context_cfg = context_cfg
+        self.options = self.scenario_cfg['options']
+
+        self.key_name = self.options.get("key_name", "yardstick_key")
+        self.key_filename = self.options.get("key_path", "/tmp/yardstick_key")
+
+        self.setup_done = False
+
+    def setup(self):
+        """scenario setup"""
+
+        self.setup_done = True
+
+    def run(self, result):
+        """execute the test"""
+
+        if not self.setup_done:
+            self.setup()
+
+        rsa_key = paramiko.RSAKey.generate(bits=2048, progress_func=None)
+        rsa_key.write_private_key_file(self.key_filename)
+        print("Writing %s ..." % self.key_filename)
+        with open(self.key_filename + ".pub", "w") as pubkey_file:
+            pubkey_file.write(
+                "%s %s\n" % (rsa_key.get_name(), rsa_key.get_base64()))
+        del rsa_key
+
+        keypair = op_utils.create_keypair(self.key_name,
+                                          self.key_filename + ".pub")
+
+        if keypair:
+            LOG.info("Create keypair successful!")
+        else:
+            LOG.info("Create keypair failed!")
+        try:
+            keys = self.scenario_cfg.get('output', '').split()
+        except KeyError:
+            pass
+        else:
+            values = [keypair.id]
+            return self._push_to_outputs(keys, values)
