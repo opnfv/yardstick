89280110f650434d0036452583edce4b8eb0a156
Ross Brattain <ross.b.brattain@intel.com>
2017-08-13 12:09:53 -0700

    cli: typo, s/dispath/dispatch/g
    Change-Id: I3df6bc606f2ac78baf611aee22b9247f6f9d5136
    Signed-off-by: Ross Brattain <ross.b.brattain@intel.com>

--- yardstick/cmd/cli.py
+++ yardstick/cmd/cli.py
@@ -133,13 +133,13 @@ class YardstickCLI():   # pragma: no cover
         if CONF.debug:
             _LOG_STREAM_HDLR.setLevel(logging.DEBUG)
 
-    def _dispath_func_notask(self):
+    def _dispatch_func_notask(self):
 
         # dispatch to category parser
         func = CONF.category.func
         func(CONF.category)
 
-    def _dispath_func_task(self, task_id):
+    def _dispatch_func_task(self, task_id):
 
         # dispatch to category parser
         func = CONF.category.func
@@ -159,7 +159,7 @@ class YardstickCLI():   # pragma: no cover
 
             self._handle_global_opts()
 
-            self._dispath_func_notask()
+            self._dispatch_func_notask()
         finally:
             self._clear_config_opts()
 
@@ -172,6 +172,6 @@ class YardstickCLI():   # pragma: no cover
 
             self._handle_global_opts()
 
-            self._dispath_func_task(task_id)
+            self._dispatch_func_task(task_id)
         finally:
             self._clear_config_opts()
