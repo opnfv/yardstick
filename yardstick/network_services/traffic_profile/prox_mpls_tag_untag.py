13aaaeb29e09a51239742122d6098fc141a0ee60
abhijitsinha <abhijit.sinha@intel.com>
2017-08-28 20:35:46 +0100

    DRAFT: Addition of Prox NSB changes in yardstick
    JIRA: YARDSTICK-802
    
    Addition of Prox L2Fwd, MPLS test cases for BM and Heat.
    
    updates:
    Most of tg_prox and prox_vnf were absorbed into the base classes.
    
    delete most of ProxDpdkVnfSetupEnvHelper, it is handled by DpdkVnfSetupEnvHelper baseclass
    
    use standard _build_pipeline_kwargs methods
    
    don't use terminate() use baseclass version
    
    add new method kill_vnf that runs pkill -x
    
    replace resource_helper.execute() with vnf_execture for dumping stats
    
    In order to share code between tg_prox and vnf_prox
    refactor to have tg_prox hold and wrap a ProxApproxVnf instance and call
    methods on that class.   Do this instead of multiple-inheritance.
    
    Implement ProxApproxVnf.terminate() using prox socket command
    based exit, (stop_all, quit, force_quit).
    
    vnf_execute calls resource_helper.execute() which calls
    socket methods on the sut object.
    
    Since tg_prox wraps the VNF object, we can call
    terminate on the VNF object and it should work correctly.
    
    move prox config generation to parent process
        we need to get core number info from config file
        inside the TG processes, so we need to generate
        the config in the parent process so the data is
        copied to the child during the fork.
    
    moved more config file methods to the setup_helper class.
    
    we run force_quit after quit, so the socket should already be closed
    this will trigger socket error, so add _ignore_errors option for
    vnf_execute to ignore socket errors
    
    Fixed the terminate issue. Added MPLS tests.
    
    Change-Id: Idd9493f597c668a3bb7d90e167e6a418546106e8
    Signed-off-by: Abhijit Sinha <abhijit.sinha@intel.com>

--- yardstick/network_services/traffic_profile/prox_mpls_tag_untag.py
+++ yardstick/network_services/traffic_profile/prox_mpls_tag_untag.py
@@ -22,13 +22,13 @@ from yardstick.network_services.traffic_profile.prox_profile import ProxProfile
 LOG = logging.getLogger(__name__)
 
 
-class ProxBinSearchProfile(ProxProfile):
+class ProxMplsTagUntagProfile(ProxProfile):
     """
     This profile adds a single stream at the beginning of the traffic session
     """
 
     def __init__(self, tp_config):
-        super(ProxBinSearchProfile, self).__init__(tp_config)
+        super(ProxMplsTagUntagProfile, self).__init__(tp_config)
         self.current_lower = self.lower_bound
         self.current_upper = self.upper_bound
 
@@ -98,5 +98,4 @@ class ProxBinSearchProfile(ProxProfile):
                 self.current_upper = test_value
 
             samples = result.get_samples(pkt_size, successful_pkt_loss)
-            #TODO:Fill in port stats here.
             self.queue.put(samples)
