de27033fcc0aa190c3988834f52fd6df53827a48
Ross Brattain <ross.b.brattain@intel.com>
2017-09-05 15:38:52 -0700

    collectd: write config file from Jinja2 template
    We have the collectd.conf inside the python package
    so instead of copying it from various places,
    write the template directly to the remote system.
    
    collectd: read collect.conf template with pkgresources
    
    read the collectd.conf file as a string directly
    and upload without creating temp file
    
    use Jinja2 template, disable failing plugins
    
    use proper Jinja2 template, disable the plugins that
    were failing to load and blocking startup
    
    add support for per-testcase collectd.conf config
    using YAML
    
    Change-Id: Id904f7b7c9f41a9dd7adf5dfa06c064d65c25d2d
    Signed-off-by: Ross Brattain <ross.b.brattain@intel.com>

--- yardstick/network_services/vnf_generic/vnf/sample_vnf.py
+++ yardstick/network_services/vnf_generic/vnf/sample_vnf.py
@@ -282,9 +282,11 @@ class DpdkVnfSetupEnvHelper(SetupEnvHelper):
 
     def setup_vnf_environment(self):
         self._setup_dpdk()
-        resource = self._setup_resources()
+        self.bound_pci = [v['virtual-interface']["vpci"] for v in self.vnfd_helper.interfaces]
         self.kill_vnf()
+        # bind before _setup_resources so we can use dpdk_port_num
         self._detect_and_bind_drivers()
+        resource = self._setup_resources()
         return resource
 
     def kill_vnf(self):
@@ -307,10 +309,16 @@ class DpdkVnfSetupEnvHelper(SetupEnvHelper):
         if exit_status != 0:
             self.ssh_helper.execute("bash %s dpdk >/dev/null 2>&1" % dpdk_setup)
 
-    def _setup_resources(self):
-        interfaces = self.vnfd_helper.interfaces
-        self.bound_pci = [v['virtual-interface']["vpci"] for v in interfaces]
+    def get_collectd_plugins(self):
+        plugins = self.scenario_helper.all_options["collectd"]
+        # override with specific node settings
+        if plugins:
+            plugins.update(self.scenario_helper.options.get("collectd", {}))
+        else:
+            plugins = {}
+        return plugins
 
+    def _setup_resources(self):
         # what is this magic?  how do we know which socket is for which port?
         # what about quad-socket?
         if any(v[5] == "0" for v in self.bound_pci):
@@ -319,8 +327,14 @@ class DpdkVnfSetupEnvHelper(SetupEnvHelper):
             self.socket = 1
 
         cores = self._validate_cpu_cfg()
+        all_port_names = (intf["name"] for intf in self.vnfd_helper.interfaces)
+        # implicit ordering, presumably by DPDK port num, so pre-sort by port_num
+        # this won't work because we don't have DPDK port numbers yet
+        port_names = sorted(all_port_names, key=self.vnfd_helper.port_num)
+        plugins = self.get_collectd_plugins()
         return ResourceProfile(self.vnfd_helper.mgmt_interface,
-                               interfaces=self.vnfd_helper.interfaces, cores=cores)
+                               port_names=port_names, cores=cores,
+                               plugins=plugins)
 
     def _detect_and_bind_drivers(self):
         interfaces = self.vnfd_helper.interfaces
