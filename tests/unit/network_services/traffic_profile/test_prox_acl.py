a4b9b183b2e58f7cb3f6c4595eee71a466343e5c
DanielMartinBuckley <daniel.m.buckley@intel.com>
2017-09-05 19:39:23 +0100

    DRAFT: Addition of PROX NSB tests to yardstick
    JIRA: YARDSTICK-802
    
    Addition of PROX L2FWD_Multiflow, ACL plus grafana dashboards
    
    Supports 2 and 4 port Baremetal & Heat
    
    TODO: Codecoverage to be added if required
          Load balancing to be added
    
    Change-Id: I1f3990d5451de265ee3901302569c355ece3b146
    Signed-off-by: Daniel Martin Buckley <daniel.m.buckley@intel.com>

--- tests/unit/network_services/traffic_profile/test_prox_acl.py
+++ tests/unit/network_services/traffic_profile/test_prox_acl.py
@@ -25,42 +25,69 @@ stl_patch = mock.patch.dict("sys.modules", STL_MOCKS)
 stl_patch.start()
 
 if stl_patch:
-    from yardstick.network_services.traffic_profile.prox_ACL import ProxACLProfile
     from yardstick.network_services.vnf_generic.vnf.prox_helpers import ProxTestDataTuple
+    from yardstick.network_services.traffic_profile.prox_ACL import ProxACLProfile
 
 
-class TestProxRampProfile(unittest.TestCase):
+class TestProxACLProfile(unittest.TestCase):
 
-    def test_run_test_with_pkt_size(self):
+    def test_execute_1(self):
+        def target(*args, **kwargs):
+            runs.append(args[2])
+            if args[2] < 0 or args[2] > 100:
+                raise RuntimeError(' '.join([str(args), str(runs)]))
+            if args[2] > 75.0:
+                return fail_tuple
+            return success_tuple
+
         tp_config = {
             'traffic_profile': {
-                'upper_bound': 100.0,
+                'packet_sizes': [200],
             },
         }
 
+        runs = []
         success_tuple = ProxTestDataTuple(10.0, 1, 2, 3, 4, [5.1, 5.2, 5.3], 995, 1000, 123.4)
         fail_tuple = ProxTestDataTuple(10.0, 1, 2, 3, 4, [5.6, 5.7, 5.8], 850, 1000, 123.4)
 
-        traffic_gen = mock.MagicMock()
-        traffic_gen.resource_helper.run_test.side_effect = [
-            success_tuple,
-            success_tuple,
-            success_tuple,
-            fail_tuple,
-            success_tuple,
-            fail_tuple,
-            fail_tuple,
-            fail_tuple,
-        ]
-
-        fill_values = [1, 2, 3, 4, RuntimeError]
+        traffic_generator = mock.MagicMock()
+        traffic_generator.resource_helper.run_test = target
 
         profile = ProxACLProfile(tp_config)
-        profile.fill_samples = fill_samples = mock.MagicMock(side_effect=fill_values)
-        profile.queue = mock.MagicMock()
+        profile.init(mock.MagicMock())
 
-        with self.assertRaises(RuntimeError):
-            profile.run_test_with_pkt_size(traffic_gen, 128, 30)
+        profile.execute(traffic_generator)
+        self.assertEqual(round(profile.current_lower, 2), 74.69)
+        self.assertEqual(round(profile.current_upper, 2), 75.39)
+        self.assertEqual(len(runs), 8)
 
-        self.assertEqual(traffic_gen.resource_helper.run_test.call_count, 5)
-        self.assertEqual(fill_samples.call_count, 5)
+    def test_execute_2(self):
+        def target(*args, **kwargs):
+            runs.append(args[2])
+            if args[2] < 0 or args[2] > 100:
+                raise RuntimeError(' '.join([str(args), str(runs)]))
+            if args[2] > 25.0:
+                return fail_tuple
+            return success_tuple
+
+        tp_config = {
+            'traffic_profile': {
+                'packet_sizes': [200],
+                'test_precision': 2.0,
+            },
+        }
+
+        runs = []
+        success_tuple = ProxTestDataTuple(10.0, 1, 2, 3, 4, [5.1, 5.2, 5.3], 995, 1000, 123.4)
+        fail_tuple = ProxTestDataTuple(10.0, 1, 2, 3, 4, [5.6, 5.7, 5.8], 850, 1000, 123.4)
+
+        traffic_generator = mock.MagicMock()
+        traffic_generator.resource_helper.run_test = target
+
+        profile = ProxACLProfile(tp_config)
+        profile.init(mock.MagicMock())
+
+        profile.execute(traffic_generator)
+        self.assertEqual(round(profile.current_lower, 2), 24.06)
+        self.assertEqual(round(profile.current_upper, 2), 25.47)
+        self.assertEqual(len(runs), 7)
