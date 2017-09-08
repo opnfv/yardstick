872eaa6e7de364d485e505746b6de05fbddce1ea
Ross Brattain <ross.b.brattain@intel.com>
2017-09-08 00:38:03 -0700

    NSBperf fixes
    Change-Id: I8288b6e729445108e42d1c5754bac6153223d673
    Signed-off-by: Ross Brattain <ross.b.brattain@intel.com>

--- tests/unit/cmd/test_NSBperf.py
+++ tests/unit/cmd/test_NSBperf.py
@@ -55,18 +55,19 @@ class TestYardstickNSCli(unittest.TestCase):
         nfvi = {"collect_stats": {"cpu": {"ipc": 0, "Hz": 2.6}}}
         self.assertIsNone(yardstick_ns_cli.generate_nfvi_results(nfvi))
 
-    def test_handle_list_options(self):
+    @mock.patch("yardstick.cmd.NSBperf.os")
+    def test_handle_list_options(self, mock_os):
+        mock_os.listdir.return_value = "abcdefghi"
         yardstick_ns_cli = YardstickNSCli()
         CLI_PATH = os.path.dirname(os.path.realpath(__file__))
         repo_dir = CLI_PATH + "/../../"
         test_path = os.path.join(repo_dir, "../samples/vnf_samples/nsut/")
         args = {"list_vnfs": True, "list": False}
-        self.assertRaises(SystemExit, yardstick_ns_cli.handle_list_options,
-                          args, test_path)
+        with self.assertRaises(SystemExit):
+            yardstick_ns_cli.handle_list_options(args, test_path)
         args = {"list_vnfs": False, "list": True}
-        self.assertRaises(SystemExit,
-                          yardstick_ns_cli.handle_list_options,
-                          args, test_path)
+        with self.assertRaises(SystemExit):
+            yardstick_ns_cli.handle_list_options(args, test_path)
 
     def test_main(self):
         yardstick_ns_cli = YardstickNSCli()
