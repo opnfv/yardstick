4daffd1177578a26a4d1fbac7a7bd099d85d08a4
Abhijit Sinha <abhijit.sinha@intel.com>
2017-09-12 19:08:41 +0100

    DRAFT: Addition of Prox NSB BNG-QoS test
    JIRA: YARDSTICK-802
    
    Added Prox BNG-QoS Test
    - The tests supports BM, Openstack Heat
    - Supports 4 ports
    - Test added for bng traffic profile
    
    TODO:
    - packets_in is 0 in heat tests (Debug heat test)
    - Need to write unit tests
    - Grafana Dashboard to be added
    
    Change-Id: Ib40811bedb45a3c3030643943f32679a4044e076
    Signed-off-by: Abhijit Sinha <abhijit.sinha@intel.com>

---
+++ tests/unit/network_services/traffic_profile/test_prox_bng.py
@@ -0,0 +1,93 @@
+# Copyright (c) 2017 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from __future__ import absolute_import
+
+import unittest
+import mock
+
+from tests.unit import STL_MOCKS
+
+STLClient = mock.MagicMock()
+stl_patch = mock.patch.dict("sys.modules", STL_MOCKS)
+stl_patch.start()
+
+if stl_patch:
+    from yardstick.network_services.vnf_generic.vnf.prox_helpers import ProxTestDataTuple
+    from yardstick.network_services.traffic_profile.prox_bng import ProxBngProfile
+
+
+class TestProxBngProfile(unittest.TestCase):
+
+    def test_bng_1(self):
+        def target(*args, **kwargs):
+            runs.append(args[2])
+            if args[2] < 0 or args[2] > 100:
+                raise RuntimeError(' '.join([str(args), str(runs)]))
+            if args[2] > 75.0:
+                return fail_tuple, {}
+            return success_tuple, {}
+
+        tp_config = {
+            'traffic_profile': {
+                'packet_sizes': [200],
+            },
+        }
+
+        runs = []
+        success_tuple = ProxTestDataTuple(10.0, 1, 2, 3, 4, [5.1, 5.2, 5.3], 995, 1000, 123.4)
+        fail_tuple = ProxTestDataTuple(10.0, 1, 2, 3, 4, [5.6, 5.7, 5.8], 850, 1000, 123.4)
+
+        traffic_generator = mock.MagicMock()
+        traffic_generator.run_test = target
+
+        profile = ProxBngProfile(tp_config)
+        profile.init(mock.MagicMock())
+
+        profile.execute(traffic_generator)
+        self.assertEqual(round(profile.current_lower, 2), 74.69)
+        self.assertEqual(round(profile.current_upper, 2), 75.39)
+        self.assertEqual(len(runs), 8)
+
+    def test_bng_2(self):
+        def target(*args, **kwargs):
+            runs.append(args[2])
+            if args[2] < 0 or args[2] > 100:
+                raise RuntimeError(' '.join([str(args), str(runs)]))
+            if args[2] > 25.0:
+                return fail_tuple, {}
+            return success_tuple, {}
+
+        tp_config = {
+            'traffic_profile': {
+                'packet_sizes': [200],
+                'test_precision': 2.0,
+            },
+        }
+
+        runs = []
+        success_tuple = ProxTestDataTuple(10.0, 1, 2, 3, 4, [5.1, 5.2, 5.3], 995, 1000, 123.4)
+        fail_tuple = ProxTestDataTuple(10.0, 1, 2, 3, 4, [5.6, 5.7, 5.8], 850, 1000, 123.4)
+
+        traffic_generator = mock.MagicMock()
+        traffic_generator.run_test = target
+
+        profile = ProxBngProfile(tp_config)
+        profile.init(mock.MagicMock())
+
+        profile.execute(traffic_generator)
+        self.assertEqual(round(profile.current_lower, 2), 24.06)
+        self.assertEqual(round(profile.current_upper, 2), 25.47)
+        self.assertEqual(len(runs), 7)
