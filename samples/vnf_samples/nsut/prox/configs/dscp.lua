5aedbbd1f0bc3f1fc91cb3aa97dd827c15f632a5
Abhijit Sinha <abhijit.sinha@intel.com>
2017-09-12 19:08:41 +0100

    DRAFT: Addition of Prox NSB BNG-QoS test
    JIRA: YARDSTICK-802
    
    Added Prox BNG-QoS Test
    - The tests supports BM, Openstack Heat
    - Supports 4 ports
    - Test added for bng traffic profile
    
    TODO:
    - packets_in is 0 in heat tests (Debug heat test)
    - Need to write unit tests
    - Grafana Dashboard to be added
    
    Change-Id: Ib40811bedb45a3c3030643943f32679a4044e076
    Signed-off-by: Abhijit Sinha <abhijit.sinha@intel.com>

---
+++ samples/vnf_samples/nsut/prox/configs/dscp.lua
@@ -0,0 +1,82 @@
+-- Copyright (c) 2016-2017 Intel Corporation
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+--;
+
+return {
+   {dscp = 0,  tc = 0, queue = 0},
+   {dscp = 1,  tc = 0, queue = 1},
+   {dscp = 2,  tc = 0, queue = 2},
+   {dscp = 3,  tc = 0, queue = 3},
+   {dscp = 4,  tc = 1, queue = 0},
+   {dscp = 5,  tc = 1, queue = 1},
+   {dscp = 6,  tc = 1, queue = 2},
+   {dscp = 7,  tc = 1, queue = 3},
+   {dscp = 8,  tc = 2, queue = 0},
+   {dscp = 9,  tc = 2, queue = 1},
+   {dscp = 10, tc = 2, queue = 2},
+   {dscp = 11, tc = 2, queue = 3},
+   {dscp = 12, tc = 3, queue = 0},
+   {dscp = 13, tc = 3, queue = 1},
+   {dscp = 14, tc = 3, queue = 2},
+   {dscp = 15, tc = 3, queue = 3},
+   {dscp = 16, tc = 0, queue = 0},
+   {dscp = 17, tc = 0, queue = 1},
+   {dscp = 18, tc = 0, queue = 2},
+   {dscp = 19, tc = 0, queue = 3},
+   {dscp = 20, tc = 1, queue = 0},
+   {dscp = 21, tc = 1, queue = 1},
+   {dscp = 22, tc = 1, queue = 2},
+   {dscp = 23, tc = 1, queue = 3},
+   {dscp = 24, tc = 2, queue = 0},
+   {dscp = 25, tc = 2, queue = 1},
+   {dscp = 26, tc = 2, queue = 2},
+   {dscp = 27, tc = 2, queue = 3},
+   {dscp = 28, tc = 3, queue = 0},
+   {dscp = 29, tc = 3, queue = 1},
+   {dscp = 30, tc = 3, queue = 2},
+   {dscp = 31, tc = 3, queue = 3},
+   {dscp = 32, tc = 0, queue = 0},
+   {dscp = 33, tc = 0, queue = 1},
+   {dscp = 34, tc = 0, queue = 2},
+   {dscp = 35, tc = 0, queue = 3},
+   {dscp = 36, tc = 1, queue = 0},
+   {dscp = 37, tc = 1, queue = 1},
+   {dscp = 38, tc = 1, queue = 2},
+   {dscp = 39, tc = 1, queue = 3},
+   {dscp = 40, tc = 2, queue = 0},
+   {dscp = 41, tc = 2, queue = 1},
+   {dscp = 42, tc = 2, queue = 2},
+   {dscp = 43, tc = 2, queue = 3},
+   {dscp = 44, tc = 3, queue = 0},
+   {dscp = 45, tc = 3, queue = 1},
+   {dscp = 46, tc = 3, queue = 2},
+   {dscp = 47, tc = 3, queue = 3},
+   {dscp = 48, tc = 0, queue = 0},
+   {dscp = 49, tc = 0, queue = 1},
+   {dscp = 50, tc = 0, queue = 2},
+   {dscp = 51, tc = 0, queue = 3},
+   {dscp = 52, tc = 1, queue = 0},
+   {dscp = 53, tc = 1, queue = 1},
+   {dscp = 54, tc = 1, queue = 2},
+   {dscp = 55, tc = 1, queue = 3},
+   {dscp = 56, tc = 2, queue = 0},
+   {dscp = 57, tc = 2, queue = 1},
+   {dscp = 58, tc = 2, queue = 2},
+   {dscp = 59, tc = 2, queue = 3},
+   {dscp = 60, tc = 3, queue = 0},
+   {dscp = 61, tc = 3, queue = 1},
+   {dscp = 62, tc = 3, queue = 2},
+   {dscp = 63, tc = 3, queue = 3},
+}
