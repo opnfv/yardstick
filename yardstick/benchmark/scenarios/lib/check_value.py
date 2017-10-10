82bf0069bc6420dbcfe305beab086e902b81066d
JingLu5 <lvjing5@huawei.com>
2017-10-10 07:17:59 +0000

    refine the commom openstack operation scenarios
    Change-Id: I48152676ad6a748b5b9da00febee3fc26144c102
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

--- yardstick/benchmark/scenarios/lib/check_value.py
+++ yardstick/benchmark/scenarios/lib/check_value.py
@@ -20,11 +20,27 @@ LOG = logging.getLogger(__name__)
 class CheckValue(base.Scenario):
     """Check values between value1 and value2
 
-    options:
-        operator: equal(eq) and not equal(ne)
-        value1:
-        value2:
-    output: check_result
+    Parameters:
+        operator: available operators are 'equal', 'not equal', 'less than' and 'larger than'
+            type:       string
+            unit:       N/A
+            default:    N/A
+
+        value1: input value1 for comparison
+            type:       N/A
+            unit:       N/A
+            default:    N/A
+
+        value2: input value2 for comparison
+            type:       N/A
+            unit:       N/A
+            default:    N/A
+
+        fluctuation: fluctuation rate for value1. only work with 'equal' operation now.
+            type:       float
+            unit:       N/A
+            default:    0.0
+
     """
 
     __scenario_type__ = "CheckValue"
@@ -37,22 +53,49 @@ class CheckValue(base.Scenario):
     def run(self, result):
         """execute the test"""
 
-        op = self.options.get("operator")
+        check_result = "PASS"
+
         LOG.debug("options=%s", self.options)
+        op = self.options.get("operator")
         value1 = str(self.options.get("value1"))
         value2 = str(self.options.get("value2"))
-        check_result = "PASS"
-        if op == "eq" and value1 != value2:
-            LOG.info("value1=%s, value2=%s, error: should equal!!!", value1,
-                     value2)
-            check_result = "FAIL"
-            assert value1 == value2, "Error %s!=%s" % (value1, value2)
-        elif op == "ne" and value1 == value2:
-            LOG.info("value1=%s, value2=%s, error: should not equal!!!",
-                     value1, value2)
-            check_result = "FAIL"
-            assert value1 != value2, "Error %s==%s" % (value1, value2)
-        LOG.info("Check result is %s", check_result)
-        keys = self.scenario_cfg.get('output', '').split()
-        values = [check_result]
-        return self._push_to_outputs(keys, values)
+
+        fluctuation = float(self.options.get("fluctuation", 0))
+        if fluctuation != 0:
+            value1_top = float(value2) * float(1 + fluctuation)
+            value1_bottom = float(value2) * float(1 - fluctuation)
+
+        try:
+            if fluctuation != 0 and op == "equal" and not value1_bottom <= float(value1) <= value1_top:
+                LOG.info("value1=%s, value2=%s, error: should fall within the range %s!!!",
+                         value1, value2, fluctuation)
+                check_result = "FAIL"
+                assert (value1_bottom <= float(value1) <= value1_top), \
+                    "Error %s!=%s in fluctuation range %s" % (value1, value2, fluctuation)
+            elif fluctuation == 0 and op == "equal" and value1 != value2:
+                LOG.info("value1=%s, value2=%s, error: should equal!!!", value1,
+                         value2)
+                check_result = "FAIL"
+                assert value1 == value2, "Error %s!=%s" % (value1, value2)
+            elif op == "larger than" and float(value1) < float(value2):
+                LOG.info("value1=%s, value2=%s, error: should larger than!!!", value1,
+                         value2)
+                check_result = "FAIL"
+                assert value1 > value2, "Error %s<=%s" % (value1, value2)
+            elif op == "less than" and float(value1) > float(value2):
+                LOG.info("value1=%s, value2=%s, error: should less than!!!", value1,
+                         value2)
+                check_result = "FAIL"
+                assert value1 < value2, "Error %s>=%s" % (value1, value2)
+            elif op == "not equal" and value1 == value2:
+                LOG.info("value1=%s, value2=%s, error: should not equal!!!",
+                         value1, value2)
+                check_result = "FAIL"
+                assert value1 != value2, "Error %s==%s" % (value1, value2)
+        except AssertionError, e:
+            LOG.info("Check result is %s", check_result)
+            result.update({'check_result': check_result})
+            raise e
+        else:
+            LOG.info("Check result is %s", check_result)
+            result.update({'check_result': check_result})
