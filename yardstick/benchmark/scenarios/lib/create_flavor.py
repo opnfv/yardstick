b318b8c220652f7d7ed184150dd0c3026d70b98a
JingLu5 <lvjing5@huawei.com>
2017-10-10 07:17:59 +0000

    refine the commom openstack operation scenarios
    Change-Id: I48152676ad6a748b5b9da00febee3fc26144c102
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

--- yardstick/benchmark/scenarios/lib/create_flavor.py
+++ yardstick/benchmark/scenarios/lib/create_flavor.py
@@ -19,7 +19,35 @@ LOG = logging.getLogger(__name__)
 
 
 class CreateFlavor(base.Scenario):
-    """Create an OpenStack flavor"""
+    """Create an OpenStack flavor
+
+    Parameters
+        flavor_name - name of the flavor to be created
+            type:    string
+            unit:    N/A
+            default: 'test-flavor'
+        ram - memory size in MB
+            type:    int
+            unit:    MB
+            default: 1024
+        vcpus - the number of vcpus
+            type:    int
+            unit:    N/A
+            default: 2
+        disk - disk space in GB
+            type:    int
+            unit:    GB
+            default: 100
+        openstack_params - additional openstack parameters
+            type:    dict
+            unit:    N/A
+            default: null
+
+    Outputs:
+        flavor_id - id of the created flavor
+            type:    string
+            unit:    N/A
+    """
 
     __scenario_type__ = "CreateFlavor"
 
@@ -28,11 +56,11 @@ class CreateFlavor(base.Scenario):
         self.context_cfg = context_cfg
         self.options = self.scenario_cfg['options']
 
-        self.flavor_name = self.options.get("flavor_name", "TestFlavor")
-        self.vcpus = self.options.get("vcpus", 2)
+        self.flavor_name = self.options.get("flavor_name", "test-flavor")
         self.ram = self.options.get("ram", 1024)
+        self.vcpus = self.options.get("vcpus", 2)
         self.disk = self.options.get("disk", 100)
-        self.public = self.options.get("is_public", True)
+        self.openstack_params = self.options.get("openstack_params", {})
 
         self.setup_done = False
 
@@ -49,10 +77,8 @@ class CreateFlavor(base.Scenario):
 
         LOG.info("Creating flavor %s with %s vcpus, %sMB ram and %sGB disk",
                  self.flavor_name, self.vcpus, self.ram, self.disk)
-        paras = {'is_public': self.public}
-        flavor = op_utils.create_flavor(self.flavor_name,
-                                        self.ram, self.vcpus, self.disk,
-                                        **paras)
+        flavor = op_utils.create_flavor(self.flavor_name, self.ram, self.vcpus, self.disk,
+                                        **self.openstack_params)
 
         if flavor:
             LOG.info("Create flavor successful!")
@@ -61,5 +87,9 @@ class CreateFlavor(base.Scenario):
             LOG.info("Create flavor failed!")
             values = []
 
-        keys = self.scenario_cfg.get('output', '').split()
-        return self._push_to_outputs(keys, values)
+        try:
+            keys = self.scenario_cfg.get('output', '').split()
+        except KeyError:
+            pass
+        else:
+            return self._push_to_outputs(keys, values)
