d431d0ff3def5357c6d67ea7077f2958ba8c2719
Ross Brattain <ross.b.brattain@intel.com>
2017-10-11 15:16:53 -0700

    ping: always save rtt data, influxdb ignore empty data
    If the SLA was failing we were raising AssertionError and not storing
    the rtt in the data dict.  This caused influxdb parse errors because the
    data was empty.
    
    Fixup influxdb to ignore records with no data, so we don't try to parse
    no data.
    
    Change the ping logic to always record the rtt result even if the SLA
    was not met.
    
    Also fixup ping logic in cases where ping does not return results.  If
    SLA is defined use SLA * 10 otherwise use large float that doesn't break
    the grafana scale too much, maybe 9999
    
    JIRA: YARDSTICK-809
    
    Change-Id: Id2d51216581644a80e8c7b9aa98919a766008adf
    Signed-off-by: Ross Brattain <ross.b.brattain@intel.com>

--- yardstick/benchmark/scenarios/networking/ping.py
+++ yardstick/benchmark/scenarios/networking/ping.py
@@ -60,6 +60,7 @@ class Ping(base.Scenario):
 
         rtt_result = {}
         ping_result = {"rtt": rtt_result}
+        sla_max_rtt = self.scenario_cfg.get("sla", {}).get("max_rtt")
 
         for pos, dest in enumerate(dest_list):
             if 'targets' in self.scenario_cfg:
@@ -76,20 +77,38 @@ class Ping(base.Scenario):
             if exit_status != 0:
                 raise RuntimeError(stderr)
 
+            if isinstance(target_vm, dict):
+                target_vm_name = target_vm.get("name")
+            else:
+                target_vm_name = target_vm.split('.')[0]
             if stdout:
-                if isinstance(target_vm, dict):
-                    target_vm_name = target_vm.get("name")
-                else:
-                    target_vm_name = target_vm.split('.')[0]
-                rtt_result[target_vm_name] = float(stdout)
-                if "sla" in self.scenario_cfg:
-                    sla_max_rtt = int(self.scenario_cfg["sla"]["max_rtt"])
+                rtt_result[target_vm_name] = float(stdout.strip())
+                # store result before potential AssertionError
+                result.update(utils.flatten_dict_key(ping_result))
+                if sla_max_rtt is not None:
+                    sla_max_rtt = float(sla_max_rtt)
                     assert rtt_result[target_vm_name] <= sla_max_rtt,\
                         "rtt %f > sla: max_rtt(%f); " % \
                         (rtt_result[target_vm_name], sla_max_rtt)
             else:
+                # we need to specify a result to satisfy influxdb schema
+                # choose a very large number to inidcate timeout
+                if sla_max_rtt is not None:
+                    sla_max_rtt = float(sla_max_rtt)
+                    # in this case choose an order of magnitude greater than the SLA
+                    rtt_result[target_vm_name] = sla_max_rtt * 10
+                    # store result before potential AssertionError
+                    result.update(utils.flatten_dict_key(ping_result))
+                    assert rtt_result[target_vm_name] <= sla_max_rtt, \
+                        "packet dropped rtt %f > sla: max_rtt(%f); " % \
+                        (rtt_result[target_vm_name], sla_max_rtt)
+                else:
+                    # in this case this we don't have sla use very larg number that isn't +infinity
+                    rtt_result[target_vm_name] = float(9999)
+                    # store result before potential AssertionError
+                    result.update(utils.flatten_dict_key(ping_result))
+                    assert False, "packet dropped rtt %f" % (rtt_result[target_vm_name])
                 LOG.error("ping '%s' '%s' timeout", options, target_vm)
-        result.update(utils.flatten_dict_key(ping_result))
 
 
 def _test():    # pragma: no cover
