d1b8d4fa885733d0ec2f6dbc9e0f28b23163f33a
Emma Foley <emma.l.foley@intel.com>
2017-11-20 15:20:49 +0000

    Clean unit tests in "benchmark.scenarios.lib.test_create_colume.py
    Use mock_object.assert_called_with instead of
      self.assertTrue(mock_object.called)
    
    Change-Id: I54cc56434d316f059b3822879145e6a299866fcb
    Signed-off-by: Emma Foley <emma.l.foley@intel.com>

--- tests/unit/benchmark/scenarios/lib/test_create_volume.py
+++ tests/unit/benchmark/scenarios/lib/test_create_volume.py
@@ -18,19 +18,31 @@ class CreateVolumeTestCase(unittest.TestCase):
     @mock.patch('yardstick.common.openstack_utils.get_image_id')
     @mock.patch('yardstick.common.openstack_utils.get_cinder_client')
     @mock.patch('yardstick.common.openstack_utils.get_glance_client')
-    def test_create_volume(self, mock_get_glance_client, mock_get_cinder_client, mock_image_id, mock_create_volume):
-        options = {
-                'volume_name': 'yardstick_test_volume_01',
-                'size': '256',
-                'image': 'cirros-0.3.5'
-        }
-        args = {"options": options}
-        obj = CreateVolume(args, {})
-        obj.run({})
-        self.assertTrue(mock_create_volume.called)
-        self.assertTrue(mock_image_id.called)
-        self.assertTrue(mock_get_glance_client.called)
-        self.assertTrue(mock_get_cinder_client.called)
+    def test_create_volume(self, mock_get_glance_client,
+                           mock_get_cinder_client, mock_image_id,
+                           mock_create_volume):
+        """Test create_volume.
+
+        Expected bahaviour:
+            * The glance client is called (to get image info).
+            * The cinder client is called to create the volume.
+        """
+        scenario_cfg = {
+            "options" :
+                {
+                    'volume_name': 'yardstick_test_volume_01',
+                    'size': '256',
+                    'image': 'cirros-0.3.5'
+                }
+            }
+
+        obj = CreateVolume(scenario_cfg, {})
+        obj.run(result={})
+
+        mock_get_glance_client.assert_called()
+        mock_get_cinder_client.assert_called()
+        mock_image_id.assert_called()
+        mock_create_volume.assert_called()
 
 def main():
     unittest.main()
