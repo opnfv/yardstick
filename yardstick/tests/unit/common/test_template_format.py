ce80c92f9df9a974fa0a0a669183e486afe39501
Emma Foley <emma.l.foley@intel.com>
2017-11-29 23:34:15 +0000

    Move tests: unit/common
    * Fix pylint errors
    * Add TODOs
    
    Some errors are ignored locally, as they were a symptom of other problems.
    These issues have been flagged with a TODO, and should be fixed later.
    
    Change-Id: I2847c380556985fae2c92bb8a7da9c47e91e1338
    Jira: YARDSTICK-837
    Signed-off-by: Emma Foley <emma.l.foley@intel.com>

--- yardstick/tests/unit/common/test_template_format.py
+++ yardstick/tests/unit/common/test_template_format.py
@@ -24,6 +24,8 @@ class TemplateFormatTestCase(unittest.TestCase):
 
     def test_parse_to_value_exception(self):
 
+        # TODO: Don't hide the error that occurs in template_format.parse
+        # TODO: Separate these tests; one per error type
         with mock.patch.object(yaml, 'load') as yaml_loader:
             yaml_loader.side_effect = yaml.scanner.ScannerError()
             self.assertRaises(ValueError, template_format.parse, 'FOOBAR')
