ee9b15dd48c6d1241c473c357c50f6a4d2317a92
Emma Foley <emma.l.foley@intel.com>
2017-11-30 00:19:26 +0000

    Move tests: unit/network_services
    * Fix pylint errors
    * Add TODOs
    
    Some errors are ignored locally, as they were a symptom of other problems.
    These issues have been flagged with a TODO, and should be fixed later.
    
    Jira: YARDSTICK-837
    Signed-off-by: Emma Foley <emma.l.foley@intel.com>
    Change-Id: Idc2e70291b6d9b4cf7e7249a10c4a290b999206c

---
+++ yardstick/tests/unit/network_services/traffic_profile/test_prox_profile.py
@@ -0,0 +1,115 @@
+# Copyright (c) 2017 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import unittest
+import mock
+
+from yardstick.tests.unit import STL_MOCKS
+
+STLClient = mock.MagicMock()
+stl_patch = mock.patch.dict("sys.modules", STL_MOCKS)
+stl_patch.start()
+
+if stl_patch:
+    from yardstick.network_services.traffic_profile.prox_profile import ProxProfile
+    from yardstick.network_services.vnf_generic.vnf.prox_helpers import ProxResourceHelper
+
+
+class TestProxProfile(unittest.TestCase):
+
+    def test_fill_samples(self):
+        samples = {}
+        traffic_generator = mock.MagicMock()
+        traffic_generator.vpci_if_name_ascending = [
+            ['id1', 'name1'],
+            ['id2', 'name2'],
+        ]
+
+        traffic_generator.resource_helper.sut.port_stats.side_effect = [
+            list(range(12)),
+            list(range(10, 22)),
+        ]
+
+        expected = {
+            'name1': {
+                'in_packets': 6,
+                'out_packets': 7,
+            },
+            'name2': {
+                'in_packets': 16,
+                'out_packets': 17,
+            },
+        }
+        ProxProfile.fill_samples(samples, traffic_generator)
+        self.assertDictEqual(samples, expected)
+
+    def test_init(self):
+        tp_config = {
+            'traffic_profile': {},
+        }
+
+        profile = ProxProfile(tp_config)
+        queue = mock.Mock()
+        profile.init(queue)
+        self.assertIs(profile.queue, queue)
+
+    def test_execute_traffic(self):
+        packet_sizes = [
+            10,
+            100,
+            1000,
+        ]
+        tp_config = {
+            'traffic_profile': {
+                'packet_sizes': packet_sizes,
+            },
+        }
+
+        traffic_generator = mock.MagicMock()
+
+        setup_helper = traffic_generator.setup_helper
+        setup_helper.find_in_section.return_value = None
+
+        prox_resource_helper = ProxResourceHelper(setup_helper)
+        traffic_generator.resource_helper = prox_resource_helper
+
+        profile = ProxProfile(tp_config)
+
+        self.assertFalse(profile.done)
+        for _ in packet_sizes:
+            with self.assertRaises(NotImplementedError):
+                profile.execute_traffic(traffic_generator)
+
+        self.assertIsNone(profile.execute_traffic(traffic_generator))
+        self.assertTrue(profile.done)
+
+    def test_bounds_iterator(self):
+        tp_config = {
+            'traffic_profile': {},
+        }
+
+        profile = ProxProfile(tp_config)
+        value = 0.0
+        for value in profile.bounds_iterator():
+            pass
+
+        self.assertEqual(value, 100.0)
+
+        mock_logger = mock.MagicMock()
+        for _ in profile.bounds_iterator(mock_logger):
+            pass
+
+        self.assertEqual(mock_logger.debug.call_count, 1)
+        self.assertEqual(mock_logger.info.call_count, 10)
