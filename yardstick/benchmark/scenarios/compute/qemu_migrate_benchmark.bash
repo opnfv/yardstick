26b2c9b2ec4f12bc81e8adef3bb99b27f1550821
Navya <navyax.bathula@intel.com>
2017-11-30 14:32:25 +0530

    KVMFORNFV: Changes in Livemigration scripts
    This patch is used to modify the Livemigration code which includes
    redirecting the console output of the migration process to qemu.log
    file and obtaining the JSON output at the end.
    
    Change-Id: I5028f45e0cab4be5a83f3a0002fdf6ffd29bf784
    Signed-off-by: Navya <navyax.bathula@intel.com>

--- yardstick/benchmark/scenarios/compute/qemu_migrate_benchmark.bash
+++ yardstick/benchmark/scenarios/compute/qemu_migrate_benchmark.bash
@@ -21,17 +21,8 @@ max_down_time=$6
 
 OUTPUT_FILE=/tmp/output-qemu.log
 
-echo "To check the parameters:"
-echo "SRC: $src"
-echo "DST: $dst"
-echo "DST_IP: $dst_ip"
-echo "MIGRATE_PORT: $migrate_to_port"
-echo "DOWN_TIME: $max_down_time"
-
 do_migrate()
 {
-        echo "Execution of Live Migration"
-
         echo "info status" | nc -U $src
         # with no speed limit
         echo "migrate_set_speed 0" | nc -U $src
@@ -48,18 +39,15 @@ do_migrate()
                 sleep 1;
         done
 
-        echo "End of Live Migration"
-}
+} > /dev/null
 
 output_qemu()
 {
-        echo "Checking status of Migration"
         # print detail information
         echo "info migrate" | nc -U $src
         echo "quit" | nc -U $src
         echo "quit" | nc -U $dst
         sleep 5
-        echo "Migration executed successfully"
 
 } > $OUTPUT_FILE
 
@@ -74,14 +62,17 @@ echo -e "{ \
         \"setuptime\":\"$setuptime\" \
          }"
 }
+
 # main entry
 main()
 {
-    echo "Perform LiveMigration"
+    # Perform LiveMigration
     do_migrate
-    echo "LiveMigration Status"
+
+    # LiveMigration Status
     output_qemu
-    echo "LiveMigration JSON output "
+
+    # LiveMigration JSON output
     output_json
 }
 main
