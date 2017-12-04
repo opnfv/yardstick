34bccb32a505604ac33faaf4a4b6ff6cb6918409
Rodolfo Alonso Hernandez <rodolfo.alonso.hernandez@intel.com>
2017-12-04 16:24:13 +0000

    Replace "python-heatclient" with "shade" client
    Replaced "python-heatclient" with "shade" client. The "python-heatclient"
    library is removed from requirements file. "shade" client version 1.22.2
    is the required one for OpenStack Pike release.
    
    shade is the recommended client to interact with OpenStack clouds [1]
    
    [1] https://github.com/openstack-infra/shade
    
    JIRA: YARDSTICK-880
    
    Change-Id: Ibbdfc42a543d2d9ed4cf70d78de3de8cbecd0076
    Signed-off-by: Rodolfo Alonso Hernandez <rodolfo.alonso.hernandez@intel.com>

---
+++ yardstick/common/exceptions.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2017 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Yardstick custom exceptions"""
+
+from oslo_utils import excutils
+
+
+class YardstickException(Exception):
+    """Base Yardstick Exception.
+
+    To correctly use this class, inherit from it and define
+    a 'message' property. That message will get printf'd
+    with the keyword arguments provided to the constructor.
+
+    Based on NeutronException class.
+    """
+    message = "An unknown exception occurred."
+
+    def __init__(self, **kwargs):
+        try:
+            super(YardstickException, self).__init__(self.message % kwargs)
+            self.msg = self.message % kwargs
+        except Exception: # pylint: disable=W0703
+            with excutils.save_and_reraise_exception() as ctxt:
+                if not self.use_fatal_exceptions():
+                    ctxt.reraise = False
+                    # At least get the core message out if something happened
+                    super(YardstickException, self).__init__(self.message)
+
+    def __str__(self):
+        return self.msg
+
+    def use_fatal_exceptions(self):
+        """Is the instance using fatal exceptions.
+
+        :returns: Always returns False.
+        """
+        return False
+
+
+class HeatTemplateError(YardstickException):
+    """Error in Heat during the stack deployment"""
+    message = ('Error in Heat during the creation of the OpenStack stack '
+               '"%(stack_name)"')
