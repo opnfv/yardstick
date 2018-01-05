60b6d9011c8f68602fd82a6e6885a37821fdd53a
JingLu5 <lvjing5@huawei.com>
2018-01-05 01:44:12 +0000

    Integrate TOM tool
    JIRA: YARDSTICK-913
    
    This patch integrates TOM tool
    The TOM code is located at 'yardstick/resource/ directory.
    Yardstick provides ansible scripts to run TOM in Yardstick container.
    Yardstick also provide a script to generate the test data csv file.
    
    Change-Id: Ia1ad846a1b8f174fd3cd0e12f5a1ab57e380f260
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

---
+++ yardstick/resources/TOM/generate_tom_csv.py
@@ -0,0 +1,31 @@
+##############################################################################
+# Copyright (c) 2017 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+from __future__ import absolute_import
+
+import sys
+import csv
+
+from api.utils import influx
+
+def get_influx_data(args):
+    data = influx.query(args)
+    return data
+
+def write_csv(data):
+    with open('output.csv', 'wb') as csvfile:
+        w = csv.writer(csvfile)
+        # Write the header
+        fieldnames=data[0].keys()
+        w.writerow(fieldnames)
+        for row in data:
+            w.writerow(row.values())
+
+if __name__ == '__main__':
+    data = get_influx_data(sys.argv[1])
+    write_csv(data)
