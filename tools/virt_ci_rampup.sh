ec847e56d0a822291eea4c884428eec633071fbe
Stepan Andrushko <stepanx.andrushko@intel.com>
2018-01-11 19:51:45 +0200

    Check RS for virtualized OpenStack/Yardstick
    Draft for env requirements reading and validation
    Script can be run from tools/virt_ci_rampup.sh. First requirement file is
    parsed/stored. This file must be created in ahead and named as
    'infra_deploy.yaml'. Then separate ansible role is used to validate
    resources. Ansible-role fails if there is not enough resources on the host
    for cpu, ram, disk space.
    
    JIRA: YARDSTICK-945
    
    Change-Id: I86cd4bdc963de3aa51392fc73a239cbd426bc64a
    Signed-off-by: Stepan Andrushko <stepanx.andrushko@intel.com>

---
+++ tools/virt_ci_rampup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Copyright (c) 2018 Intel Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ANSIBLE_SCRIPTS="${0%/*}/../ansible"
+
+cd ${ANSIBLE_SCRIPTS} &&\
+ansible-playbook \
+         -e rs_file='../etc/infra/infra_deploy.yaml' \
+         -i inventory.ini infra_deploy.yml
