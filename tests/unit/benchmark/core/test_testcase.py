05cf6ff03d3c6b9b9d85563bee94a1025467cd7d
JingLu5 <lvjing5@huawei.com>
2018-01-19 01:20:01 +0000

    [WIP]enhance yardstick command to support extending test cases in a batch
    JIRA: YARDSTICK-910
    
    Task: enhance yardstick command to support extending test cases in a batch
    1. support tar to be upload for yardstick command to enable more test cases in a batch.
    2. add test cases extend function in CLI
    3. the path of tar need to be the same with yardstick repo path
    
    Change-Id: I2ca8ff43e32f296684408b4fcbf7c1fb1738fb95
    Signed-off-by: JingLu5 <lvjing5@huawei.com>

--- tests/unit/benchmark/core/test_testcase.py
+++ tests/unit/benchmark/core/test_testcase.py
@@ -12,6 +12,7 @@
 # Unittest for yardstick.cmd.commands.testcase
 
 from __future__ import absolute_import
+import mock
 import unittest
 
 from yardstick.benchmark.core import testcase
@@ -21,13 +22,14 @@ class Arg(object):
 
     def __init__(self):
         self.casename = ('opnfv_yardstick_tc001',)
+        self.input_file = 'extended_testcases.tar'
 
 
 class TestcaseUT(unittest.TestCase):
 
     def test_list_all(self):
         t = testcase.Testcase()
-        result = t.list_all("")
+        result = t.list_all()
         self.assertIsInstance(result, list)
 
     def test_show(self):
@@ -36,6 +38,22 @@ class TestcaseUT(unittest.TestCase):
         result = t.show(casename)
         self.assertTrue(result)
 
+    @mock.patch('yardstick.benchmark.core.testcase.shutil.copytree')
+    def test_enable(self, copytree_mock):
+        t = testcase.Testcase()
+        input_file = Arg()
+        result = t.enable(input_file)
+        copytree_mock.assert_called()
+        self.assertTrue(result)
+
+    @mock.patch('yardstick.benchmark.core.testcase.shutil.rmtree')
+    @mock.patch('yardstick.benchmark.core.testcase.shutil.copytree')
+    def test_disable(self, rmtree_mock, copytree_mock):
+        t = testcase.Testcase()
+        result = t.disable()
+        rmtree_mock.assert_called()
+        copytree_mock.assert_called()
+        self.assertTrue(result)
 
 def main():
     unittest.main()
