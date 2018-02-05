530f13fe01e1cd593d446cbb07acd613011bab81
Shobhi Jain <shobhi.jain@intel.com>
2018-02-05 15:18:43 +0000

    Replace neutron network creation with shade.
    Function create_neutron_net now uses shade client instead of neutron
    client.
    
    JIRA: YARDSTICK-890
    
    Change-Id: I456078e98550901c1f736640c9f9bd0d5f9c3df6
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/tests/unit/benchmark/scenarios/lib/test_create_network.py
+++ yardstick/tests/unit/benchmark/scenarios/lib/test_create_network.py
@@ -6,25 +6,53 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+
+from oslo_utils import uuidutils
 import unittest
 import mock
 
-from yardstick.benchmark.scenarios.lib.create_network import CreateNetwork
+from yardstick.common import openstack_utils
+from yardstick.common import exceptions
+from yardstick.benchmark.scenarios.lib import create_network
 
 
 class CreateNetworkTestCase(unittest.TestCase):
 
-    @mock.patch('yardstick.common.openstack_utils.get_neutron_client')
-    @mock.patch('yardstick.common.openstack_utils.create_neutron_net')
-    def test_create_network(self, mock_get_neutron_client, mock_create_neutron_net):
-        options = {
-            'openstack_paras': {
-                'name': 'yardstick_net',
-                'admin_state_up': 'True'
-            }
-        }
-        args = {"options": options}
-        obj = CreateNetwork(args, {})
-        obj.run({})
-        self.assertTrue(mock_get_neutron_client.called)
-        self.assertTrue(mock_create_neutron_net.called)
+    def setUp(self):
+
+        self._mock_create_neutron_net = mock.patch.object(
+            openstack_utils, 'create_neutron_net')
+        self.mock_create_neutron_net = self._mock_create_neutron_net.start()
+        self._mock_get_shade_client = mock.patch.object(
+            openstack_utils, 'get_shade_client')
+        self.mock_get_shade_client = self._mock_get_shade_client.start()
+        self._mock_log = mock.patch.object(create_network, 'LOG')
+        self.mock_log = self._mock_log.start()
+        self.args = {'options': {'network_name': 'yardstick_net'}}
+        self.result = {"network_create": 0}
+
+        self._cnet_obj = create_network.CreateNetwork(self.args, mock.ANY)
+        self.addCleanup(self._stop_mock)
+
+    def _stop_mock(self):
+        self._mock_create_neutron_net.stop()
+        self._mock_get_shade_client.stop()
+        self._mock_log.stop()
+
+    def test_run(self):
+        _uuid = uuidutils.generate_uuid()
+        self._cnet_obj.scenario_cfg = {'output': 'id'}
+        self.mock_create_neutron_net.return_value = _uuid
+        output = self._cnet_obj.run(self.result)
+        self.assertDictEqual({"network_create": 1}, self.result)
+        self.assertDictEqual({'id': _uuid}, output)
+        self.mock_log.info.asset_called_once_with('Create network successful!')
+
+    def test_run_fail_exception(self):
+        self._cnet_obj.scenario_cfg = {'output': 'id'}
+        self.mock_create_neutron_net.return_value = None
+        with self.assertRaises(exceptions.ScenarioCreateNetworkError):
+            self._cnet_obj.run(self.result)
+        self.assertDictEqual({"network_create": 0}, self.result)
+        self.mock_log.error.assert_called_once_with(
+            'Create network failed!')
