cf173c39286cbf2475dce39f21867e51c03b2e72
Shobhi Jain <shobhi.jain@intel.com>
2018-02-07 11:08:05 +0000

    Replace neutron subnet creation with shade.
    Function 'create_neutron_subnet' now uses shade client
    instead of neutron client.
    
    JIRA: YARDSTICK-890
    
    Change-Id: I8eb5295cecd73742bfb5a7d0764af6f45ef0685e
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/tests/unit/common/test_openstack_utils.py
+++ yardstick/tests/unit/common/test_openstack_utils.py
@@ -109,3 +109,29 @@ class CreateNeutronNetTestCase(unittest.TestCase):
         mock_logger.exception.assert_called_once_with(
             "Error [create_neutron_net(shade_client)]")
         self.assertIsNone(output)
+
+
+class CreateNeutronSubnetTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_shade_client = mock.Mock()
+        self.network_name_or_id = 'name_or_id'
+        self.mock_shade_client.create_subnet = mock.Mock()
+
+    def test_create_neutron_subnet(self):
+        _uuid = uuidutils.generate_uuid()
+        self.mock_shade_client.create_subnet.return_value = {'id': _uuid}
+        output = openstack_utils.create_neutron_subnet(
+            self.mock_shade_client, self.network_name_or_id)
+        self.assertEqual(_uuid, output)
+
+    @mock.patch.object(openstack_utils, 'log')
+    def test_create_neutron_subnet_exception(self, mock_logger):
+        self.mock_shade_client.create_subnet.side_effect = (
+            exc.OpenStackCloudException('error message'))
+
+        output = openstack_utils.create_neutron_subnet(
+            self.mock_shade_client, self.network_name_or_id)
+        mock_logger.exception.assert_called_once_with(
+            "Error [create_neutron_subnet(shade_client)]")
+        self.assertIsNone(output)
