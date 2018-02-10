83a220f94349aa805f6356dd61bc03fe7a17bb95
Chornyi, TarasX <tarasx.chornyi@intel.com>
2018-02-10 20:30:06 +0200

    WIP: vFW scale-up template.
    Added topology and trafic profile templates
    Added support for usage Jinga templates in topology defenition
    Added support for static pipeline configs for SampleVNFs
    
    TODO: update unittest.
    
    Change-Id: Iab99fd5b5ad69ca32ee70b9fe47779387ad27e7f
    Signed-off-by: Chornyi, TarasX <tarasx.chornyi@intel.com>

--- yardstick/benchmark/scenarios/networking/vnf_generic.py
+++ yardstick/benchmark/scenarios/networking/vnf_generic.py
@@ -100,12 +100,7 @@ class NetworkServiceTestCase(scenario_base.Scenario):
         self.scenario_cfg = scenario_cfg
         self.context_cfg = context_cfg
 
-        # fixme: create schema to validate all fields have been provided
-        with utils.open_relative_file(scenario_cfg["topology"],
-                                      scenario_cfg['task_path']) as stream:
-            topology_yaml = yaml_load(stream)
-
-        self.topology = topology_yaml["nsd:nsd-catalog"]["nsd"][0]
+        self._render_topology()
         self.vnfs = []
         self.collector = None
         self.traffic_profile = None
@@ -184,6 +179,12 @@ class NetworkServiceTestCase(scenario_base.Scenario):
         with utils.open_relative_file(profile, path) as infile:
             return infile.read()
 
+    def _get_topology(self):
+        topology = self.scenario_cfg["topology"]
+        path = self.scenario_cfg["task_path"]
+        with utils.open_relative_file(topology, path) as infile:
+            return infile.read()
+
     def _fill_traffic_profile(self):
         tprofile = self._get_traffic_profile()
         extra_args = self.scenario_cfg.get('extra_args', {})
@@ -197,6 +198,15 @@ class NetworkServiceTestCase(scenario_base.Scenario):
 
         traffic_vnfd = vnfdgen.generate_vnfd(tprofile, tprofile_data)
         self.traffic_profile = tprofile_base.TrafficProfile.get(traffic_vnfd)
+
+    def _render_topology(self):
+        topology = self._get_topology()
+        topology_args = self.scenario_cfg.get('extra_args', {})
+        topolgy_data = {
+            'extra_args': topology_args
+        }
+        topology_yaml = vnfdgen.generate_vnfd(topology, topolgy_data)
+        self.topology = topology_yaml["nsd:nsd-catalog"]["nsd"][0]
 
     def _find_vnf_name_from_id(self, vnf_id):
         return next((vnfd["vnfd-id-ref"]
