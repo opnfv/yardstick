b7513baa593bc9715abe652ef337c923619ead21
Rodolfo Alonso Hernandez <rodolfo.alonso.hernandez@intel.com>
2018-02-13 17:17:22 +0000

    Mock TRex STL libraries globally
    TRex Python libraries are not going to be available during
    unit testing execution. Those modules calling TRex libraries
    should be mocked individually. This patch will remove the need
    of mocking those libraries per module.
    
    JIRA: YARDSTICK-1010
    
    Change-Id: I4aa11d43ecf32a3dad78f869541b4afea4ec1d28
    Signed-off-by: Rodolfo Alonso Hernandez <rodolfo.alonso.hernandez@intel.com>

--- yardstick/tests/unit/__init__.py
+++ yardstick/tests/unit/__init__.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import absolute_import
+import sys
+
 import mock
 
 
@@ -74,3 +75,6 @@ STL_MOCKS = {
     'trex_stl_lib.zlib': mock.MagicMock(),
     'trex_stl_lib.zmq': mock.MagicMock(),
 }
+
+mock_stl = mock.patch.dict(sys.modules, STL_MOCKS)
+mock_stl.start()
