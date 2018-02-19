cff15075a511710b01a7fa0f78105f751fb6317a
Shobhi Jain <shobhi.jain@intel.com>
2018-02-19 16:50:51 +0000

    Replace neutron router deletion with shade.
    Function delete_neutron_router now uses shade client instead of neutron
    client.
    
    JIRA: YARDSTICK-890
    
    Change-Id: I5b557b640396f343b85b5f5eff415384decc083c
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/tests/unit/benchmark/scenarios/lib/test_delete_router.py
+++ yardstick/tests/unit/benchmark/scenarios/lib/test_delete_router.py
@@ -6,22 +6,49 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+
+from oslo_utils import uuidutils
 import unittest
 import mock
 
-from yardstick.benchmark.scenarios.lib.delete_router import DeleteRouter
+from yardstick.common import openstack_utils
+from yardstick.common import exceptions
+from yardstick.benchmark.scenarios.lib import delete_router
 
 
 class DeleteRouterTestCase(unittest.TestCase):
 
-    @mock.patch('yardstick.common.openstack_utils.get_neutron_client')
-    @mock.patch('yardstick.common.openstack_utils.delete_neutron_router')
-    def test_delete_router(self, mock_get_neutron_client, mock_delete_neutron_router):
-        options = {
-            'router_id': '123-123-123'
-        }
-        args = {"options": options}
-        obj = DeleteRouter(args, {})
-        obj.run({})
-        self.assertTrue(mock_get_neutron_client.called)
-        self.assertTrue(mock_delete_neutron_router.called)
+    def setUp(self):
+        self._mock_delete_neutron_router = mock.patch.object(
+            openstack_utils, 'delete_neutron_router')
+        self.mock_delete_neutron_router = (
+            self._mock_delete_neutron_router.start())
+        self._mock_get_shade_client = mock.patch.object(
+            openstack_utils, 'get_shade_client')
+        self.mock_get_shade_client = self._mock_get_shade_client.start()
+        self._mock_log = mock.patch.object(delete_router, 'LOG')
+        self.mock_log = self._mock_log.start()
+        self.args = {'options': {'router_id': uuidutils.generate_uuid()}}
+        self.result = {"delete_router": 0}
+
+        self._del_obj = delete_router.DeleteRouter(self.args, mock.ANY)
+
+        self.addCleanup(self._stop_mock)
+
+    def _stop_mock(self):
+        self._mock_delete_neutron_router.stop()
+        self._mock_get_shade_client.stop()
+        self._mock_log.stop()
+
+    def test_run(self):
+        self.mock_delete_neutron_router.return_value = True
+        self.assertIsNone(self._del_obj.run(self.result))
+        self.assertEqual({"delete_router": 1}, self.result)
+        self.mock_log.info.assert_called_once_with("Delete router successful!")
+
+    def test_run_fail(self):
+        self.mock_delete_neutron_router.return_value = False
+        with self.assertRaises(exceptions.ScenarioDeleteRouterError):
+            self._del_obj.run(self.result)
+        self.assertEqual({"delete_router": 0}, self.result)
+        self.mock_log.error.assert_called_once_with("Delete router failed!")
