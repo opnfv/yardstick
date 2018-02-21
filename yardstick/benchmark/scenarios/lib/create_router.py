ce5399af22de264e497bb878861630bccf669435
Shobhi Jain <shobhi.jain@intel.com>
2018-02-21 14:45:21 +0000

    Replace neutron router creation with shade.
    Function create_neutron_router now uses shade client instead of neutron
    client.
    
    JIRA: YARDSTICK-890
    
    Change-Id: I819d26f4edb3af5e50f777e8ddbadf4d24820b7f
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/benchmark/scenarios/lib/create_router.py
+++ yardstick/benchmark/scenarios/lib/create_router.py
@@ -7,13 +7,10 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-from __future__ import print_function
-from __future__ import absolute_import
-
 import logging
 
 from yardstick.benchmark.scenarios import base
-import yardstick.common.openstack_utils as op_utils
+from yardstick.common import openstack_utils
 
 LOG = logging.getLogger(__name__)
 
@@ -28,9 +25,16 @@ class CreateRouter(base.Scenario):
         self.context_cfg = context_cfg
         self.options = self.scenario_cfg['options']
 
-        self.openstack = self.options.get("openstack_paras", None)
+        self.name = self.options.get('name', None)
+        self.admin_state_up = self.options.get('admin_state_up', True)
+        self.ext_gateway_net_id = self.options.get('ext_gateway_net_id', None)
+        self.enable_snat = self.options.get('enable_snat', None)
+        self.ext_fixed_ips = self.options.get('ext_fixed_ips', None)
+        self.project_id = self.options.get('project_id', None)
+        self.availability_zone_hints = self.options.get(
+            'availability_zone_hints', None)
 
-        self.neutron_client = op_utils.get_neutron_client()
+        self.shade_client = openstack_utils.get_shade_client()
 
         self.setup_done = False
 
@@ -39,28 +43,21 @@ class CreateRouter(base.Scenario):
 
         self.setup_done = True
 
-    def run(self, result):
+    def run(self, *args):
         """execute the test"""
 
         if not self.setup_done:
             self.setup()
 
-        openstack_paras = {'router': self.openstack}
-        router_id = op_utils.create_neutron_router(self.neutron_client,
-                                                   openstack_paras)
-        if router_id:
-            result.update({"network_create": 1})
-            LOG.info("Create router successful!")
-        else:
-            result.update({"network_create": 0})
+        router_id = openstack_utils.create_neutron_router(
+            self.shade_client, self.name, self.admin_state_up,
+            self.ext_gateway_net_id, self.enable_snat, self.ext_fixed_ips,
+            self.project_id, self.availability_zone_hints)
+        if not router_id:
             LOG.error("Create router failed!")
+            return
 
-        check_result = router_id
-
-        try:
-            keys = self.scenario_cfg.get('output', '').split()
-        except KeyError:
-            pass
-        else:
-            values = [check_result]
-            return self._push_to_outputs(keys, values)
+        LOG.info("Create router successful!")
+        keys = self.scenario_cfg.get('output', '').split()
+        values = [router_id]
+        return self._push_to_outputs(keys, values)
