112081c86c5c7e4d34651410117033b2a3e6a0f7
Shobhi Jain <shobhi.jain@intel.com>
2018-02-21 14:45:21 +0000

    Replace neutron router creation with shade.
    Function create_neutron_router now uses shade client instead of neutron
    client.
    
    JIRA: YARDSTICK-890
    
    Change-Id: I819d26f4edb3af5e50f777e8ddbadf4d24820b7f
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/tests/unit/benchmark/scenarios/lib/test_create_router.py
+++ yardstick/tests/unit/benchmark/scenarios/lib/test_create_router.py
@@ -6,25 +6,49 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+
+from oslo_utils import uuidutils
 import unittest
 import mock
 
-from yardstick.benchmark.scenarios.lib.create_router import CreateRouter
+from yardstick.common import openstack_utils
+from yardstick.benchmark.scenarios.lib import create_router
 
 
 class CreateRouterTestCase(unittest.TestCase):
 
-    @mock.patch('yardstick.common.openstack_utils.get_neutron_client')
-    @mock.patch('yardstick.common.openstack_utils.create_neutron_router')
-    def test_create_router(self, mock_get_neutron_client, mock_create_neutron_router):
-        options = {
-            'openstack_paras': {
-                'admin_state_up': 'True',
-                'name': 'yardstick_router'
-            }
-        }
-        args = {"options": options}
-        obj = CreateRouter(args, {})
-        obj.run({})
-        self.assertTrue(mock_get_neutron_client.called)
-        self.assertTrue(mock_create_neutron_router.called)
+    def setUp(self):
+
+        self._mock_create_neutron_router = mock.patch.object(
+            openstack_utils, 'create_neutron_router')
+        self.mock_create_neutron_router = (
+            self._mock_create_neutron_router.start())
+        self._mock_get_shade_client = mock.patch.object(
+            openstack_utils, 'get_shade_client')
+        self.mock_get_shade_client = self._mock_get_shade_client.start()
+        self._mock_log = mock.patch.object(create_router, 'LOG')
+        self.mock_log = self._mock_log.start()
+        self.args = {'options': {'name': 'yardstick_net'}}
+
+        self._crouter_obj = create_router.CreateRouter(self.args, mock.ANY)
+        self.addCleanup(self._stop_mock)
+
+    def _stop_mock(self):
+        self._mock_create_neutron_router.stop()
+        self._mock_get_shade_client.stop()
+        self._mock_log.stop()
+
+    def test_run(self):
+        _uuid = uuidutils.generate_uuid()
+        self._crouter_obj.scenario_cfg = {'output': 'id'}
+        self.mock_create_neutron_router.return_value = _uuid
+        output = self._crouter_obj.run()
+        self.assertDictEqual({'id': _uuid}, output)
+        self.mock_log.info.asset_called_once_with('Create router successful!')
+
+    def test_run_fail(self):
+        self._crouter_obj.scenario_cfg = {'output': 'id'}
+        self.mock_create_neutron_router.return_value = None
+        output = self._crouter_obj.run()
+        self.assertEqual(None, output)
+        self.mock_log.error.assert_called_once_with('Create router failed!')
