68c934be759962d18d109fde35726f416088a126
Shobhi Jain <shobhi.jain@intel.com>
2018-02-21 16:13:16 +0000

    Replace neutron router interface deletion with shade.
    Function remove_interface_router now uses shade client instead of neutron
    client.
    
    JIRA: YARDSTICK-890
    
    Change-Id: I6bd36e35a339cce64dfa8b69c1e7b56cd70af956
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/tests/unit/benchmark/scenarios/lib/test_delete_router_interface.py
+++ yardstick/tests/unit/benchmark/scenarios/lib/test_delete_router_interface.py
@@ -6,23 +6,51 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+
+from oslo_utils import uuidutils
 import unittest
 import mock
 
-from yardstick.benchmark.scenarios.lib.delete_router_interface import DeleteRouterInterface
+from yardstick.benchmark.scenarios.lib import delete_router_interface
+from yardstick.common import openstack_utils
+from yardstick.common import exceptions
 
 
 class DeleteRouterInterfaceTestCase(unittest.TestCase):
 
-    @mock.patch('yardstick.common.openstack_utils.get_neutron_client')
-    @mock.patch('yardstick.common.openstack_utils.remove_interface_router')
-    def test_delete_router_interface(self, mock_get_neutron_client, mock_remove_interface_router):
-        options = {
-            'router_id': '123-123-123',
-            'subnet_id': '321-321-321'
-        }
-        args = {"options": options}
-        obj = DeleteRouterInterface(args, {})
-        obj.run({})
-        mock_get_neutron_client.assert_called_once()
-        mock_remove_interface_router.assert_called_once()
+    def setUp(self):
+        self._mock_remove_router_interface = mock.patch.object(
+            openstack_utils, 'remove_router_interface')
+        self.mock_remove_router_interface = (
+            self._mock_remove_router_interface.start())
+        self._mock_get_shade_client = mock.patch.object(
+            openstack_utils, 'get_shade_client')
+        self.mock_get_shade_client = self._mock_get_shade_client.start()
+        self._mock_log = mock.patch.object(delete_router_interface, 'LOG')
+        self.mock_log = self._mock_log.start()
+        self.args = {'options': {'router': uuidutils.generate_uuid()}}
+        self.result = {}
+        self.delrout_obj = delete_router_interface.DeleteRouterInterface(
+            self.args, mock.ANY)
+
+        self.addCleanup(self._stop_mock)
+
+    def _stop_mock(self):
+        self._mock_remove_router_interface.stop()
+        self._mock_get_shade_client.stop()
+        self._mock_log.stop()
+
+    def test_run(self):
+        self.mock_remove_router_interface.return_value = True
+        self.assertIsNone(self.delrout_obj.run(self.result))
+        self.assertEqual({"delete_router_interface": 1}, self.result)
+        self.mock_log.info.assert_called_once_with(
+            "Delete router interface successful!")
+
+    def test_run_fail(self):
+        self.mock_remove_router_interface.return_value = False
+        with self.assertRaises(exceptions.ScenarioRemoveRouterIntError):
+            self.delrout_obj.run(self.result)
+        self.assertEqual({"delete_router_interface": 0}, self.result)
+        self.mock_log.error.assert_called_once_with(
+            "Delete router interface failed!")
