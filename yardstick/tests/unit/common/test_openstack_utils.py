e7d94b376252d12e6834721726933b4802aafabc
Shobhi Jain <shobhi.jain@intel.com>
2018-02-23 14:59:09 +0000

    Replace neutron floating ip creation with shade.
    Function create_floating_ip now uses shade client instead of neutron
    client.
    
    JIRA: YARDSTICK-890
    
    Change-Id: I3defd691dad998cebf98442b52f0555b176f1af4
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/tests/unit/common/test_openstack_utils.py
+++ yardstick/tests/unit/common/test_openstack_utils.py
@@ -208,3 +208,27 @@ class RemoveRouterInterfaceTestCase(unittest.TestCase):
             self.mock_shade_client, self.router)
         mock_logger.error.assert_called_once()
         self.assertFalse(output)
+
+
+class CreateFloatingIpTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_shade_client = mock.Mock()
+        self.network_name_or_id = 'name'
+        self.mock_shade_client.create_floating_ip = mock.Mock()
+
+    def test_create_floating_ip(self):
+        self.mock_shade_client.create_floating_ip.return_value = \
+            {'floating_ip_address': 'value1', 'id': 'value2'}
+        output = openstack_utils.create_floating_ip(self.mock_shade_client,
+                                                    self.network_name_or_id)
+        self.assertEqual({'fip_addr': 'value1', 'fip_id': 'value2'}, output)
+
+    @mock.patch.object(openstack_utils, 'log')
+    def test_create_floating_ip_exception(self, mock_logger):
+        self.mock_shade_client.create_floating_ip.side_effect = (
+            exc.OpenStackCloudException('error message'))
+        output = openstack_utils.create_floating_ip(
+            self.mock_shade_client, self.network_name_or_id)
+        mock_logger.error.assert_called_once()
+        self.assertIsNone(output)
