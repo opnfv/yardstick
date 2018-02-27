6a8451fa3c983b76e1ca8be02cdd33dd344f8084
Shobhi Jain <shobhi.jain@intel.com>
2018-02-27 13:19:00 +0000

    Replace neutron floating ip deletion with shade.
    Function delete_floating_ip now uses shade client.
    
    JIRA: YARDSTICK-890
    
    Change-Id: I960630926b664266afbe7be00bb1352243b41be0
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/benchmark/scenarios/lib/delete_floating_ip.py
+++ yardstick/benchmark/scenarios/lib/delete_floating_ip.py
@@ -7,13 +7,12 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-from __future__ import print_function
-from __future__ import absolute_import
-
 import logging
 
+from yardstick.common import openstack_utils
+from yardstick.common import exceptions
 from yardstick.benchmark.scenarios import base
-import yardstick.common.openstack_utils as op_utils
+
 
 LOG = logging.getLogger(__name__)
 
@@ -28,9 +27,10 @@ class DeleteFloatingIp(base.Scenario):
         self.context_cfg = context_cfg
         self.options = self.scenario_cfg['options']
 
-        self.floating_ip_id = self.options.get("floating_ip_id", None)
+        self.floating_ip_id = self.options["floating_ip_id"]
+        self.retry = self.options.get("retry", 1)
 
-        self.nova_client = op_utils.get_nova_client()
+        self.shade_client = openstack_utils.get_shade_client()
         self.setup_done = False
 
     def setup(self):
@@ -44,11 +44,13 @@ class DeleteFloatingIp(base.Scenario):
         if not self.setup_done:
             self.setup()
 
-        status = op_utils.delete_floating_ip(nova_client=self.nova_client,
-                                             floatingip_id=self.floating_ip_id)
-        if status:
-            result.update({"delete_floating_ip": 1})
-            LOG.info("Delete floating ip successful!")
-        else:
+        status = openstack_utils.delete_floating_ip(
+            self.shade_client, self.floating_ip_id,
+            retry=self.retry)
+        if not status:
             result.update({"delete_floating_ip": 0})
             LOG.error("Delete floating ip failed!")
+            raise exceptions.ScenarioDeleteFloatingIPError
+
+        result.update({"delete_floating_ip": 1})
+        LOG.info("Delete floating ip successful!")
