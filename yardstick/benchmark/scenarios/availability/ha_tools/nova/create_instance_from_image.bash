fd1aa4c364ce5c29be1a60e0fcdb314643e7cdfc
Dimitrios Markou <mardim@intracom-telecom.com>
2018-03-03 03:26:41 +0200

    [Bug Fix] Support nova create instance operation
    Add the ability to create a nova instance through the
    operation perspective
    
    JIRA: YARDSTICK-1038
    
    Change-Id: Idac141958d65cc6c53b6bd27df83ac74ae8804ec
    Signed-off-by: Dimitrios Markou <mardim@intracom-telecom.com>

---
+++ yardstick/benchmark/scenarios/availability/ha_tools/nova/create_instance_from_image.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright (c) 2018 Intracom Telecom and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# create nova server
+# parameters: $1 - server name, $2 - image name, $3 - flavor name, $4 - network name
+
+set -e
+
+if [ $OS_INSECURE ] && [ "$(echo $OS_INSECURE | tr '[:upper:]' '[:lower:]')" = "true" ]; then
+    SECURE="--insecure"
+else
+    SECURE=""
+fi
+
+NETNAME="$(openstack ${SECURE} network list -f value -c Name | grep $4)"
+
+openstack ${SECURE} server create $1 --image $2 --flavor $3 --network ${NETNAME}
+
