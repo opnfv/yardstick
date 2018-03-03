f6aca33346a285db176c63764f06df8d80995495
Dimitrios Markou <mardim@intracom-telecom.com>
2018-03-03 03:38:31 +0200

    Assign nova instance to security group
    Support the assigning of a nova instance to an
    existing security group through the operation
    perspective
    
    Change-Id: Ic1c26031c25d400fede2ecb298a66aec42dcfbc5
    Signed-off-by: Dimitrios Markou <mardim@intracom-telecom.com>

---
+++ yardstick/benchmark/scenarios/availability/ha_tools/nova/list_secgroups.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright (c) 2018 Intracom Telecom and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# list security groups
+
+set -e
+
+if [ $OS_INSECURE ] && [ "$(echo $OS_INSECURE | tr '[:upper:]' '[:lower:]')" = "true" ]; then
+    SECURE="--insecure"
+else
+    SECURE=""
+fi
+
+openstack ${SECURE} security group list
+
