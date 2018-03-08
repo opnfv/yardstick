8243b8aa6f96bb07044fec77e846488b079c2706
Ross Brattain <ross.b.brattain@intel.com>
2018-03-08 14:45:06 -0800

    env: validate installer_ip from environment
    validate installer_ip is proper IPv4 address in case of user error
    
    Change-Id: Ib01916bc85ec411c159af1564f5142177f464047
    Signed-off-by: Ross Brattain <ross.b.brattain@intel.com>

--- api/resources/v1/env.py
+++ api/resources/v1/env.py
@@ -10,6 +10,8 @@ from __future__ import absolute_import
 
 import errno
 import logging
+
+import ipaddress
 import os
 import subprocess
 import threading
@@ -269,6 +271,8 @@ class V1Env(ApiResource):
                     LOG.info('Openrc file not found')
                     installer_ip = os.environ.get('INSTALLER_IP',
                                                   '192.168.200.2')
+                    # validate installer_ip is a valid ipaddress
+                    installer_ip = str(ipaddress.IPv4Address(installer_ip))
                     installer_type = os.environ.get('INSTALLER_TYPE', 'compass')
                     LOG.info('Getting openrc file from %s', installer_type)
                     self._get_remote_rc_file(rc_file,
