de2158e9ca18b02fa12ddc74ce862ca6cbd231b4
chenjiankun <chenjiankun1@huawei.com>
2018-03-14 09:41:01 +0000

    Create API to get SUT information
    JIRA: YARDSTICK-1072
    
    We need to show SUT information in GUI.
    
    Change-Id: I40bcd513f3c6a443c82764687637ac5258b99584
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

--- yardstick/common/exceptions.py
+++ yardstick/common/exceptions.py
@@ -3,8 +3,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
+# #      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -112,3 +111,11 @@ class ScenarioCreateSubnetError(YardstickException):
 
 class ScenarioDeleteRouterError(YardstickException):
     message = 'Delete Neutron Router Scenario failed'
+
+
+class MissingPodInfoError(YardstickException):
+    message = 'Missing pod args, please check'
+
+
+class UnsupportedPodFormatError(YardstickException):
+    message = 'Failed to load pod info, unsupported format'
