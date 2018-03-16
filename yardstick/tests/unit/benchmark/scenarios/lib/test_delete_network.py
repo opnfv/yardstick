9bd2af84573dd65da4bab9a08fec34caaa650d28
Shobhi Jain <shobhi.jain@intel.com>
2018-03-16 13:07:55 +0000

    Update neutron delete_neutron_net function.
    Update the changes done in patch [1].
    
    JIRA: YARDSTICK-890
    
    [1] https://gerrit.opnfv.org/gerrit/#/c/52361/
    
    Change-Id: Iccbf16d0a2e72919d5092366241ff41686f01a58
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/tests/unit/benchmark/scenarios/lib/test_delete_network.py
+++ yardstick/tests/unit/benchmark/scenarios/lib/test_delete_network.py
@@ -11,7 +11,8 @@ from oslo_utils import uuidutils
 import unittest
 import mock
 
-import yardstick.common.openstack_utils as op_utils
+from yardstick.common import openstack_utils
+from yardstick.common import exceptions
 from yardstick.benchmark.scenarios.lib import delete_network
 
 
@@ -19,15 +20,15 @@ class DeleteNetworkTestCase(unittest.TestCase):
 
     def setUp(self):
         self._mock_delete_neutron_net = mock.patch.object(
-            op_utils, 'delete_neutron_net')
+            openstack_utils, 'delete_neutron_net')
         self.mock_delete_neutron_net = self._mock_delete_neutron_net.start()
         self._mock_get_shade_client = mock.patch.object(
-            op_utils, 'get_shade_client')
+            openstack_utils, 'get_shade_client')
         self.mock_get_shade_client = self._mock_get_shade_client.start()
         self._mock_log = mock.patch.object(delete_network, 'LOG')
         self.mock_log = self._mock_log.start()
-        _uuid = uuidutils.generate_uuid()
-        self.args = {'options': {'network_id': _uuid}}
+        self.args = {'options': {'network_id': uuidutils.generate_uuid()}}
+        self.result = {}
         self._del_obj = delete_network.DeleteNetwork(self.args, mock.ANY)
 
         self.addCleanup(self._stop_mock)
@@ -39,11 +40,14 @@ class DeleteNetworkTestCase(unittest.TestCase):
 
     def test_run(self):
         self.mock_delete_neutron_net.return_value = True
-        self.assertTrue(self._del_obj.run({}))
+        self.assertIsNone(self._del_obj.run(self.result))
+        self.assertEqual({"delete_network": 1}, self.result)
         self.mock_log.info.assert_called_once_with(
             "Delete network successful!")
 
     def test_run_fail(self):
         self.mock_delete_neutron_net.return_value = False
-        self.assertFalse(self._del_obj.run({}))
+        with self.assertRaises(exceptions.ScenarioDeleteNetworkError):
+            self._del_obj.run(self.result)
+        self.assertEqual({"delete_network": 0}, self.result)
         self.mock_log.error.assert_called_once_with("Delete network failed!")
