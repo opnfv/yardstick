5280ca6c30fd1a14d82e808fd3aeb4eef59e4fd4
Shobhi Jain <shobhi.jain@intel.com>
2018-03-16 13:07:55 +0000

    Update neutron delete_neutron_net function.
    Update the changes done in patch [1].
    
    JIRA: YARDSTICK-890
    
    [1] https://gerrit.opnfv.org/gerrit/#/c/52361/
    
    Change-Id: Iccbf16d0a2e72919d5092366241ff41686f01a58
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/common/exceptions.py
+++ yardstick/common/exceptions.py
@@ -170,3 +170,7 @@ class ScenarioDeleteFloatingIPError(YardstickException):
 
 class ScenarioCreateSecurityGroupError(YardstickException):
     message = 'Create Neutron Security Group Scenario failed'
+
+
+class ScenarioDeleteNetworkError(YardstickException):
+    message = 'Delete Neutron Network Scenario failed'
