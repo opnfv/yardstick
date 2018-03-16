b6b58b5d31217fbde69806a23815a0ce663990de
Stepan Andrushko <stepanx.andrushko@intel.com>
2018-03-16 20:53:06 +0200

    OpenStack deployment using kolla
    OpenStack deployment using Kolla installer for all-in-one mode and
    multinode inside VMs which are already created. Two types of Open Stack
    nodes are supported as input in infra_deploy.yaml.sample: control and compute.
    Kolla build is used to build VMs.
    
    JIRA: YARDSTICK-1078
    
    Change-Id: I51616a7a17ac565a01ec6da57b589290237d18ee
    Signed-off-by: Stepan Andrushko <stepanx.andrushko@intel.com>

--- tools/virt_ci_rampup.sh
+++ tools/virt_ci_rampup.sh
@@ -13,9 +13,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+env_http_proxy=$(sed -ne "s/^http_proxy=[\"\']\(.*\)[\"\']/\1/p" /etc/environment)
+if [[ -z ${http_proxy} ]] && [[ ! -z ${env_http_proxy} ]]; then
+    export http_proxy=${env_http_proxy}
+fi
+env_https_proxy=$(sed -ne "s/^https_proxy=[\"\']\(.*\)[\"\']/\1/p" /etc/environment)
+if [[ -z ${https_proxy} ]] && [[ ! -z ${env_https_proxy} ]]; then
+    export https_proxy=${env_https_proxy}
+fi
+env_ftp_proxy=$(sed -ne "s/^ftp_proxy=[\"\']\(.*\)[\"\']/\1/p" /etc/environment)
+if [[ -z ${ftp_proxy} ]] && [[ ! -z ${env_ftp_proxy} ]]; then
+    export ftp_proxy=${env_ftp_proxy}
+fi
+if [[ ! -z ${http_proxy} ]] || [[ ! -z ${https_proxy} ]]; then
+    export no_proxy="${no_proxy}"
+    extra_args="${extra_args} -e @/tmp/proxy.yml "
+    cat <<EOF > /tmp/proxy.yml
+---
+proxy_env:
+  http_proxy: ${http_proxy}
+  https_proxy: ${https_proxy}
+  ftp_proxy: ${ftp_proxy}
+  no_proxy: ${no_proxy}
+EOF
+fi
 ANSIBLE_SCRIPTS="${0%/*}/../ansible"
 
 cd ${ANSIBLE_SCRIPTS} &&\
 sudo -EH ansible-playbook \
-         -e rs_file='../etc/infra/infra_deploy.yaml' \
+         -e rs_file='../etc/infra/infra_deploy_multi.yaml ' ${extra_args} \
          -i inventory.ini infra_deploy.yml
