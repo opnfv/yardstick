8da6cf719d6fffabc7e7b686d48e8961ef2c79e8
liyin <liyin11@huawei.com>
2018-03-18 14:51:19 +0000

    TC042 bug fix
    JIRA:YARDSTICK-786
    
    yardstick DPDK image have problems to build.
    We will fix this by using nsb image
    the pktgen DPDK testcase will use sampleVNF image
    
    Change-Id: I24b7b6a9acfe21ec956fa9c4ee7ebe7ae1787dbb
    Signed-off-by: liyin <liyin11@huawei.com>

--- yardstick/benchmark/scenarios/networking/testpmd_fwd.bash
+++ yardstick/benchmark/scenarios/networking/testpmd_fwd.bash
@@ -13,6 +13,8 @@ set -e
 
 # Commandline arguments
 DST_MAC=$1         # MAC address of the peer port
+ETH1=$2
+ETH2=$3
 
 load_modules()
 {
@@ -25,13 +27,13 @@ load_modules()
     if lsmod | grep "igb_uio" &> /dev/null ; then
     echo "igb_uio module is loaded"
     else
-    insmod /dpdk/x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
+    insmod /opt/tempT/dpdk-17.02/x86_64-native-linuxapp-gcc/kmod/igb_uio.ko
     fi
 
     if lsmod | grep "rte_kni" &> /dev/null ; then
     echo "rte_kni module is loaded"
     else
-    insmod /dpdk/x86_64-native-linuxapp-gcc/kmod/rte_kni.ko
+    insmod /opt/tempT/dpdk-17.02/x86_64-native-linuxapp-gcc/kmod/rte_kni.ko
     fi
 }
 
@@ -42,15 +44,17 @@ change_permissions()
 }
 
 add_interface_to_dpdk(){
+    ip link set $ETH1 down
+    ip link set $ETH2 down
     interfaces=$(lspci |grep Eth |tail -n +2 |awk '{print $1}')
-    /dpdk/tools/dpdk-devbind.py --bind=igb_uio $interfaces
+    /opt/tempT/dpdk-17.02/usertools//dpdk-devbind.py --bind=igb_uio $interfaces
 }
 
 run_testpmd()
 {
     blacklist=$(lspci |grep Eth |awk '{print $1}'|head -1)
-    cd /dpdk
-    sudo ./destdir/bin/testpmd -c 0x07 -n 4 -b $blacklist -- -a --eth-peer=1,$DST_MAC --forward-mode=mac
+    cd /opt/tempT/dpdk-17.02/x86_64-native-linuxapp-gcc/app
+    sudo ./testpmd -c 0x07 -n 4 -b $blacklist -- -a --eth-peer=1,$DST_MAC --forward-mode=mac
 }
 
 main()
