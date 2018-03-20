71e5a2a758552e952eb65a06f6400a9223880a86
chenjiankun <chenjiankun1@huawei.com>
2018-03-20 09:05:31 +0000

    Bugfix: Error when using API(v1) to run test suite
    The reason is missing TasksHandler parameter to run test suite.
    
    Change-Id: I9dd45caa87d0e39afbf7485443a6e566317f5cea
    Signed-off-by: chenjiankun <chenjiankun1@huawei.com>

---
+++ yardstick/tests/unit/apiserver/resources/v1/test_testsuites.py
@@ -0,0 +1,39 @@
+##############################################################################
+# Copyright (c) 2018 Huawei Technologies Co.,Ltd and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+import mock
+
+import unittest
+
+from yardstick.tests.unit.apiserver import APITestCase
+from api.utils.thread import TaskThread
+
+
+class TestsuiteTestCase(APITestCase):
+
+    def test_run_test_suite(self):
+        if self.app is None:
+            unittest.skip('host config error')
+            return
+
+        TaskThread.start = mock.MagicMock()
+
+        url = 'yardstick/testsuites/action'
+        data = {
+            'action': 'run_test_suite',
+            'args': {
+                'opts': {},
+                'testsuite': 'opnfv_smoke'
+            }
+        }
+        resp = self._post(url, data)
+        self.assertEqual(resp.get('status'), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
