9127cd3804f71058a57f07c07e2aec8fab84f6cc
Shobhi Jain <shobhi.jain@intel.com>
2018-03-22 16:45:13 +0000

    Replace nova create instance with shade client.
    Remove redundant functions:
    *get_instances
    *get_instance_status
    *get_instance_by_name
    
    Function create_instance_and_wait_for_active now uses shade client.
    
    JIRA: YARDSTICK-1088
    
    Change-Id: I766da333dabbb1b83a7b4ede4bf73567ec70eb8c
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/tests/unit/benchmark/scenarios/lib/test_create_server.py
+++ yardstick/tests/unit/benchmark/scenarios/lib/test_create_server.py
@@ -6,29 +6,55 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+from oslo_utils import uuidutils
 import unittest
 import mock
 
-from yardstick.benchmark.scenarios.lib.create_server import CreateServer
+from yardstick.common import openstack_utils
+from yardstick.common import exceptions
+from yardstick.benchmark.scenarios.lib import create_server
 
 
 class CreateServerTestCase(unittest.TestCase):
 
-    @mock.patch('yardstick.common.openstack_utils.create_instance_and_wait_for_active')
-    @mock.patch('yardstick.common.openstack_utils.get_nova_client')
-    @mock.patch('yardstick.common.openstack_utils.get_glance_client')
-    @mock.patch('yardstick.common.openstack_utils.get_neutron_client')
-    def test_create_server(self, mock_get_nova_client, mock_get_neutron_client,
-                           mock_get_glance_client, mock_create_instance_and_wait_for_active):
-        scenario_cfg = {
-            'options': {
-                'openstack_paras': 'example'
-            },
-            'output': 'server'
-        }
-        obj = CreateServer(scenario_cfg, {})
-        obj.run({})
-        mock_get_nova_client.assert_called_once()
-        mock_get_glance_client.assert_called_once()
-        mock_get_neutron_client.assert_called_once()
-        mock_create_instance_and_wait_for_active.assert_called_once()
+    def setUp(self):
+
+        self._mock_create_instance_and_wait_for_active = mock.patch.object(
+            openstack_utils, 'create_instance_and_wait_for_active')
+        self.mock_create_instance_and_wait_for_active = (
+            self._mock_create_instance_and_wait_for_active.start())
+        self._mock_get_shade_client = mock.patch.object(
+            openstack_utils, 'get_shade_client')
+        self.mock_get_shade_client = self._mock_get_shade_client.start()
+        self._mock_log = mock.patch.object(create_server, 'LOG')
+        self.mock_log = self._mock_log.start()
+        self.args = {
+            'options': {'name': 'server-name', 'image': 'image-name',
+                        'flavor': 'flavor-name'}}
+        self.result = {}
+
+        self.cserver_obj = create_server.CreateServer(self.args, mock.ANY)
+        self.addCleanup(self._stop_mock)
+
+    def _stop_mock(self):
+        self._mock_create_instance_and_wait_for_active.stop()
+        self._mock_get_shade_client.stop()
+        self._mock_log.stop()
+
+    def test_run(self):
+        _uuid = uuidutils.generate_uuid()
+        self.cserver_obj.scenario_cfg = {'output': 'id'}
+        self.mock_create_instance_and_wait_for_active.return_value = {
+            'server': {'name': 'server-name', 'flavor': 'flavor-name',
+                       'id': _uuid}}
+        output = self.cserver_obj.run(self.result)
+        self.assertEqual({'instance_create': 1}, self.result)
+        self.assertEqual({'id': _uuid}, output)
+        self.mock_log.info.asset_called_once_with('Create server successful!')
+
+    def test_run_fail(self):
+        self.mock_create_instance_and_wait_for_active.return_value = None
+        with self.assertRaises(exceptions.ScenarioCreateServerError):
+            self.cserver_obj.run(self.result)
+        self.assertEqual({'instance_create': 0}, self.result)
+        self.mock_log.error.assert_called_once_with('Create server failed!')
