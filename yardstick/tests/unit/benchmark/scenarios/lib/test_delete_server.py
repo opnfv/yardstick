fde40fae3d9ebf7c0b25ea4ec7ec28bf61d9e150
Shobhi Jain <shobhi.jain@intel.com>
2018-03-26 14:56:59 +0100

    Replace nova delete instance with shade client.
    Function delete_instance now uses shade client.
    
    JIRA: YARDSTICK-1088
    
    Change-Id: I5772a74c6dc2d103edd410042a8eaf721d1bcc5c
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/tests/unit/benchmark/scenarios/lib/test_delete_server.py
+++ yardstick/tests/unit/benchmark/scenarios/lib/test_delete_server.py
@@ -6,22 +6,49 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+from oslo_utils import uuidutils
 import unittest
 import mock
 
-from yardstick.benchmark.scenarios.lib.delete_server import DeleteServer
+from yardstick.common import openstack_utils
+from yardstick.common import exceptions
+from yardstick.benchmark.scenarios.lib import delete_server
 
 
 class DeleteServerTestCase(unittest.TestCase):
 
-    @mock.patch('yardstick.common.openstack_utils.delete_instance')
-    @mock.patch('yardstick.common.openstack_utils.get_nova_client')
-    def test_delete_server(self, mock_get_nova_client, mock_delete_instance):
-        options = {
-            'server_id': '1234-4567-0000'
-        }
-        args = {"options": options}
-        obj = DeleteServer(args, {})
-        obj.run({})
-        mock_get_nova_client.assert_called_once()
-        mock_delete_instance.assert_called_once()
+    def setUp(self):
+        self._mock_delete_instance = mock.patch.object(
+            openstack_utils, 'delete_instance')
+        self.mock_delete_instance = (
+            self._mock_delete_instance.start())
+        self._mock_get_shade_client = mock.patch.object(
+            openstack_utils, 'get_shade_client')
+        self.mock_get_shade_client = self._mock_get_shade_client.start()
+        self._mock_log = mock.patch.object(delete_server, 'LOG')
+        self.mock_log = self._mock_log.start()
+        self.args = {'options': {'name_or_id': uuidutils.generate_uuid()
+                                 }}
+        self.result = {}
+
+        self.delserver_obj = delete_server.DeleteServer(self.args, mock.ANY)
+
+        self.addCleanup(self._stop_mock)
+
+    def _stop_mock(self):
+        self._mock_delete_instance.stop()
+        self._mock_get_shade_client.stop()
+        self._mock_log.stop()
+
+    def test_run(self):
+        self.mock_delete_instance.return_value = True
+        self.assertIsNone(self.delserver_obj.run(self.result))
+        self.assertEqual({'delete_server': 1}, self.result)
+        self.mock_log.info.assert_called_once_with('Delete server successful!')
+
+    def test_run_fail(self):
+        self.mock_delete_instance.return_value = False
+        with self.assertRaises(exceptions.ScenarioDeleteServerError):
+            self.delserver_obj.run(self.result)
+        self.assertEqual({'delete_server': 0}, self.result)
+        self.mock_log.error.assert_called_once_with('Delete server failed!')
