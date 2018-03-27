33b8208628adca865c721de21209c9d6e5d7e222
Shobhi Jain <shobhi.jain@intel.com>
2018-03-27 14:28:08 +0100

    Replace nova client create keypair with shade.
    Function create_keypair now uses shade client instead
    of nova client.
    
    JIRA: YARDSTICK-1088
    
    Change-Id: I060580504d6969c5ba859ed4494082feb998d37d
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/benchmark/scenarios/lib/create_keypair.py
+++ yardstick/benchmark/scenarios/lib/create_keypair.py
@@ -6,15 +6,11 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
-
-from __future__ import print_function
-from __future__ import absolute_import
-
 import logging
-import paramiko
 
 from yardstick.benchmark.scenarios import base
-import yardstick.common.openstack_utils as op_utils
+from yardstick.common import openstack_utils
+from yardstick.common import exceptions
 
 LOG = logging.getLogger(__name__)
 
@@ -27,10 +23,11 @@ class CreateKeypair(base.Scenario):
     def __init__(self, scenario_cfg, context_cfg):
         self.scenario_cfg = scenario_cfg
         self.context_cfg = context_cfg
-        self.options = self.scenario_cfg['options']
+        self.options = self.scenario_cfg["options"]
 
-        self.key_name = self.options.get("key_name", "yardstick_key")
-        self.key_filename = self.options.get("key_path", "/tmp/yardstick_key")
+        self.name = self.options["key_name"]
+        self.public_key = self.options.get("public_key")
+        self.shade_client = openstack_utils.get_shade_client()
 
         self.setup_done = False
 
@@ -45,27 +42,17 @@ class CreateKeypair(base.Scenario):
         if not self.setup_done:
             self.setup()
 
-        rsa_key = paramiko.RSAKey.generate(bits=2048, progress_func=None)
-        rsa_key.write_private_key_file(self.key_filename)
-        LOG.info("Writing key_file %s ...", self.key_filename)
-        with open(self.key_filename + ".pub", "w") as pubkey_file:
-            pubkey_file.write(
-                "%s %s\n" % (rsa_key.get_name(), rsa_key.get_base64()))
-        del rsa_key
+        keypair = openstack_utils.create_keypair(
+            self.shade_client, self.name, public_key=self.public_key)
 
-        keypair = op_utils.create_keypair(self.key_name,
-                                          self.key_filename + ".pub")
-
-        if keypair:
-            result.update({"keypair_create": 1})
-            LOG.info("Create keypair successful!")
-        else:
+        if not keypair:
             result.update({"keypair_create": 0})
-            LOG.info("Create keypair failed!")
-        try:
-            keys = self.scenario_cfg.get('output', '').split()
-        except KeyError:
-            pass
-        else:
-            values = [keypair.id]
-            return self._push_to_outputs(keys, values)
+            LOG.error("Create keypair failed!")
+            raise exceptions.ScenarioCreateKeypairError
+
+        result.update({"keypair_create": 1})
+        LOG.info("Create keypair successful!")
+        keys = self.scenario_cfg.get("output", '').split()
+        keypair_id = keypair["keypair"]["id"]
+        values = [keypair_id]
+        return self._push_to_outputs(keys, values)
