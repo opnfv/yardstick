0222fb329f4e68321adba807fb1fbe79c38c5650
Shobhi Jain <shobhi.jain@intel.com>
2018-03-27 16:47:04 +0100

    Replace nova delete keypair with shade client.
    Function delete_keypair now uses shade client.
    
    JIRA: YARDSTICK-1088
    
    Change-Id: I46b895748c5c44b0bf7d5b035395676ebff48d7f
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>
    (cherry pick from commit aded0fdb30caf035e0eb94c1216f436ba597c2c7)

--- yardstick/benchmark/scenarios/lib/delete_keypair.py
+++ yardstick/benchmark/scenarios/lib/delete_keypair.py
@@ -6,14 +6,12 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
-
-from __future__ import print_function
-from __future__ import absolute_import
-
 import logging
 
+from yardstick.common import openstack_utils
+from yardstick.common import exceptions
 from yardstick.benchmark.scenarios import base
-import yardstick.common.openstack_utils as op_utils
+
 
 LOG = logging.getLogger(__name__)
 
@@ -26,11 +24,11 @@ class DeleteKeypair(base.Scenario):
     def __init__(self, scenario_cfg, context_cfg):
         self.scenario_cfg = scenario_cfg
         self.context_cfg = context_cfg
-        self.options = self.scenario_cfg['options']
+        self.options = self.scenario_cfg["options"]
 
-        self.key_name = self.options.get("key_name", "yardstick_key")
+        self.key_name = self.options["key_name"]
 
-        self.nova_client = op_utils.get_nova_client()
+        self.shade_client = openstack_utils.get_shade_client()
 
         self.setup_done = False
 
@@ -45,12 +43,13 @@ class DeleteKeypair(base.Scenario):
         if not self.setup_done:
             self.setup()
 
-        status = op_utils.delete_keypair(self.nova_client,
-                                         self.key_name)
+        status = openstack_utils.delete_keypair(self.shade_client,
+                                                self.key_name)
 
-        if status:
-            result.update({"delete_keypair": 1})
-            LOG.info("Delete keypair successful!")
-        else:
+        if not status:
             result.update({"delete_keypair": 0})
-            LOG.info("Delete keypair failed!")
+            LOG.error("Delete keypair failed!")
+            raise exceptions.ScenarioDeleteKeypairError
+
+        result.update({"delete_keypair": 1})
+        LOG.info("Delete keypair successful!")
--- yardstick/tests/unit/benchmark/scenarios/lib/test_delete_keypair.py
+++ yardstick/tests/unit/benchmark/scenarios/lib/test_delete_keypair.py
@@ -9,19 +9,43 @@
 import unittest
 import mock
 
-from yardstick.benchmark.scenarios.lib.delete_keypair import DeleteKeypair
+from yardstick.common import openstack_utils
+from yardstick.common import exceptions
+from yardstick.benchmark.scenarios.lib import delete_keypair
 
 
 class DeleteKeypairTestCase(unittest.TestCase):
 
-    @mock.patch('yardstick.common.openstack_utils.get_nova_client')
-    @mock.patch('yardstick.common.openstack_utils.delete_keypair')
-    def test_detach_volume(self, mock_get_nova_client, mock_delete_keypair):
-        options = {
-            'key_name': 'yardstick_key'
-        }
-        args = {"options": options}
-        obj = DeleteKeypair(args, {})
-        obj.run({})
-        mock_get_nova_client.assert_called_once()
-        mock_delete_keypair.assert_called_once()
+    def setUp(self):
+        self._mock_delete_keypair = mock.patch.object(
+            openstack_utils, 'delete_keypair')
+        self.mock_delete_keypair = self._mock_delete_keypair.start()
+        self._mock_get_shade_client = mock.patch.object(
+            openstack_utils, 'get_shade_client')
+        self.mock_get_shade_client = self._mock_get_shade_client.start()
+        self._mock_log = mock.patch.object(delete_keypair, 'LOG')
+        self.mock_log = self._mock_log.start()
+        self.args = {'options': {'key_name': 'yardstick_key'}}
+        self.result = {}
+        self.delkey_obj = delete_keypair.DeleteKeypair(self.args, mock.ANY)
+
+        self.addCleanup(self._stop_mock)
+
+    def _stop_mock(self):
+        self._mock_delete_keypair.stop()
+        self._mock_get_shade_client.stop()
+        self._mock_log.stop()
+
+    def test_run(self):
+        self.mock_delete_keypair.return_value = True
+        self.assertIsNone(self.delkey_obj.run(self.result))
+        self.assertEqual({'delete_keypair': 1}, self.result)
+        self.mock_log.info.assert_called_once_with(
+            'Delete keypair successful!')
+
+    def test_run_fail(self):
+        self.mock_delete_keypair.return_value = False
+        with self.assertRaises(exceptions.ScenarioDeleteKeypairError):
+            self.delkey_obj.run(self.result)
+        self.assertEqual({'delete_keypair': 0}, self.result)
+        self.mock_log.error.assert_called_once_with("Delete keypair failed!")
