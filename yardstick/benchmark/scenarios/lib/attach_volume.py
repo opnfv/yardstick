6df8edddb0ad266fa2f8f60632928dc632e1aca1
Shobhi Jain <shobhi.jain@intel.com>
2018-03-28 12:19:35 +0100

    Replace nova attach volume to server with shade client.
    Function attach_volume_to_server now uses shade client
    instead of nova client.
    
    JIRA: YARDSTICK-1088
    
    Change-Id: Id00df672c2c195b5c338cbbc30ddf2742a4e4d29
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/benchmark/scenarios/lib/attach_volume.py
+++ yardstick/benchmark/scenarios/lib/attach_volume.py
@@ -6,20 +6,17 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
-
-from __future__ import print_function
-from __future__ import absolute_import
-
 import logging
 
 from yardstick.benchmark.scenarios import base
-import yardstick.common.openstack_utils as op_utils
+from yardstick.common import openstack_utils
+from yardstick.common import exceptions
 
 LOG = logging.getLogger(__name__)
 
 
 class AttachVolume(base.Scenario):
-    """Attach a volmeu to an instance"""
+    """Attach a volume to an instance"""
 
     __scenario_type__ = "AttachVolume"
 
@@ -28,8 +25,12 @@ class AttachVolume(base.Scenario):
         self.context_cfg = context_cfg
         self.options = self.scenario_cfg['options']
 
-        self.server_id = self.options.get("server_id", "TestServer")
-        self.volume_id = self.options.get("volume_id", None)
+        self.server_id = self.options["server_id"]
+        self.volume_id = self.options["volume_id"]
+        self.device = self.options.get("device")
+        self.wait = self.options.get("wait", True)
+        self.timeout = self.options.get("timeout", None)
+        self.shade_client = openstack_utils.get_shade_client()
 
         self.setup_done = False
 
@@ -44,10 +45,14 @@ class AttachVolume(base.Scenario):
         if not self.setup_done:
             self.setup()
 
-        status = op_utils.attach_server_volume(self.server_id,
-                                               self.volume_id)
+        status = openstack_utils.attach_volume_to_server(
+            self.shade_client, self.server_id, self.volume_id,
+            device=self.device, wait=self.wait, timeout=self.timeout)
 
-        if status:
-            LOG.info("Attach volume to server successful!")
-        else:
-            LOG.info("Attach volume to server failed!")
+        if not status:
+            result.update({"attach_volume": 0})
+            LOG.error("Attach volume to server failed!")
+            raise exceptions.ScenarioAttachVolumeError
+
+        result.update({"attach_volume": 1})
+        LOG.info("Attach volume to server successful!")
