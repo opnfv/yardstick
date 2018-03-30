3fc1ea5c0a34ad7585d4d045f12110567ad2fe3c
Shobhi Jain <shobhi.jain@intel.com>
2018-03-30 10:45:54 +0100

    Replace nova get server with shade client.
    Rename get_server_by_name with get_server.
    Function get_server now uses shade client.
    
    JIRA: YARDSTICK-1088
    
    Change-Id: I69c59145cefdb565f3ece27baaaf932905e1b757
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/tests/unit/benchmark/scenarios/lib/test_get_server.py
+++ yardstick/tests/unit/benchmark/scenarios/lib/test_get_server.py
@@ -6,37 +6,52 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
+from oslo_utils import uuidutils
 import unittest
 import mock
 
-from yardstick.benchmark.scenarios.lib.get_server import GetServer
+from yardstick.common import openstack_utils
+from yardstick.common import exceptions
+from yardstick.benchmark.scenarios.lib import get_server
 
 
 class GetServerTestCase(unittest.TestCase):
 
-    @mock.patch('yardstick.common.openstack_utils.get_server_by_name')
-    @mock.patch('yardstick.common.openstack_utils.get_nova_client')
-    def test_get_server_with_name(self, mock_get_nova_client, mock_get_server_by_name):
-        scenario_cfg = {
-            'options': {
-                'server_name': 'yardstick_server'
-            },
-            'output': 'status server'
-        }
-        obj = GetServer(scenario_cfg, {})
-        obj.run({})
-        mock_get_nova_client.assert_called_once()
-        mock_get_server_by_name.assert_called_once()
+    def setUp(self):
 
-    @mock.patch('yardstick.common.openstack_utils.get_nova_client')
-    def test_get_server_with_id(self, mock_get_nova_client):
-        scenario_cfg = {
-            'options': {
-                'server_id': '1'
-            },
-            'output': 'status server'
-        }
-        mock_get_nova_client().servers.get.return_value = None
-        obj = GetServer(scenario_cfg, {})
-        obj.run({})
-        mock_get_nova_client.assert_called()
+        self._mock_get_server = mock.patch.object(
+            openstack_utils, 'get_server')
+        self.mock_get_server = (
+            self._mock_get_server.start())
+        self._mock_get_shade_client = mock.patch.object(
+            openstack_utils, 'get_shade_client')
+        self.mock_get_shade_client = self._mock_get_shade_client.start()
+        self._mock_log = mock.patch.object(get_server, 'LOG')
+        self.mock_log = self._mock_log.start()
+        self.args = {'options': {'server_name': 'yardstick_server'}}
+        self.result = {}
+
+        self.getserver_obj = get_server.GetServer(self.args, mock.ANY)
+        self.addCleanup(self._stop_mock)
+
+    def _stop_mock(self):
+        self.mock_get_server.stop()
+        self._mock_get_shade_client.stop()
+        self._mock_log.stop()
+
+    def test_run(self):
+        _uuid = uuidutils.generate_uuid()
+        self.getserver_obj.scenario_cfg = {'output': 'id'}
+        self.mock_get_server.return_value = {
+            'server': {'name': 'server-name', 'id': _uuid}}
+        output = self.getserver_obj.run(self.result)
+        self.assertDictEqual({'get_server': 1}, self.result)
+        self.assertDictEqual({'id': _uuid}, output)
+        self.mock_log.info.asset_called_once_with('Get Server successful!')
+
+    def test_run_fail(self):
+        self.mock_get_server.return_value = None
+        with self.assertRaises(exceptions.ScenarioGetServerError):
+            self.getserver_obj.run(self.result)
+        self.assertDictEqual({'get_server': 0}, self.result)
+        self.mock_log.error.assert_called_once_with('Get Server failed!')
