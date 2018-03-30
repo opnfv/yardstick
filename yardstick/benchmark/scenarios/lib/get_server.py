2ab92fa9d88405f44cf1d20393fb379b34ed8bca
Shobhi Jain <shobhi.jain@intel.com>
2018-03-30 10:45:54 +0100

    Replace nova get server with shade client.
    Rename get_server_by_name with get_server.
    Function get_server now uses shade client.
    
    JIRA: YARDSTICK-1088
    
    Change-Id: I69c59145cefdb565f3ece27baaaf932905e1b757
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/benchmark/scenarios/lib/get_server.py
+++ yardstick/benchmark/scenarios/lib/get_server.py
@@ -6,78 +6,52 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
-
-from __future__ import print_function
-from __future__ import absolute_import
-
 import logging
 
 from yardstick.benchmark.scenarios import base
-import yardstick.common.openstack_utils as op_utils
+from yardstick.common import openstack_utils
+from yardstick.common import exceptions
 
 LOG = logging.getLogger(__name__)
 
 
 class GetServer(base.Scenario):
-    """Get a server instance
-
-  Parameters
-    server_id - ID of the server
-        type:    string
-        unit:    N/A
-        default: null
-    server_name - name of the server
-        type:    string
-        unit:    N/A
-        default: null
-
-    Either server_id or server_name is required.
-
-  Outputs
-    rc - response code of getting server instance
-        0 for success
-        1 for failure
-        type:    int
-        unit:    N/A
-    server - instance of the server
-        type:    dict
-        unit:    N/A
-    """
+    """Get a server instance"""
 
     __scenario_type__ = "GetServer"
 
     def __init__(self, scenario_cfg, context_cfg):
         self.scenario_cfg = scenario_cfg
         self.context_cfg = context_cfg
-        self.options = self.scenario_cfg.get('options', {})
-
-        self.server_id = self.options.get("server_id")
-        if self.server_id:
-            LOG.debug('Server id is %s', self.server_id)
+        self.options = self.scenario_cfg['options']
 
         default_name = self.scenario_cfg.get('host',
                                              self.scenario_cfg.get('target'))
-        self.server_name = self.options.get('server_name', default_name)
-        if self.server_name:
-            LOG.debug('Server name is %s', self.server_name)
+        self.server_name_or_id = self.options.get('server_name_or_id',
+                                                  default_name)
+        self.filters = self.options.get('filters')
+        self.detailed = self.options.get('detailed', False)
+        self.bare = self.options.get('bare', False)
+        self.all_projects = self.options.get('all_projects', False)
 
-        self.nova_client = op_utils.get_nova_client()
+        self.shade_client = openstack_utils.get_shade_client()
 
     def run(self, result):
         """execute the test"""
 
-        if self.server_id:
-            server = self.nova_client.servers.get(self.server_id)
-        else:
-            server = op_utils.get_server_by_name(self.server_name)
+        server = openstack_utils.get_server(
+            self.shade_client, name_or_id=self.server_name_or_id,
+            filters=self.filters, detailed=self.detailed, bare=self.bare,
+            all_projects=self.all_projects)
 
+        if not server:
+            result.update({"get_server": 0})
+            LOG.error("Get Server failed!")
+            raise exceptions.ScenarioGetServerError
+
+        result.update({"get_server": 1})
+        LOG.info("Get Server successful!")
         keys = self.scenario_cfg.get('output', '').split()
-
-        if server:
-            LOG.info("Get server successful!")
-            values = [0, self._change_obj_to_dict(server)]
-        else:
-            LOG.info("Get server failed!")
-            values = [1]
-
+        server_id = server['server']['id']
+        values = [server_id]
         return self._push_to_outputs(keys, values)
