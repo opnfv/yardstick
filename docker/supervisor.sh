opnfv/yardstick

commit 18546b4f43b9c927c4c91529dd912c7368b941bf

Fix RabbitMQ service installation and initialization
RabbitMQ service added in [1] is not correctly installed and initialized:
- There is an error during the installation process ("\" character
  missing).
- In the installation script, the service needs to be started first.
- In the container installation, the service needs to be started via
  supervisor.

[1]https://gerrit.opnfv.org/gerrit/#/c/53597/

JIRA: YARDSTICK-1103

Change-Id: Iade3d6ce4b522e6f576af71b7afe5559081f7929
Signed-off-by: Rodolfo Alonso Hernandez <rodolfo.alonso.hernandez@intel.com>

docker/supervisor.sh
@@ -24,3 +24,6 @@ directory = /etc/yardstick
 command = uwsgi -i yardstick.ini
 EOF
 fi
+
+[program:rabbitmq]
+command = service rabbitmq-server restart

install.sh
@@ -84,7 +84,7 @@ apt-get update && apt-get install -y \
     libxft-dev \
     libxss-dev \
     sudo \
-    iputils-ping
+    iputils-ping \
     rabbitmq-server
 
 if [[ "${DOCKER_ARCH}" != "aarch64" ]]; then
@@ -95,10 +95,11 @@ apt-get -y autoremove && apt-get clean
 
 git config --global http.sslVerify false
 
-# Configure and restart RabbitMQ
+# Start and configure RabbitMQ
+service rabbitmq-server restart
+rabbitmqctl start_app
 rabbitmqctl add_user yardstick yardstick
 rabbitmqctl set_permissions yardstick ".*" ".*" ".*"
-rabbitmqctl reset
 
 # install yardstick + dependencies
 easy_install -U pip==9.0.1

commit ead90064d12be552bd334828d41ffffa75fad01b

docker/supervisor.sh: Fix condition block end
Fixes: 18546b4f

Change-Id: I51fcb48e790ff7c57273a3f6712ccb2b9bc4aa32
Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

docker/supervisor.sh
@@ -11,7 +11,7 @@
 # nginx service start when boot
 supervisor_config='/etc/supervisor/conf.d/yardstick.conf'
 
-if [[ ! -e "${supervisor_config}" ]];then
+if [[ ! -e "${supervisor_config}" ]]; then
     cat << EOF > "${supervisor_config}"
 [supervisord]
 nodaemon = true
@@ -22,8 +22,8 @@ command = service nginx restart
 [program:yardstick_uwsgi]
 directory = /etc/yardstick
 command = uwsgi -i yardstick.ini
-EOF
-fi
 
 [program:rabbitmq]
 command = service rabbitmq-server restart
+EOF
+fi