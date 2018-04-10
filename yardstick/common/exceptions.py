4a06c196195fa118737b3b1229aa63332f3a5b2e
Rodolfo Alonso Hernandez <rodolfo.alonso.hernandez@intel.com>
2018-04-10 09:15:55 +0100

    Improve IXIA IxNetwork library and traffic profile (2)
    This patch modifies the way the packet frame and the flow is configured
    before the injection.
    
    "IxNextgen.update_frame" modifies the L2 frame according to the
    test case and setup the frame rate, frame size, traffic injection
    duration and MAC addresses.
    
    JIRA: YARDSTICK-1116
    
    Change-Id: Ife08f15a4eda24d7835c92c4172b450854d112ee
    Signed-off-by: Rodolfo Alonso Hernandez <rodolfo.alonso.hernandez@intel.com>
    Signed-off-by: Emma Foley <emma.l.foley@intel.com>
    (cherry picked from commit 8d10a6a8447fc2798491a6f3856e21b85fa309ba)

--- yardstick/common/exceptions.py
+++ yardstick/common/exceptions.py
@@ -222,6 +222,18 @@ class UpdateOpenrcError(ApiServerError):
     message = 'Update openrc ERROR!'
 
 
+class IxNetworkClientNotConnected(YardstickException):
+    message = 'IxNetwork client not connected to a TCL server'
+
+
+class IxNetworkFlowNotPresent(YardstickException):
+    message = 'Flow Group "%(flow_group)s" is not present'
+
+
+class IxNetworkFieldNotPresentInStackItem(YardstickException):
+    message = 'Field "%(field_name)s" not present in stack item %(stack_item)s'
+
+
 class AclMissingActionArguments(YardstickException):
     message = ('Missing ACL action parameter '
                '[action=%(action_name)s parameter=%(action_param)s]')
