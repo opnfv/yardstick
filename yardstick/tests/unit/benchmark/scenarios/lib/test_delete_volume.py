91684fece718f40465cb889036884235d6b51d03
Shobhi Jain <shobhi.jain@intel.com>
2018-04-10 14:13:50 +0100

    Replace cinder delete volume with shade client.
    Function delete volume now uses shade client.
    
    JIRA: YARDSTICK-891
    
    Change-Id: I016e1d3bf5972879cad176b56c7282e35413945e
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/tests/unit/benchmark/scenarios/lib/test_delete_volume.py
+++ yardstick/tests/unit/benchmark/scenarios/lib/test_delete_volume.py
@@ -9,19 +9,44 @@
 import unittest
 import mock
 
-from yardstick.benchmark.scenarios.lib.delete_volume import DeleteVolume
+from yardstick.common import openstack_utils
+from yardstick.common import exceptions
+from yardstick.benchmark.scenarios.lib import delete_volume
 
 
 class DeleteVolumeTestCase(unittest.TestCase):
 
-    @mock.patch('yardstick.common.openstack_utils.get_cinder_client')
-    @mock.patch('yardstick.common.openstack_utils.delete_volume')
-    def test_delete_volume(self, mock_get_cinder_client, mock_delete_volume):
-        options = {
-            'volume_id': '123-123-123'
-        }
-        args = {"options": options}
-        obj = DeleteVolume(args, {})
-        obj.run({})
-        mock_get_cinder_client.assert_called_once()
-        mock_delete_volume.assert_called_once()
+    def setUp(self):
+        self._mock_delete_volume = mock.patch.object(
+            openstack_utils, 'delete_volume')
+        self.mock_delete_volume = (
+            self._mock_delete_volume.start())
+        self._mock_get_shade_client = mock.patch.object(
+            openstack_utils, 'get_shade_client')
+        self.mock_get_shade_client = self._mock_get_shade_client.start()
+        self._mock_log = mock.patch.object(delete_volume, 'LOG')
+        self.mock_log = self._mock_log.start()
+        self.args = {'options': {'volume_name_or_id': 'yardstick_volume'}}
+        self.result = {}
+
+        self.delvol_obj = delete_volume.DeleteVolume(self.args, mock.ANY)
+
+        self.addCleanup(self._stop_mock)
+
+    def _stop_mock(self):
+        self._mock_delete_volume.stop()
+        self._mock_get_shade_client.stop()
+        self._mock_log.stop()
+
+    def test_run(self):
+        self.mock_delete_volume.return_value = True
+        self.assertIsNone(self.delvol_obj.run(self.result))
+        self.assertEqual({'delete_volume': 1}, self.result)
+        self.mock_log.info.assert_called_once_with('Delete volume successful!')
+
+    def test_run_fail(self):
+        self.mock_delete_volume.return_value = False
+        with self.assertRaises(exceptions.ScenarioDeleteVolumeError):
+            self.delvol_obj.run(self.result)
+        self.assertEqual({'delete_volume': 0}, self.result)
+        self.mock_log.error.assert_called_once_with('Delete volume failed!')
