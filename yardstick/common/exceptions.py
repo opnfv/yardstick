4ede392c9392fe90889cba11752769dcf8e3c343
Shobhi Jain <shobhi.jain@intel.com>
2018-04-10 14:13:50 +0100

    Replace cinder delete volume with shade client.
    Function delete volume now uses shade client.
    
    JIRA: YARDSTICK-891
    
    Change-Id: I016e1d3bf5972879cad176b56c7282e35413945e
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>

--- yardstick/common/exceptions.py
+++ yardstick/common/exceptions.py
@@ -206,3 +206,7 @@ class ScenarioGetFlavorError(YardstickException):
 
 class ScenarioCreateVolumeError(YardstickException):
     message = 'Cinder Create Volume Scenario failed'
+
+
+class ScenarioDeleteVolumeError(YardstickException):
+    message = 'Cinder Delete Volume Scenario failed'
