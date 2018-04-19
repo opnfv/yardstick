d7f8717c3beb1854976e77d56823f482497ffff3
Ross Brattain <ross.b.brattain@intel.com>
2018-04-18 22:16:32 -0700

    Fix "os.path" mock problems during tests
    This patch removes the global mocking applied on "os.path".
    
    Change-Id: Ia18d2c90195c5408a1852792bdf05b6f0c1ad21f
    Signed-off-by: Rodolfo Alonso Hernandez <rodolfo.alonso.hernandez@intel.com>

--- yardstick/benchmark/contexts/heat.py
+++ yardstick/benchmark/contexts/heat.py
@@ -7,9 +7,6 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-from __future__ import absolute_import
-from __future__ import print_function
-
 import collections
 import logging
 import os
