8282326645d7debf15fa1f6b24250a990f68c708
Rodolfo Alonso Hernandez <rodolfo.alonso.hernandez@intel.com>
2018-04-22 19:52:49 +0100

    Add "TrafficGeneratorProducer" for "GenericTrafficGen" instances
    Added "TrafficGeneratorProducer" class, a message producer specific for
    "GenericTrafficGen" derived classes.
    
    JIRA: YARDSTICK-1127
    
    Change-Id: Icc87a6920155612e759a1d4d2f29028940c2e040
    Signed-off-by: Rodolfo Alonso Hernandez <rodolfo.alonso.hernandez@intel.com>

--- yardstick/network_services/vnf_generic/vnf/base.py
+++ yardstick/network_services/vnf_generic/vnf/base.py
@@ -18,6 +18,9 @@ import abc
 import logging
 import six
 
+from yardstick.common import messaging
+from yardstick.common.messaging import payloads
+from yardstick.common.messaging import producer
 from yardstick.network_services.helpers.samplevnf_helper import PortPairs
 
 
@@ -138,6 +141,17 @@ class VnfdHelper(dict):
             yield port_name, port_num
 
 
+class TrafficGeneratorProducer(producer.MessagingProducer):
+    """Class implementing the message producer for traffic generators
+
+    This message producer must be instantiated in the process created
+    "run_traffic" process.
+    """
+    def __init__(self, pid):
+        super(TrafficGeneratorProducer, self).__init__(messaging.TOPIC_TG,
+                                                       pid=pid)
+
+
 @six.add_metaclass(abc.ABCMeta)
 class GenericVNF(object):
     """Class providing file-like API for generic VNF implementation
@@ -204,6 +218,7 @@ class GenericTrafficGen(GenericVNF):
         super(GenericTrafficGen, self).__init__(name, vnfd)
         self.runs_traffic = True
         self.traffic_finished = False
+        self._mq_producer = None
 
     @abc.abstractmethod
     def run_traffic(self, traffic_profile):
@@ -254,3 +269,10 @@ class GenericTrafficGen(GenericVNF):
         :return: True/False
         """
         pass
+
+    def _setup_mq_producer(self, pid):
+        """Setup the TG MQ producer to send messages between processes
+
+        :return: (derived class from ``MessagingProducer``) MQ producer object
+        """
+        return TrafficGeneratorProducer(pid)
