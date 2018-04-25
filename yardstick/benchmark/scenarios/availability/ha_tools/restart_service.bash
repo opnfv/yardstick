b961773c8cbeb21813c6d2a28440f263ae58bd0e
Dimitrios Markou <mardim@intracom-telecom.com>
2018-04-25 17:37:03 +0300

    [WIP] Add support for restarting a service
    Change-Id: Id57defb574eb7e0f5be81b52803cdb05f46796ab
    Signed-off-by: Dimitrios Markou <mardim@intracom-telecom.com>

---
+++ yardstick/benchmark/scenarios/availability/ha_tools/restart_service.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+##############################################################################
+# Copyright (c) 2018 Intracom Telecom and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# Restart a service and check the service is started
+
+set -e
+
+service_name=$1
+
+Distributor=$(lsb_release -a | grep "Distributor ID" | awk '{print $3}')
+
+if [ "$Distributor" != "Ubuntu" -a "$service_name" != "keystone" -a "$service_name" != "neutron-server" -a "$service_name" != "haproxy" ]; then
+    service_name="openstack-"${service_name}
+elif [ "$Distributor" = "Ubuntu" -a "$service_name" = "keystone" ]; then
+    service_name="apache2"
+elif [ "$service_name" = "keystone" ]; then
+    service_name="httpd"
+fi
+
+if which systemctl 2>/dev/null; then
+    systemctl restart $service_name
+else
+    service $service_name restart
+fi
+
