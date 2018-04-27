e602a50fbddfaad69cb18e153ba99dcb7da50292
Dimitrios Markou <mardim@intracom-telecom.com>
2018-04-27 11:51:17 +0300

    Bug Fix: Start openvswitch service in Centos distro
    JIRA: YARDSTICK-1142
    
    Change-Id: I82c479c95e10de37416081eacf2d8bee396e50dc
    Signed-off-by: Dimitrios Markou <mardim@intracom-telecom.com>
    (cherry picked from commit d00660e583ef89fe07149c7f0cffcced7cd72133)

--- yardstick/benchmark/scenarios/availability/ha_tools/start_service.bash
+++ yardstick/benchmark/scenarios/availability/ha_tools/start_service.bash
@@ -15,11 +15,9 @@ set -e
 
 service_name=$1
 
-Distributor=$(lsb_release -a | grep "Distributor ID" | awk '{print $3}')
-
-if [ "$Distributor" != "Ubuntu" -a "$service_name" != "keystone" -a "$service_name" != "neutron-server" -a "$service_name" != "haproxy" ]; then
+if [ -f /usr/bin/yum -a "$service_name" != "keystone" -a "$service_name" != "neutron-server" -a "$service_name" != "haproxy" -a "$service_name" != "openvswitch" ]; then
     service_name="openstack-"${service_name}
-elif [ "$Distributor" = "Ubuntu" -a "$service_name" = "keystone" ]; then
+elif [ -f /usr/bin/apt -a "$service_name" = "keystone" ]; then
     service_name="apache2"
 elif [ "$service_name" = "keystone" ]; then
     service_name="httpd"
