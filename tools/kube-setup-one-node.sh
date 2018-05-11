eec7c400585e0a3165af98bd816ce48a1907a018
Mytnyk, Volodymyr <volodymyrx.mytnyk@intel.com>
2018-05-11 17:36:20 +0100

    Add ansible scripts to deploy Kubernetes
    For know, only one node is supported.
    
    - build/configure CNI drivers
    - setup SRIOV ports
    - Init Kubernetes
    
    Change-Id: I6dd9dff7a4c7dfe2d7148812d2656966115c5ce9
    Signed-off-by: Mytnyk, Volodymyr <volodymyrx.mytnyk@intel.com>

---
+++ tools/kube-setup-one-node.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Copyright (c) 2018 Intel Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+env_http_proxy=$(sed -ne "s/^http_proxy=[\"\']\(.*\)[\"\']/\1/p" /etc/environment)
+if [[ -z ${http_proxy} ]] && [[ ! -z ${env_http_proxy} ]]; then
+    export http_proxy=${env_http_proxy}
+fi
+env_https_proxy=$(sed -ne "s/^https_proxy=[\"\']\(.*\)[\"\']/\1/p" /etc/environment)
+if [[ -z ${https_proxy} ]] && [[ ! -z ${env_https_proxy} ]]; then
+    export https_proxy=${env_https_proxy}
+fi
+env_ftp_proxy=$(sed -ne "s/^ftp_proxy=[\"\']\(.*\)[\"\']/\1/p" /etc/environment)
+if [[ -z ${ftp_proxy} ]] && [[ ! -z ${env_ftp_proxy} ]]; then
+    export ftp_proxy=${env_ftp_proxy}
+fi
+if [[ ! -z ${http_proxy} ]] || [[ ! -z ${https_proxy} ]]; then
+    export no_proxy="${no_proxy}"
+    extra_args="${extra_args} -e @/tmp/proxy.yml "
+    cat <<EOF > /tmp/proxy.yml
+---
+proxy_env:
+  http_proxy: ${http_proxy}
+  https_proxy: ${https_proxy}
+  ftp_proxy: ${ftp_proxy}
+  no_proxy: ${no_proxy}
+EOF
+fi
+ANSIBLE_SCRIPTS="${0%/*}/../ansible"
+
+cd ${ANSIBLE_SCRIPTS} && \
+ansible-playbook \
+         ${extra_args} -i kube-inventory.ini deploy_kube.yml
