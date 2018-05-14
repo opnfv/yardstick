10a416fd70fc91426bedc67d92f5d8623261d3a8
Abhijit Sinha <abhijit.sinha@intel.com>
2018-05-14 14:31:52 +0100

    Fix PROX throughput result calculation
    The success criteria needed a fix for throughput
    calculation. The results on success criteria were
    muliplied by 1000*1000 which is not correct and
    not needed as this was leading to false results
    in grafana.
    
    JIRA: YARDSTICK-1172
    
    Change-Id: I56b24a700f8a565db80897aeab796e7039cae5c9
    Signed-off-by: Abhijit Sinha <abhijit.sinha@intel.com>

--- yardstick/network_services/traffic_profile/prox_binsearch.py
+++ yardstick/network_services/traffic_profile/prox_binsearch.py
@@ -116,7 +116,7 @@ class ProxBinSearchProfile(ProxProfile):
                 self.current_lower = test_value
                 successful_pkt_loss = result.pkt_loss
                 samples = result.get_samples(pkt_size, successful_pkt_loss, port_samples)
-                samples["TxThroughput"] = samples["TxThroughput"] * 1000 * 1000
+                samples["TxThroughput"] = samples["TxThroughput"]
 
                 # store results with success tag in influxdb
                 success_samples = {'Success_' + key: value for key, value in samples.items()}
