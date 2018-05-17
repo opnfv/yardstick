136f285b35a32763e4b577092cb39d3712e13dc4
John O'Loughlin <john.oloughlin@intel.com>
2018-05-17 15:06:53 +0000

    Add "ports" parameters in Kubernetes context
    This new parameter, " ** ports", will allow the user to
    automatically to add port variables in a pod definition:
    context:
      type: Kubernetes
        servers:
            host:
                image: ...
                commands: ...
                ports:
                    - containerPort: <port value>    # Mandatory
                      hostIP: <host IP>              # Optional
                      hostPort: <host port>          # Optional
                      name: <port name>              # Optional
                      protocol: <protocol name>      # Optional
    
    The port type and the definition must be one of the supported ones
    in Kubernetes [1].
    Container port definition, V1 API, Kubernetes Python library v6.0.0 [2]
    
    [1] https://kubernetes.io/docs/concepts/services-networking/connect-applications-service/
    [2] https://github.com/kubernetes-client/python/blob/6.0.0/kubernetes/client/models/v1_container_port.py
    
    JIRA: YARDSTICK-1183
    
    Change-Id: I3673dbeec8d380cb2f8b1dacf6c25c7cdbc37e28
    Signed-off-by: John O Loughlin <john.oloughlin@intel.com>

--- yardstick/common/exceptions.py
+++ yardstick/common/exceptions.py
@@ -182,6 +182,9 @@ class KubernetesCRDObjectDefinitionError(YardstickException):
 class KubernetesWrongRestartPolicy(YardstickException):
     message = 'Restart policy "%(rpolicy)s" is not valid'
 
+class KubernetesContainerPortNotDefined(YardstickException):
+    message = 'Container port not defined in "%(port)s"'
+
 class ScenarioCreateNetworkError(YardstickException):
     message = 'Create Neutron Network Scenario failed'
 
