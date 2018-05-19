82346e59af7c74384c271d8ea7b256cb5f9f6301
Rodolfo Alonso Hernandez <rodolfo.alonso.hernandez@intel.com>
2018-05-19 19:31:37 +0100

    Add send socket commands function
    Pktgen provides a TCP socket connection to allow the user to control
    it from a remote console or program [1].
    
    This new method will provide Yardstick the ability to send string
    commands to a port in a remote host.
    
    [1] http://pktgen-dpdk.readthedocs.io/en/latest/socket.html
    
    JIRA: YARDSTICK-1186
    
    Change-Id: I9d64ccad662fa3599de65654c5dab02833fcc91d
    Signed-off-by: Rodolfo Alonso Hernandez <rodolfo.alonso.hernandez@intel.com>

--- yardstick/tests/functional/common/test_utils.py
+++ yardstick/tests/functional/common/test_utils.py
@@ -12,8 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import multiprocessing
 import unittest
+import socket
 import sys
+import time
 
 from yardstick.common import utils
 
@@ -32,3 +35,38 @@ class ImportModulesFromPackageTestCase(unittest.TestCase):
         library_obj = getattr(module_obj, library_name)
         class_obj = getattr(library_obj, class_name)
         self.assertEqual(class_name, class_obj().__class__.__name__)
+
+
+class SendSocketCommandTestCase(unittest.TestCase):
+
+    @staticmethod
+    def _run_socket_server(port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('localhost', port))
+        sock.listen(1)
+        conn = None
+        while not conn:
+            conn, _ = sock.accept()
+        sock.close()
+
+    @staticmethod
+    def _terminate_server(socket_server):
+        # Wait until the socket server closes the open port.
+        time.sleep(1)
+        if socket_server and socket_server.is_alive():
+            socket_server.terminate()
+
+    def test_send_command(self):
+        port = 47001
+
+        socket_server = multiprocessing.Process(
+            name='run_socket_server',
+            target=SendSocketCommandTestCase._run_socket_server,
+            args=(port, )).start()
+
+        self.addCleanup(self._terminate_server, socket_server)
+
+        # Wait until the socket is open.
+        time.sleep(0.5)
+        self.assertEqual(
+            0, utils.send_socket_command('localhost', port, 'test_command'))
