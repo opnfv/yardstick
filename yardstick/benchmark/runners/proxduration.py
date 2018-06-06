e4a6637508f8e44077f4d9e3cd4358c12bb3b426
DanielMartinBuckley <daniel.m.buckley@intel.com>
2018-06-06 15:06:01 +0100

    Addition of Configurable Sampling and Configurable Confirmation Retry
    YIRA: YARDSTICK-1221
    
    In order to increase accuracy of result the following are done :-
      - Improve Measurement Accuracy (YARDSTICK-1212)
      - Improve Sampling Interval (YARDSTICK-1219)
      - Allow 4 PROX ports to be read simultaneously (YARDSTICK-1220)
    
    This change does the following :-
      - Stores LINE Rate statistics of Sample
      - Requires a confirmation retry before deciding to increase or decrease
        This is configurable.
      - Allows the user to disable Sampling or specify a sample interval
    
    Change-Id: I27242ac1849c9a2712866385b5fbc05977c71516
    Signed-off-by: Daniel Martin Buckley <daniel.m.buckley@intel.com>

---
+++ yardstick/benchmark/runners/proxduration.py
@@ -0,0 +1,186 @@
+# Copyright 2014: Mirantis Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# yardstick comment: this is a modified copy of
+# rally/rally/benchmark/runners/constant.py
+
+"""A runner that runs a specific time before it returns
+"""
+
+from __future__ import absolute_import
+import datetime
+import os
+import multiprocessing
+import logging
+import traceback
+import time
+
+from yardstick.benchmark.runners import base
+
+LOG = logging.getLogger(__name__)
+
+
+QUEUE_PUT_TIMEOUT = 10
+
+def dumpclean(obj):
+    if type(obj) == dict:
+        for k, v in obj.items():
+            if hasattr(v, '__iter__'):
+                LOG.info (">>>#> k %s", k)
+                dumpclean(v)
+            # else:
+            #    LOG.info (">>>#> %s : %s", k, v)
+    elif type(obj) == list:
+        for v in obj:
+            if hasattr(v, '__iter__'):
+                dumpclean(v)
+            else:
+                LOG.info(">>>#> v %s", v)
+    else:
+        LOG.info(">>>#> obj %s", obj)
+
+def _worker_process(queue, cls, method_name, scenario_cfg,
+                    context_cfg, aborted, output_queue):
+
+    sequence = 1
+
+    runner_cfg = scenario_cfg['runner']
+
+    requested_interval = interval = runner_cfg.get("interval", 20)
+    duration = runner_cfg.get("duration", 60)
+    sampled = runner_cfg.get("sampled", False)
+
+    LOG.info("Worker START, duration is %ds", duration)
+    LOG.debug("class is %s", cls)
+
+    runner_cfg['runner_id'] = os.getpid()
+
+    benchmark = cls(scenario_cfg, context_cfg)
+    benchmark.setup()
+    method = getattr(benchmark, method_name)
+
+    sla_action = None
+    if "sla" in scenario_cfg:
+        sla_action = scenario_cfg["sla"].get("action", "assert")
+
+
+    start = time.time()
+    timeout = start + duration
+    while True:
+
+        LOG.debug("runner=%(runner)s seq=%(sequence)s START",
+                  {"runner": runner_cfg["runner_id"], "sequence": sequence})
+
+        data = {}
+        errors = ""
+
+        benchmark.pre_run_wait_time(interval)
+
+        if sampled:
+            try:
+                pre_adjustment = time.time()
+                result = method(data)
+                post_adjustment = time.time()
+                if requested_interval > post_adjustment - pre_adjustment:
+                    interval = requested_interval - (post_adjustment - pre_adjustment)
+                else:
+                    interval = 0
+
+            except AssertionError as assertion:
+                # SLA validation failed in scenario, determine what to do now
+                if sla_action == "assert":
+                    raise
+                elif sla_action == "monitor":
+                    LOG.warning("SLA validation failed: %s", assertion.args)
+                    errors = assertion.args
+            # catch all exceptions because with multiprocessing we can have un-picklable exception
+            # problems  https://bugs.python.org/issue9400
+            except Exception:  # pylint: disable=broad-except
+                errors = traceback.format_exc()
+                LOG.exception("")
+            else:
+                if result:
+                    # add timeout for put so we don't block test
+                    # if we do timeout we don't care about dropping individual KPIs
+                    output_queue.put(result, True, QUEUE_PUT_TIMEOUT)
+
+            # benchmark.post_run_wait_time(interval)
+
+            benchmark_output = {
+                'timestamp': time.time(),
+                'sequence': sequence,
+                'data': data,
+                'errors': errors
+            }
+
+            queue.put(benchmark_output, True, QUEUE_PUT_TIMEOUT)
+
+            LOG.debug(">>>#>runner=%(runner)s seq=%(sequence)s END",
+                      {"runner": runner_cfg["runner_id"], "sequence": sequence})
+
+            for k, v in data.iteritems():
+               LOG.info ("Sample Time%s -Item  %s -Content %s", time.time(), k, v)
+
+        sequence += 1
+
+        if (errors and sla_action is None):
+            LOG.info ("Exit on Errors or SLA action ")
+            break
+        elif time.time() > timeout:
+            LOG.info("Exit on Timeout ....")
+            break
+        elif aborted.is_set():
+            LOG.info("Aborted ..")
+            break
+
+    try:
+        benchmark.teardown()
+    except Exception:
+        # catch any exception in teardown and convert to simple exception
+        # never pass exceptions back to multiprocessing, because some exceptions can
+        # be unpicklable
+        # https://bugs.python.org/issue9400
+        LOG.exception("")
+        raise SystemExit(1)
+
+    LOG.debug("queue.qsize() = %s", queue.qsize())
+    LOG.debug("output_queue.qsize() = %s", output_queue.qsize())
+
+
+class ProxDurationRunner(base.Runner):
+    """Run a scenario for a certain amount of time
+
+If the scenario ends before the time has elapsed, it will be started again.
+
+  Parameters
+    duration - amount of time the scenario will be run for
+        type:    int
+        unit:    seconds
+        default: 1 sec
+    interval - time to wait between each scenario invocation
+        type:    int
+        unit:    seconds
+        default: 1 sec
+    """
+    __execution_type__ = 'ProxDuration'
+
+    def _run_benchmark(self, cls, method, scenario_cfg, context_cfg):
+        name = "{}-{}-{}".format(self.__execution_type__, scenario_cfg.get("type"), os.getpid())
+        self.process = multiprocessing.Process(
+            name=name,
+            target=_worker_process,
+            args=(self.result_queue, cls, method, scenario_cfg,
+                  context_cfg, self.aborted, self.output_queue))
+        self.process.start()
