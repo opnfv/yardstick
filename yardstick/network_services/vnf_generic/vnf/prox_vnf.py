6e29feabb63bc31ab163d8687cb4b8203d0c2aa9
Abhijit Sinha <abhijit.sinha@intel.com>
2018-06-07 19:59:08 +0100

    NSB Topology fix for Prox 4 port test case
    The uplink and downlink interfaces for the 4 port
    test cases were inconsistent and fixed in this commit.
    
    This affects all the 4 port PROX test cases.
    
    Tested on: Heat L2fwd-4 and L3fwd-4 test case.
    
    JIRA: YARDSTICK-1185
    
    Change-Id: Ia2ce7ed0dc395b812f84ede94e259b42dc833579
    Signed-off-by:Abhijit Sinha <abhijit.sinha@intel.com>
    (cherry picked from commit 8ebd2d3fde473ffcd139090bd688882d4e5e0f44)

--- yardstick/network_services/vnf_generic/vnf/prox_vnf.py
+++ yardstick/network_services/vnf_generic/vnf/prox_vnf.py
@@ -86,12 +86,12 @@ class ProxApproxVnf(SampleVNF):
                                "1, 2 or 4 ports only supported at this time")
 
         all_port_stats = self.vnf_execute('multi_port_stats', range(port_count))
+        curr_time = time.time()
         rx_total = tx_total = 0
         try:
             for single_port_stats in all_port_stats:
                 rx_total = rx_total + single_port_stats[1]
                 tx_total = tx_total + single_port_stats[2]
-                tsc = single_port_stats[5]
         except (TypeError, IndexError):
             LOG.error("Invalid data ...")
             return {}
