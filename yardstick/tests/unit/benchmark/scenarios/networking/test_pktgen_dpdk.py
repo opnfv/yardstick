fb37dbf54fd67eddd91ac8e7ca8802a4600c6c92
Miikka Koistinen <miikka.koistinen@nokia.com>
2018-06-13 13:27:47 +0300

    Cleanup pktgen_dpdk unit tests
    * Improve test execution
    * Use common functionality in setUp()
    * Disable logging
    
    JIRA: YARDSTICK-1239
    
    Change-Id: I88d047fe25c4c13bea446c9fc9bda92306420296
    Signed-off-by: Miikka Koistinen <miikka.koistinen@nokia.com>

--- yardstick/tests/unit/benchmark/scenarios/networking/test_pktgen_dpdk.py
+++ yardstick/tests/unit/benchmark/scenarios/networking/test_pktgen_dpdk.py
@@ -9,16 +9,23 @@
 
 import mock
 import unittest
+import time
+import logging
 
 import yardstick.common.utils as utils
-from yardstick.benchmark.scenarios.networking import pktgen_dpdk
+from yardstick import ssh
+from yardstick.benchmark.scenarios.networking.pktgen_dpdk import \
+    PktgenDPDKLatency
 from yardstick import exceptions as y_exc
+
+
+logging.disable(logging.CRITICAL)
 
 
 class PktgenDPDKLatencyTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.ctx = {
+        self.context_cfg = {
             'host': {
                 'ip': '172.16.0.137',
                 'user': 'root',
@@ -31,100 +38,67 @@ class PktgenDPDKLatencyTestCase(unittest.TestCase):
                 'ipaddr': '172.16.0.138'
             }
         }
+        self.scenario_cfg = {
+            'options': {'packetsize': 60}
+        }
 
-        self._mock_ssh = mock.patch(
-            'yardstick.benchmark.scenarios.networking.pktgen_dpdk.ssh')
-        self.mock_ssh = self._mock_ssh.start()
-        self._mock_time = mock.patch(
-            'yardstick.benchmark.scenarios.networking.pktgen_dpdk.time')
-        self.mock_time = self._mock_time.start()
+        self._mock_SSH = mock.patch.object(ssh, 'SSH')
+        self.mock_SSH = self._mock_SSH.start()
+
+        self._mock_time_sleep = mock.patch.object(time, 'sleep')
+        self.mock_time_sleep = self._mock_time_sleep.start()
+
+        self.mock_SSH.from_node().execute.return_value = (0, '', '')
 
         self.addCleanup(self._stop_mock)
 
+        self.scenario = PktgenDPDKLatency(self.scenario_cfg, self.context_cfg)
+        self.scenario.server = self.mock_SSH.from_node()
+        self.scenario.client = self.mock_SSH.from_node()
+
     def _stop_mock(self):
-        self._mock_ssh.stop()
-        self._mock_time.stop()
+        self._mock_SSH.stop()
+        self._mock_time_sleep.stop()
 
     def test_pktgen_dpdk_successful_setup(self):
+        scenario = PktgenDPDKLatency(self.scenario_cfg, self.context_cfg)
+        scenario.setup()
 
-        args = {
-            'options': {'packetsize': 60},
-        }
-        p = pktgen_dpdk.PktgenDPDKLatency(args, self.ctx)
-        p.setup()
-
-        self.mock_ssh.SSH.from_node().execute.return_value = (0, '', '')
-        self.assertIsNotNone(p.server)
-        self.assertIsNotNone(p.client)
-        self.assertTrue(p.setup_done)
+        self.assertIsNotNone(scenario.server)
+        self.assertIsNotNone(scenario.client)
+        self.assertTrue(scenario.setup_done)
 
     def test_pktgen_dpdk_successful_get_port_ip(self):
+        utils.get_port_ip(self.scenario.server, "eth1")
 
-        args = {
-            'options': {'packetsize': 60},
-        }
-        p = pktgen_dpdk.PktgenDPDKLatency(args, self.ctx)
-        p.server = self.mock_ssh.SSH.from_node()
-
-        self.mock_ssh.SSH.from_node().execute.return_value = (0, '', '')
-
-        utils.get_port_ip(p.server, "eth1")
-
-        self.mock_ssh.SSH.from_node().execute.assert_called_with(
-            "ifconfig eth1 |grep 'inet addr' |awk '{print $2}' |cut -d ':' -f2 ")
+        self.mock_SSH.from_node().execute.assert_called_with(
+            "ifconfig eth1 |grep 'inet addr' "
+            "|awk '{print $2}' |cut -d ':' -f2 ")
 
     def test_pktgen_dpdk_unsuccessful_get_port_ip(self):
+        self.mock_SSH.from_node().execute.return_value = (1, '', 'FOOBAR')
 
-        args = {
-            'options': {'packetsize': 60},
-        }
-
-        p = pktgen_dpdk.PktgenDPDKLatency(args, self.ctx)
-        p.server = self.mock_ssh.SSH.from_node()
-
-        self.mock_ssh.SSH.from_node().execute.return_value = (1, '', 'FOOBAR')
-        self.assertRaises(RuntimeError, utils.get_port_ip, p.server, "eth1")
+        with self.assertRaises(RuntimeError):
+            utils.get_port_ip(self.scenario.server, "eth1")
 
     def test_pktgen_dpdk_successful_get_port_mac(self):
+        utils.get_port_mac(self.scenario.server, "eth1")
 
-        args = {
-            'options': {'packetsize': 60},
-        }
-        p = pktgen_dpdk.PktgenDPDKLatency(args, self.ctx)
-        p.server = self.mock_ssh.SSH.from_node()
-
-        self.mock_ssh.SSH.from_node().execute.return_value = (0, '', '')
-
-        utils.get_port_mac(p.server, "eth1")
-
-        self.mock_ssh.SSH.from_node().execute.assert_called_with(
+        self.mock_SSH.from_node().execute.assert_called_with(
             "ifconfig |grep HWaddr |grep eth1 |awk '{print $5}' ")
 
     def test_pktgen_dpdk_unsuccessful_get_port_mac(self):
+        self.mock_SSH.from_node().execute.return_value = (1, '', 'FOOBAR')
 
-        args = {
-            'options': {'packetsize': 60},
-        }
-
-        p = pktgen_dpdk.PktgenDPDKLatency(args, self.ctx)
-        p.server = self.mock_ssh.SSH.from_node()
-
-        self.mock_ssh.SSH.from_node().execute.return_value = (1, '', 'FOOBAR')
-        self.assertRaises(RuntimeError, utils.get_port_mac, p.server, "eth1")
+        with self.assertRaises(RuntimeError):
+            utils.get_port_mac(self.scenario.server, "eth1")
 
     def test_pktgen_dpdk_successful_no_sla(self):
-
-        args = {
-            'options': {'packetsize': 60},
-        }
+        sample_output = '100\n110\n112\n130\n149\n150\n90\n150\n200\n162\n'
+        self.mock_SSH.from_node().execute.return_value = (0, sample_output, '')
 
         result = {}
-        p = pktgen_dpdk.PktgenDPDKLatency(args, self.ctx)
-
-        sample_output = '100\n110\n112\n130\n149\n150\n90\n150\n200\n162\n'
-        self.mock_ssh.SSH.from_node().execute.return_value = (0, sample_output, '')
-
-        p.run(result)
+        self.scenario.run(result)
         # with python 3 we get float, might be due python division changes
         # AssertionError: {'avg_latency': 132.33333333333334} != {
         # 'avg_latency': 132}
@@ -132,64 +106,40 @@ class PktgenDPDKLatencyTestCase(unittest.TestCase):
         self.assertLessEqual(delta, 1)
 
     def test_pktgen_dpdk_successful_sla(self):
-
-        args = {
-            'options': {'packetsize': 60},
-            'sla': {'max_latency': 100}
-        }
-        result = {}
-
-        p = pktgen_dpdk.PktgenDPDKLatency(args, self.ctx)
+        self.scenario_cfg['sla'] = {'max_latency': 100}
+        scenario = PktgenDPDKLatency(self.scenario_cfg, self.context_cfg)
 
         sample_output = '100\n100\n100\n100\n100\n100\n100\n100\n100\n100\n'
-        self.mock_ssh.SSH.from_node().execute.return_value = (0, sample_output, '')
+        self.mock_SSH.from_node().execute.return_value = (0, sample_output, '')
 
-        p.run(result)
+        result = {}
+        scenario.run(result)
 
         self.assertEqual(result, {"avg_latency": 100})
 
     def test_pktgen_dpdk_unsuccessful_sla(self):
-
-        args = {
-            'options': {'packetsize': 60},
-            'sla': {'max_latency': 100}
-        }
-        result = {}
-
-        p = pktgen_dpdk.PktgenDPDKLatency(args, self.ctx)
-
-        p.server = self.mock_ssh.SSH.from_node()
-        p.client = self.mock_ssh.SSH.from_node()
+        self.scenario_cfg['sla'] = {'max_latency': 100}
+        scenario = PktgenDPDKLatency(self.scenario_cfg, self.context_cfg)
 
         sample_output = '100\n110\n112\n130\n149\n150\n90\n150\n200\n162\n'
-        self.mock_ssh.SSH.from_node().execute.return_value = (0, sample_output, '')
-        self.assertRaises(AssertionError, p.run, result)
+        self.mock_SSH.from_node().execute.return_value = (0, sample_output, '')
+
+        with self.assertRaises(AssertionError):
+            scenario.run({})
 
     def test_pktgen_dpdk_run_unsuccessful_get_port_mac(self):
+        self.mock_SSH.from_node().execute.return_value = (1, '', 'FOOBAR')
 
-        args = {
-            'options': {'packetsize': 60},
-            'sla': {'max_latency': 100}
-        }
-        result = {}
-
-        p = pktgen_dpdk.PktgenDPDKLatency(args, self.ctx)
-
-        self.mock_ssh.SSH.from_node().execute.return_value = (1, '', 'FOOBAR')
-        self.assertRaises(RuntimeError, p.run, result)
+        with self.assertRaises(RuntimeError):
+            self.scenario.run({})
 
     def test_pktgen_dpdk_run_unsuccessful_script_error(self):
-        args = {
-            'options': {'packetsize': 60}
-        }
-
-        p = pktgen_dpdk.PktgenDPDKLatency(args, self.ctx)
-
-        self.mock_ssh.SSH.from_node().execute.side_effect = ((0, '', ''),
-                                                             (0, '', ''),
-                                                             (0, '', ''),
-                                                             (0, '', ''),
-                                                             (0, '', ''),
-                                                             y_exc.SSHError)
-        self.assertRaises(y_exc.SSHError, p.run, {})
-        self.assertEqual(self.mock_ssh.SSH.from_node().execute.call_count, 6)
+        self.mock_SSH.from_node().execute.side_effect = ((0, '', ''),
+                                                         (0, '', ''),
+                                                         (0, '', ''),
+                                                         (0, '', ''),
+                                                         (0, '', ''),
+                                                         y_exc.SSHError)
+        with self.assertRaises(y_exc.SSHError):
+            self.scenario.run({})
+        self.assertEqual(self.mock_SSH.from_node().execute.call_count, 6)
