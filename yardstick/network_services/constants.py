d131c37bca3705b942e6014b1a32de0f474c40d5
DanielMartinBuckley <daniel.m.buckley@intel.com>
2018-07-05 17:55:29 +0100

    Update Multi Port Stats reading for PROX
    JIRA: YARDSTICK-1458
    
    Removed excessive jitter by removing extra sleeps in start/stop/reset
      ssh
    Removed extra retries when bad data returned
    Added a slep at end of test to give runner a chance to empty the queue
      on shutdown
    Added a timeout on port retry attempts
    
    Change-Id: Ica60b11c7af242817a83c154157827581f60d16a
    Signed-off-by: Daniel Martin Buckley <daniel.m.buckley@intel.com>

--- yardstick/network_services/constants.py
+++ yardstick/network_services/constants.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016-2017 Intel Corporation
+# Copyright (c) 2016-2018 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,3 +17,4 @@ DEFAULT_VNF_TIMEOUT = 3600
 PROCESS_JOIN_TIMEOUT = 3
 ONE_GIGABIT_IN_BITS = 1000000000
 NIC_GBPS_DEFAULT = 10
+RETRY_TIMEOUT = 5
