625125f54e35fe230fc7614f1d5f3e007f620125
Mytnyk, Volodymyr <volodymyrx.mytnyk@intel.com>
2018-07-17 09:42:47 +0100

    kubernetes_utils: avoid 404 error code in delete_pod()
    JIRA: YARDSTICK-1320
    
    Change-Id: I5b762d4732440008630093e1d0952f7e41a8a191
    Signed-off-by: Mytnyk, Volodymyr <volodymyrx.mytnyk@intel.com>

--- yardstick/tests/unit/common/test_kubernetes_utils.py
+++ yardstick/tests/unit/common/test_kubernetes_utils.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 
 import mock
+import httplib
+
 from kubernetes import client
 from kubernetes.client import rest
 from kubernetes import config
@@ -222,3 +224,17 @@ class DeleteNetworkTestCase(base.BaseUnitTestCase):
             kubernetes_utils.delete_network(
                 constants.SCOPE_CLUSTER, mock.ANY, mock.ANY, mock.ANY,
                 mock.ANY)
+
+
+class DeletePodTestCase(base.BaseUnitTestCase):
+    @mock.patch.object(kubernetes_utils, 'get_core_api')
+    def test_execute_exception(self, mock_get_api):
+        mock_api = mock.Mock()
+        mock_api.delete_namespaced_pod.side_effect = \
+            rest.ApiException(status=httplib.NOT_FOUND)
+
+        mock_get_api.return_value = mock_api
+        kubernetes_utils.delete_pod(
+            mock.ANY,
+            skip_codes=[httplib.NOT_FOUND]
+        )
