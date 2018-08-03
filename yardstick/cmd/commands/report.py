8c8fa4ec745ba4820ed4c7f5d52e305dd830dfef
Emma Foley <emma.l.foley@intel.com>
2018-08-03 14:02:46 +0100

    Add report generate-nsb command
    TODO: Add UTs
    
    JIRA: YARDSTICK-1367
    Change-Id: I8a2f96224a745334b67dd71875bebb3b69b9adc3
    Signed-off-by: Emma Foley <emma.l.foley@intel.com>

--- yardstick/cmd/commands/report.py
+++ yardstick/cmd/commands/report.py
@@ -7,14 +7,9 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
-
 """ Handler for yardstick command 'report' """
 
-from __future__ import print_function
-
-from __future__ import absolute_import
-
-from yardstick.benchmark.core.report import Report
+from yardstick.benchmark.core import report
 from yardstick.cmd.commands import change_osloobj_to_paras
 from yardstick.common.utils import cliargs
 
@@ -30,4 +25,11 @@ class ReportCommands(object):   # pragma: no cover
     def do_generate(self, args):
         """Start a benchmark scenario."""
         param = change_osloobj_to_paras(args)
-        Report().generate(param)
+        report.Report().generate(param)
+
+    @cliargs("task_id", type=str, help=" task id", nargs=1)
+    @cliargs("yaml_name", type=str, help=" Yaml file Name", nargs=1)
+    def do_generate_nsb(self, args):
+        """Generate a report using hte NSB template."""
+        param = change_osloobj_to_paras(args)
+        report.Report().generate_nsb(param)
