2fd4fa90e920d6ced9190e83736c1501a603a1b2
Emma Foley <emma.l.foley@intel.com>
2018-08-09 13:09:29 +0100

    Adds ssh.DEFAULT_PORT
    yardstick/benchmark/scenarios/networking/pktgen_dpdk_throughput.py:PktgenDPDK.setup
    expected yardstick/ssh.py:DEFAULT_PORT to exist, but this constant was not defined.
    
    The error was not caught by testing because yardstick.ssh was being mocked, which
    caused this issue to pass unnoticed.
    
    The unittests have been fixed and the constant added.
    
    JIRA: YARDSTICK-1373
    Signed-off-by: Emma Foley <emma.l.foley@intel.com>
    Change-Id: I4654e21b55cc18cbdf6ea094418fdcd76cb42e09

--- yardstick/tests/unit/benchmark/scenarios/networking/test_pktgen_dpdk_throughput.py
+++ yardstick/tests/unit/benchmark/scenarios/networking/test_pktgen_dpdk_throughput.py
@@ -7,23 +7,17 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-# Unittest for yardstick.benchmark.scenarios.networking.pktgen.PktgenDPDK
-
-from __future__ import absolute_import
+import mock
+import time
 import unittest
 
 from oslo_serialization import jsonutils
-import mock
 
 from yardstick.benchmark.scenarios.networking import pktgen_dpdk_throughput
 from yardstick.common import exceptions as y_exc
+from yardstick import ssh
 
 
-# pylint: disable=unused-argument
-# disable this for now because I keep forgetting mock patch arg ordering
-
-
-@mock.patch('yardstick.benchmark.scenarios.networking.pktgen_dpdk_throughput.ssh')
 class PktgenDPDKTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -40,28 +34,33 @@ class PktgenDPDKTestCase(unittest.TestCase):
             }
         }
 
-        self._mock_time = mock.patch(
-            'yardstick.benchmark.scenarios.networking.pktgen_dpdk_throughput.time')
+        self._mock_time = mock.patch.object(time, 'sleep')
         self.mock_time = self._mock_time.start()
+        self._mock_ssh = mock.patch.object(ssh, 'SSH')
+        self.mock_ssh = self._mock_ssh.start()
 
         self.addCleanup(self._cleanup)
 
     def _cleanup(self):
         self._mock_time.stop()
+        self._mock_ssh.stop()
 
-    def test_pktgen_dpdk_throughput_successful_setup(self, mock_ssh):
+    @mock.patch.object(ssh, 'SSH')
+    def test_setup(self, mock_ssh):
         args = {
             'options': {'packetsize': 60},
+            'runner': {'duration': "runner_config"},
         }
         p = pktgen_dpdk_throughput.PktgenDPDK(args, self.ctx)
+        mock_ssh.execute.return_value = (0, '', '')
+
         p.setup()
 
-        mock_ssh.SSH().execute.return_value = (0, '', '')
         self.assertIsNotNone(p.server)
         self.assertIsNotNone(p.client)
         self.assertTrue(p.setup_done)
 
-    def test_pktgen_dpdk_throughput_successful_no_sla(self, mock_ssh):
+    def test_run_successful_no_sla(self):
         args = {
             'options': {'packetsize': 60, 'number_of_ports': 10},
         }
@@ -69,17 +68,16 @@ class PktgenDPDKTestCase(unittest.TestCase):
         result = {}
 
         p = pktgen_dpdk_throughput.PktgenDPDK(args, self.ctx)
+        p.setup_done = True
+        p.server = self.mock_ssh
+        p.client = self.mock_ssh
 
-        p.server = mock_ssh.SSH()
-        p.client = mock_ssh.SSH()
-
-        mock_dpdk_result = mock.Mock()
-        mock_dpdk_result.return_value = 149300
+        mock_dpdk_result = mock.Mock(return_value=149300)
         p._dpdk_get_result = mock_dpdk_result
 
         sample_output = '{"packets_per_second": 9753, "errors": 0, \
             "packets_sent": 149776, "flows": 110}'
-        mock_ssh.SSH().execute.return_value = (0, sample_output, '')
+        self.mock_ssh.execute.return_value = (0, sample_output, '')
 
         p.run(result)
         expected_result = jsonutils.loads(sample_output)
@@ -87,7 +85,38 @@ class PktgenDPDKTestCase(unittest.TestCase):
         expected_result["packetsize"] = 60
         self.assertEqual(result, expected_result)
 
-    def test_pktgen_dpdk_throughput_successful_sla(self, mock_ssh):
+    def test_run_setup_not_done(self):
+        scenario_cfg = {
+            'options': {'packet_size': 60}
+        }
+        p = pktgen_dpdk_throughput.PktgenDPDK(scenario_cfg, self.ctx)
+        p.dpdk_setup_done = True
+        p.setup = mock.Mock()
+        p.server = self.mock_ssh
+        p.client = self.mock_ssh
+
+        mock_dpdk_result = mock.Mock(return_value=149300)
+        p._dpdk_get_result = mock_dpdk_result
+
+        sample_output = '{"packets_per_second": 9753, "errors": 0, \
+            "packets_sent": 149776, "flows": 110}'
+        self.mock_ssh.execute.return_value = (0, sample_output, '')
+        p.run({})
+
+        p.setup.assert_called()        
+
+    def test_dpdk_setup_error_on_server_target_ipaddr_unset(self):
+        p = pktgen_dpdk_throughput.PktgenDPDK({}, self.ctx)
+        p._is_dpdk_setup = mock.Mock(return_value=True)
+        p.server = self.mock_ssh
+        p.client = self.mock_ssh
+        p.server.execute.return_value = (1, '', '')
+
+        self.assertIsNone(p.target_ipaddr[1])
+        self.assertRaises(RuntimeError, p.dpdk_setup)
+        self.assertIsNone(p.target_ipaddr[1])
+
+    def test_run_successful_sla(self):
         args = {
             'options': {'packetsize': 60, 'number_of_ports': 10},
             'sla': {'max_ppm': 10000}
@@ -95,17 +124,17 @@ class PktgenDPDKTestCase(unittest.TestCase):
         result = {}
 
         p = pktgen_dpdk_throughput.PktgenDPDK(args, self.ctx)
+        p.setup_done = True
 
-        p.server = mock_ssh.SSH()
-        p.client = mock_ssh.SSH()
+        p.server = self.mock_ssh
+        p.client = self.mock_ssh
 
-        mock_dpdk_result = mock.Mock()
-        mock_dpdk_result.return_value = 149300
+        mock_dpdk_result = mock.Mock(return_value=149300)
         p._dpdk_get_result = mock_dpdk_result
 
         sample_output = '{"packets_per_second": 9753, "errors": 0, \
             "packets_sent": 149776, "flows": 110}'
-        mock_ssh.SSH().execute.return_value = (0, sample_output, '')
+        self.mock_ssh.execute.return_value = (0, sample_output, '')
 
         p.run(result)
         expected_result = jsonutils.loads(sample_output)
@@ -113,7 +142,7 @@ class PktgenDPDKTestCase(unittest.TestCase):
         expected_result["packetsize"] = 60
         self.assertEqual(result, expected_result)
 
-    def test_pktgen_dpdk_throughput_unsuccessful_sla(self, mock_ssh):
+    def test_run_sla_validation_failed(self):
         args = {
             'options': {'packetsize': 60, 'number_of_ports': 10},
             'sla': {'max_ppm': 1000}
@@ -121,21 +150,21 @@ class PktgenDPDKTestCase(unittest.TestCase):
         result = {}
 
         p = pktgen_dpdk_throughput.PktgenDPDK(args, self.ctx)
+        p.setup_done = True
 
-        p.server = mock_ssh.SSH()
-        p.client = mock_ssh.SSH()
+        p.server = self.mock_ssh
+        p.client = self.mock_ssh
 
-        mock_dpdk_result = mock.Mock()
-        mock_dpdk_result.return_value = 149300
+        mock_dpdk_result = mock.Mock(return_value=149300)
         p._dpdk_get_result = mock_dpdk_result
 
         sample_output = '{"packets_per_second": 9753, "errors": 0, \
             "packets_sent": 149776, "flows": 110}'
-        mock_ssh.SSH().execute.return_value = (0, sample_output, '')
+        self.mock_ssh.execute.return_value = (0, sample_output, '')
+
         self.assertRaises(y_exc.SLAValidationError, p.run, result)
 
-    def test_pktgen_dpdk_throughput_unsuccessful_script_error(
-            self, mock_ssh):
+    def test_run_script_error(self):
         args = {
             'options': {'packetsize': 60, 'number_of_ports': 10},
             'sla': {'max_ppm': 1000}
@@ -143,52 +172,53 @@ class PktgenDPDKTestCase(unittest.TestCase):
         result = {}
 
         p = pktgen_dpdk_throughput.PktgenDPDK(args, self.ctx)
+        p.setup_done = True
 
-        p.server = mock_ssh.SSH()
-        p.client = mock_ssh.SSH()
+        p.server = self.mock_ssh
+        p.client = self.mock_ssh
 
-        mock_ssh.SSH().execute.return_value = (1, '', 'FOOBAR')
+        self.mock_ssh.execute.return_value = (1, '', 'FOOBAR')
+
         self.assertRaises(RuntimeError, p.run, result)
 
-    def test_pktgen_dpdk_throughput_is_dpdk_setup(self, mock_ssh):
+    def test_is_dpdk_setup(self):
         args = {
             'options': {'packetsize': 60},
         }
         p = pktgen_dpdk_throughput.PktgenDPDK(args, self.ctx)
-        p.server = mock_ssh.SSH()
 
-        mock_ssh.SSH().execute.return_value = (0, '', '')
+        p.server = self.mock_ssh
+        p.server.execute.return_value = (0, '', '')
 
         p._is_dpdk_setup("server")
 
-        mock_ssh.SSH().execute.assert_called_with(
+        p.server.execute.assert_called_with(
             "ip a | grep eth1 2>/dev/null")
 
-    def test_pktgen_dpdk_throughput_dpdk_setup(self, mock_ssh):
+    def test_dpdk_setup(self):
         args = {
             'options': {'packetsize': 60},
         }
         p = pktgen_dpdk_throughput.PktgenDPDK(args, self.ctx)
-        p.server = mock_ssh.SSH()
-        p.client = mock_ssh.SSH()
+        p.server = self.mock_ssh
+        p.client = self.mock_ssh
 
-        mock_ssh.SSH().execute.return_value = (0, '', '')
+        self.mock_ssh.execute.return_value = (0, '', '')
 
+        self.assertFalse(p.dpdk_setup_done)
         p.dpdk_setup()
-
         self.assertTrue(p.dpdk_setup_done)
 
-    def test_pktgen_dpdk_throughput_dpdk_get_result(self, mock_ssh):
+    def test__dpdk_get_result(self):
         args = {
             'options': {'packetsize': 60},
         }
         p = pktgen_dpdk_throughput.PktgenDPDK(args, self.ctx)
-        p.server = mock_ssh.SSH()
-        p.client = mock_ssh.SSH()
-
-        mock_ssh.SSH().execute.return_value = (0, '10000', '')
+        p.server = self.mock_ssh
+        p.client = self.mock_ssh
+        p.server.execute.return_value = (0, '10000', '')
 
         p._dpdk_get_result()
 
-        mock_ssh.SSH().execute.assert_called_with(
+        p.server.execute.assert_called_with(
             "sudo /dpdk/destdir/bin/dpdk-procinfo -- --stats-reset > /dev/null 2>&1")
