7a601d4e80f7c06d89a36ff12e8716b117a03e89
Chornyi, TarasX <tarasx.chornyi@intel.com>
2018-09-07 12:11:04 +0300

    [WIP] Added Dummy VNF support
    Using dummy vnf it is now possible to craete a "black box" testcase for any VNF.
    
    Example rfc2544 test with TRex as trafficgen
    
    samples/vnf_samples/nsut/dummy/tc_baremetal_rfc2544_ipv4_64B_trex.yaml
    
    NOTE: yardstick will not interuct with VNF.
    No KPIs from vnf will collected
    
    TODO:
    Add UT
    Add example tescase with IXIA
    
    Change-Id: I408b3dcb90f29c7b41fcf8f883b5abfc4e9c3dbc
    Signed-off-by: Chornyi, TarasX <tarasx.chornyi@intel.com>

---
+++ yardstick/network_services/vnf_generic/vnf/dummy.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+import yardstick.network_services.vnf_generic.vnf.base as base
+
+LOG = logging.getLogger(__name__)
+
+
+class DummyVnf(base.GenericVNF):
+    def __init__(self, name, vnfd, task_id):
+        super(DummyVnf, self).__init__(name, vnfd, task_id)
+
+    def instantiate(self, scenario_cfg, context_cfg):
+        pass
+
+    def wait_for_instantiate(self):
+        pass
+
+    def terminate(self):
+        pass
+
+    def scale(self, flavor=""):
+        pass
+
+    def collect_kpi(self):
+        pass
+
+    def start_collect(self):
+        pass
+
+    def stop_collect(self):
+        pass
