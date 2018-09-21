81500495dc6785158a7456c70bb3207219581451
Mytnyk, Volodymyr <volodymyrx.mytnyk@intel.com>
2018-09-21 16:31:23 +0100

    Refactor SampleVNF wait method
    JIRA: YARDSTICK-1439
    
    Change-Id: I26441fe1f6e34a867c1f19d60d2247e888af6c54
    Signed-off-by: Mytnyk, Volodymyr <volodymyrx.mytnyk@intel.com>

--- yardstick/network_services/vnf_generic/vnf/vfw_vnf.py
+++ yardstick/network_services/vnf_generic/vnf/vfw_vnf.py
@@ -57,3 +57,7 @@ class FWApproxVnf(SampleVNF):
             setup_env_helper_type = FWApproxSetupEnvHelper
 
         super(FWApproxVnf, self).__init__(name, vnfd, setup_env_helper_type, resource_helper_type)
+
+    def wait_for_instantiate(self):
+        """Wait for VNF to initialize"""
+        self.wait_for_initialize()
