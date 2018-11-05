b0b1c3948cb1d5141aecb83d964a175c9981bb0e
Xavier Simonart <xavier.simonart@intel.com>
2018-11-05 15:34:07 +0100

    Add/Improve 2 ports baremetal NSB NFVi (PROX) test cases
    JIRA: YARDSTICK-1511
    
    Buffering, lb and lw_aftr test cases for 2 ports are added as well as
    the corresponding PROX config files.
    For lw_aftr, latency support has been disabled as this would require
    increasing minimum packet size.
    
    For all those 2 ports test cases, (maximum) duration has been set to
    3600 sec (as the default traffic profile tests 7 packet sizes and each
    packet size test can take up to around 5 minutes).
    ACL traffic profile has been modified to binsearch, as there was no
    reason to use a different traffic profile.
    
    Change-Id: Ib11f66334e5a54e514dc6b01f806c3d74e8ce3d7
    Signed-off-by: Xavier Simonart <xavier.simonart@intel.com>

---
+++ samples/vnf_samples/nsut/prox/configs/tuples-2.lua
@@ -0,0 +1,28 @@
+-- Copyright (c) 2016-2018 Intel Corporation
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+--
+
+tuples = {};
+
+for i = 0,2^23-1 do
+    tuples[i] = {if_out = i%2,
+                ip_src = i%2^5,
+                ip_dst = ((i-i%2^5)/2^5)%2^5,
+                port_src = ((i-i%2^10)/2^10)%2^5,
+                port_dst = ((i-i%2^15)/2^15)%2^5,
+                proto = ((i-i%2^20)/2^20)%2^3 * 2^5,
+            }
+end
+
