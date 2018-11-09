10fadff8018c448632ac1ac5c205c2f123f1cad0
Patrice Buriez <patrice.buriez@intel.com>
2018-11-09 10:25:19 +0100

    Update unit tests for yardstick/benchmark/core/report.py
    * Remove redundant imports
    * Rename some constants
    * Add additional unit tests
    
    JIRA: YARDSTICK-1367
    Topic: report/html_table (1 of 9)
    
    Change-Id: I3d89c1ed9acaeb7421a0734e33075ebae252c035
    Signed-off-by: Emma Foley <emma.l.foley@intel.com>
    Signed-off-by: Patrice Buriez <patrice.buriez@intel.com>

--- yardstick/tests/unit/benchmark/core/test_report.py
+++ yardstick/tests/unit/benchmark/core/test_report.py
@@ -7,30 +7,24 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-# Unittest for yardstick.benchmark.core.report
-
-from __future__ import print_function
-
-from __future__ import absolute_import
-
+import mock
 import unittest
 import uuid
 
-try:
-    from unittest import mock
-except ImportError:
-    import mock
-
+from api.utils import influx
 from yardstick.benchmark.core import report
 from yardstick.cmd.commands import change_osloobj_to_paras
 
-FAKE_YAML_NAME = 'fake_name'
-FAKE_TASK_ID = str(uuid.uuid4())
-FAKE_DB_FIELDKEYS = [{'fieldKey': 'fake_key'}]
-FAKE_TIME = '0000-00-00T00:00:00.000000Z'
-FAKE_DB_TASK = [{'fake_key': 0.000, 'time': FAKE_TIME}]
+GOOD_YAML_NAME = 'fake_name'
+GOOD_TASK_ID = str(uuid.uuid4())
+GOOD_DB_FIELDKEYS = [{'fieldKey': 'fake_key'}]
+GOOD_DB_TASK = [{
+        'fake_key': 0.000,
+        'time': '0000-00-00T00:00:00.000000Z',
+        }]
+# TODO: Rename or remove
 FAKE_TIMESTAMP = ['fake_time']
-DUMMY_TASK_ID = 'aaaaaa-aaaaaaaa-aaaaaaaaaa-aaaaaa'
+BAD_TASK_ID = 'aaaaaa-aaaaaaaa-aaaaaaaaaa-aaaaaa'
 
 
 class ReportTestCase(unittest.TestCase):
@@ -38,37 +32,68 @@ class ReportTestCase(unittest.TestCase):
     def setUp(self):
         super(ReportTestCase, self).setUp()
         self.param = change_osloobj_to_paras({})
-        self.param.yaml_name = [FAKE_YAML_NAME]
-        self.param.task_id = [FAKE_TASK_ID]
+        self.param.yaml_name = [GOOD_YAML_NAME]
+        self.param.task_id = [GOOD_TASK_ID]
         self.rep = report.Report()
+
+    def test___init__(self):
+        self.assertEqual([], self.rep.Timestamp)
+        self.assertEqual("", self.rep.yaml_name)
+        self.assertEqual("", self.rep.task_id)
+
+    def test__validate(self):
+        self.rep._validate(GOOD_YAML_NAME, GOOD_TASK_ID)
+        self.assertEqual(GOOD_YAML_NAME, self.rep.yaml_name)
+        self.assertEqual(GOOD_TASK_ID, str(self.rep.task_id))
+
+    def test__validate_invalid_yaml_name(self):
+        with self.assertRaisesRegexp(ValueError, "yaml*"):
+            self.rep._validate('F@KE_NAME', GOOD_TASK_ID)
+
+    def test__validate_invalid_task_id(self):
+        with self.assertRaisesRegexp(ValueError, "task*"):
+            self.rep._validate(GOOD_YAML_NAME, BAD_TASK_ID)
+
+    @mock.patch.object(influx, 'query')
+    def test__get_field_keys(self, mock_query):
+        mock_query.return_value = GOOD_DB_FIELDKEYS
+        self.rep.yaml_name = GOOD_YAML_NAME
+        self.rep.task_id = GOOD_TASK_ID
+        self.assertEqual(GOOD_DB_FIELDKEYS, self.rep._get_fieldkeys())
+
+    @mock.patch.object(influx, 'query')
+    def test__get_fieldkeys_nodbclient(self, mock_query):
+        mock_query.side_effect = RuntimeError
+        self.assertRaises(RuntimeError, self.rep._get_fieldkeys)
+
+    @mock.patch.object(influx, 'query')
+    def test__get_fieldkeys_task_not_found(self, mock_query):
+        mock_query.return_value = []
+        self.rep.yaml_name = GOOD_YAML_NAME
+        self.rep.task_id = GOOD_TASK_ID
+        self.assertRaisesRegexp(KeyError, "Task ID", self.rep._get_fieldkeys)
+
+    @mock.patch.object(influx, 'query')
+    def test__get_tasks(self, mock_query):
+        mock_query.return_value = GOOD_DB_TASK
+        self.rep.yaml_name = GOOD_YAML_NAME
+        self.rep.task_id = GOOD_TASK_ID
+        self.assertEqual(GOOD_DB_TASK, self.rep._get_tasks())
+
+    @mock.patch.object(influx, 'query')
+    def test__get_tasks_task_not_found(self, mock_query):
+        mock_query.return_value = []
+        self.rep.yaml_name = GOOD_YAML_NAME
+        self.rep.task_id = GOOD_TASK_ID
+        self.assertRaisesRegexp(KeyError, "Task ID", self.rep._get_tasks)
 
     @mock.patch.object(report.Report, '_get_tasks')
     @mock.patch.object(report.Report, '_get_fieldkeys')
     @mock.patch.object(report.Report, '_validate')
     def test_generate_success(self, mock_valid, mock_keys, mock_tasks):
-        mock_tasks.return_value = FAKE_DB_TASK
-        mock_keys.return_value = FAKE_DB_FIELDKEYS
+        mock_tasks.return_value = GOOD_DB_TASK
+        mock_keys.return_value = GOOD_DB_FIELDKEYS
         self.rep.generate(self.param)
-        mock_valid.assert_called_once_with(FAKE_YAML_NAME, FAKE_TASK_ID)
+        mock_valid.assert_called_once_with(GOOD_YAML_NAME, GOOD_TASK_ID)
         mock_tasks.assert_called_once_with()
         mock_keys.assert_called_once_with()
-
-    # pylint: disable=deprecated-method
-    def test_invalid_yaml_name(self):
-        self.assertRaisesRegexp(ValueError, "yaml*", self.rep._validate,
-                                'F@KE_NAME', FAKE_TASK_ID)
-
-    # pylint: disable=deprecated-method
-    def test_invalid_task_id(self):
-        self.assertRaisesRegexp(ValueError, "task*", self.rep._validate,
-                                FAKE_YAML_NAME, DUMMY_TASK_ID)
-
-    @mock.patch('api.utils.influx.query')
-    def test_task_not_found(self, mock_query):
-        mock_query.return_value = []
-        self.rep.yaml_name = FAKE_YAML_NAME
-        self.rep.task_id = FAKE_TASK_ID
-        # pylint: disable=deprecated-method
-        self.assertRaisesRegexp(KeyError, "Task ID", self.rep._get_fieldkeys)
-        self.assertRaisesRegexp(KeyError, "Task ID", self.rep._get_tasks)
-        # pylint: enable=deprecated-method
