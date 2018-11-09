712ac57022f63bafe9e08ad986cdb31138be2c30
Patrice Buriez <patrice.buriez@intel.com>
2018-11-09 11:48:15 +0100

    Add support to show only selected node's data in table.
    Add timestamps in the table and
    optimize the showcasing of graphs and table.
    
    JIRA: YARDSTICK-1367
    Topic: report/html_table (9 of 11)
    
    Change-Id: I263960b059fd27409b36f825664da8c2804e0425
    Signed-off-by: Shobhi Jain <shobhi.jain@intel.com>
    Signed-off-by: Patrice Buriez <patrice.buriez@intel.com>

--- yardstick/common/report.js
+++ yardstick/common/report.js
@@ -16,19 +16,28 @@ function create_jstree(jstree_data){
    });
 }  // end create_jstree
 
-// may need to pass timestamps too...
-function create_table(table_data){
-    var tab, th, tr, td, tn, row, col, thead, tbody;
+function create_table(table_data, timestamps){
+    var tab, tr, td, tn, row, col, tbody;
     // create table
     tab = document.getElementsByTagName('table')[0];
-    thead=document.createElement('thead');
     tr = document.createElement('tr');
     tbody=document.createElement('tbody');
-
     //create table headings using timestamps
+    td = document.createElement('td');
+    tn = document.createTextNode('Timestamps');
+    td.appendChild(tn);
+    tr.appendChild(td);
+    for(row=0; row<timestamps.length; row++)
+    {
+        td = document.createElement('td');
+        tn = document.createTextNode(timestamps[row]);
+        td.appendChild(tn);
+        tr.appendChild(td);
+    }
+    tbody.appendChild(tr);
     keys = Object.keys(table_data)
     // for each metric
-    for (i=0; i< keys.length; i++){
+    for (var i=0; i< keys.length; i++){
         tr = document.createElement('tr');
         key = keys[i]
         td = document.createElement('td');
@@ -36,7 +45,7 @@ function create_table(table_data){
         tr.append(td)
         curr_data = table_data[key]
         // add each piece of data as its own column
-        for (j=0; j<curr_data.length; j++){
+        for (var j=0; j<curr_data.length; j++){
             td = document.createElement('td');
             td.append(curr_data[j])
             tr.append(td)
@@ -45,6 +54,14 @@ function create_table(table_data){
     }
     tab.appendChild(tbody);
 } // end create_table
+
+//delete rows of the table
+function deleteRows(){
+    var table = document.getElementsByTagName('table')[0];
+    for (var i=((table.rows.length)-1); i>=0; i--){
+        table.deleteRow(i);
+    }
+}
 
 function render_highcharts(plot_data, timestamps){
     $('#graph').highcharts({
