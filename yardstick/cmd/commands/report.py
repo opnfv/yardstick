6e2755c76135966231c03143ac29f0f299ade6d4
Patrice Buriez <patrice.buriez@intel.com>
2018-11-09 11:24:21 +0100

    Add report generate-nsb command
    JIRA: YARDSTICK-1367
    Topic: report/html_table (6 of 11)
    
    Change-Id: I8a2f96224a745334b67dd71875bebb3b69b9adc3
    Signed-off-by: Emma Foley <emma.l.foley@intel.com>
    Signed-off-by: Patrice Buriez <patrice.buriez@intel.com>

--- yardstick/cmd/commands/report.py
+++ yardstick/cmd/commands/report.py
@@ -7,14 +7,9 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
-
 """ Handler for yardstick command 'report' """
 
-from __future__ import print_function
-
-from __future__ import absolute_import
-
-from yardstick.benchmark.core.report import Report
+from yardstick.benchmark.core import report
 from yardstick.cmd.commands import change_osloobj_to_paras
 from yardstick.common.utils import cliargs
 
@@ -22,12 +17,19 @@ from yardstick.common.utils import cliargs
 class ReportCommands(object):   # pragma: no cover
     """Report commands.
 
-    Set of commands to manage benchmark tasks.
+    Set of commands to manage reports.
     """
 
     @cliargs("task_id", type=str, help=" task id", nargs=1)
     @cliargs("yaml_name", type=str, help=" Yaml file Name", nargs=1)
     def do_generate(self, args):
-        """Start a benchmark scenario."""
+        """Generate a report."""
         param = change_osloobj_to_paras(args)
-        Report().generate(param)
+        report.Report().generate(param)
+
+    @cliargs("task_id", type=str, help=" task id", nargs=1)
+    @cliargs("yaml_name", type=str, help=" Yaml file Name", nargs=1)
+    def do_generate_nsb(self, args):
+        """Generate a report using the NSB template."""
+        param = change_osloobj_to_paras(args)
+        report.Report().generate_nsb(param)
