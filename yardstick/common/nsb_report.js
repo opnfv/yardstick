67d3d8952d9ab6c90b47283aa33c1588ef49d3c1
Patrice Buriez <patrice.buriez@intel.com>
2018-11-09 11:29:36 +0100

    Add JS file to NSB report template
    Moved function definitions into separate nsb_report.js file.
    
    JIRA: YARDSTICK-1367
    Topic: report/html_table (7 of 11)
    
    Change-Id: I3b3cf8aae8425b486a711a76f4bb2bf8d7fe5ab5
    Signed-off-by: Emma Foley <emma.l.foley@intel.com>
    Signed-off-by: Patrice Buriez <patrice.buriez@intel.com>

---
+++ yardstick/common/nsb_report.js
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Rajesh Kudaka <4k.rajesh@gmail.com>
+ * Copyright (c) 2018 Intel Corporation.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, Version 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ ******************************************************************************/
+
+function create_tree(jstree_data)
+{
+    $('#data_selector').jstree({
+        plugins: ['checkbox'],
+        checkbox: {
+            three_state: false,
+            whole_node: true,
+            tie_selection: false,
+        },
+        core: {
+            themes: {
+                icons: false,
+                stripes: true,
+            },
+            data: jstree_data,
+        },
+    });
+}
+
+// may need to pass timestamps too...
+function create_table(table_data)
+{
+    var tab, tr, td, tn, tbody, keys, key, curr_data;
+    // create table
+    tab = document.getElementsByTagName('table')[0];
+    tbody = document.createElement('tbody');
+    // for each metric
+    keys = Object.keys(table_data);
+    for (var i = 0; i < keys.length; i++) {
+        key = keys[i];
+        tr = document.createElement('tr');
+        td = document.createElement('td');
+        tn = document.createTextNode(key);
+        td.appendChild(tn);
+        tr.appendChild(td);
+        // add each piece of data as its own column
+        curr_data = table_data[key];
+        for (var j = 0; j < curr_data.length; j++) {
+            td = document.createElement('td');
+            tn = document.createTextNode(curr_data[j]);
+            td.appendChild(tn);
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+    tab.appendChild(tbody);
+}
+
+function render_graph(plot_data, timestamps)
+{
+    $('#graph').highcharts({
+        title: {
+            text: 'Yardstick Graphs',
+            x: -20,
+        },
+        chart: {
+            marginLeft: 400,
+            zoomType: 'x',
+            type: 'spline',
+        },
+        xAxis: {
+            crosshair: {
+                width: 1,
+                color: 'black',
+            },
+            title: {
+                text: 'Timestamp',
+            },
+            categories: timestamps,
+        },
+        yAxis: {
+            crosshair: {
+                width: 1,
+                color: 'black',
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080',
+            }],
+        },
+        plotOptions: {
+            series: {
+                showCheckbox: false,
+                visible: true,
+            },
+        },
+        tooltip: {
+            valueSuffix: '',
+        },
+        legend: {
+            enabled: true,
+        },
+        series: plot_data,
+    });
+}
