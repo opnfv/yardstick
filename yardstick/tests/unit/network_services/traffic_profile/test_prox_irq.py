912e7202fe0862677dfc8e14f6a56acb17bb31cc
DanielMartinBuckley <daniel.m.buckley@intel.com>
2018-11-14 17:09:51 +0000

    Addition of IRQ Mode to NSB NFVI (PROX)
    JIRA: YARDSTICK-1524
    
    Add dashboard, Traffic Profile support, PROX IRQ support, topology
    
    Change-Id: I0c0cde66f4f4d3d25aa77c502bd4349cc1b3eca2
    Signed-off-by: Daniel Martin Buckley <daniel.m.buckley@intel.com>

---
+++ yardstick/tests/unit/network_services/traffic_profile/test_prox_irq.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2017 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+import mock
+
+from yardstick.network_services.traffic_profile import prox_irq
+
+
+class TestProxIrqProfile(unittest.TestCase):
+
+    def setUp(self):
+        self._mock_log_info = mock.patch.object(prox_irq.LOG, 'info')
+        self.mock_log_info = self._mock_log_info.start()
+        self.addCleanup(self._stop_mocks)
+
+    def _stop_mocks(self):
+        self._mock_log_info.stop()
+
+    def test_execute_1(self):
+        tp_config = {
+            'traffic_profile': {
+            },
+        }
+
+        traffic_generator = mock.MagicMock()
+        attrs1 = {'get.return_value' : 10}
+        traffic_generator.scenario_helper.all_options.configure_mock(**attrs1)
+
+        attrs2 = {'__getitem__.return_value' : 10, 'get.return_value': 10}
+        traffic_generator.scenario_helper.scenario_cfg["runner"].configure_mock(**attrs2)
+
+        profile_helper = mock.MagicMock()
+
+        profile = prox_irq.ProxIrqProfile(tp_config)
+        profile.init(mock.MagicMock())
+        profile._profile_helper = profile_helper
+
+        profile.execute_traffic(traffic_generator)
+        profile.run_test()
+        is_ended_flag = profile.is_ended()
+
+        self.assertFalse(is_ended_flag)
+        self.assertEqual(profile.lower_bound, 10.0)
