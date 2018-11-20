f3fc3571f491c0e1c583a1856c2e246c30e69748
treyad <treyad@viosoft.com>
2018-11-20 01:54:08 -0800

    Support FD.io Multiple Loss Ratio search (MLRsearch)
    Optimized binary search algorithm for finding NDR and PDR bounds
    
    JIRA: YARDSTICK-1486
    
    Change-Id: Iba0e6c7065b61f7b7f3f65cceca509d1aaff8366
    Signed-off-by: treyad <treyad@viosoft.com>

---
+++ yardstick/network_services/helpers/vpp_helpers/ndr_pdr_result.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2019 Viosoft Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This is a modified copy of
+# https://gerrit.fd.io/r/gitweb?p=csit.git;a=blob_plain;f=resources/libraries/python/MLRsearch/NdrPdrResult.py;hb=HEAD
+
+from yardstick.network_services.helpers.vpp_helpers.receive_rate_interval import \
+    ReceiveRateInterval
+
+
+class NdrPdrResult(object):
+    """Two measurement intervals, return value of search algorithms.
+
+    Partial fraction is NOT part of the result. Pdr interval should be valid
+    for all partial fractions implied by the interval."""
+
+    def __init__(self, ndr_interval, pdr_interval):
+        """Store the measured intervals after checking argument types.
+
+        :param ndr_interval: Object containing data for NDR part of the result.
+        :param pdr_interval: Object containing data for PDR part of the result.
+        :type ndr_interval: ReceiveRateInterval.ReceiveRateInterval
+        :type pdr_interval: ReceiveRateInterval.ReceiveRateInterval
+        """
+        # TODO: Type checking is not very pythonic,
+        # perhaps users can fix wrong usage without it?
+        if not isinstance(ndr_interval, ReceiveRateInterval):
+            raise TypeError("ndr_interval, is not a ReceiveRateInterval: "
+                            "{ndr!r}".format(ndr=ndr_interval))
+        if not isinstance(pdr_interval, ReceiveRateInterval):
+            raise TypeError("pdr_interval, is not a ReceiveRateInterval: "
+                            "{pdr!r}".format(pdr=pdr_interval))
+        self.ndr_interval = ndr_interval
+        self.pdr_interval = pdr_interval
+
+    def width_in_goals(self, relative_width_goal):
+        """Return a debug string related to current widths in logarithmic scale.
+
+        :param relative_width_goal: Upper bound times this is the goal
+            difference between upper bound and lower bound.
+        :type relative_width_goal: float
+        :returns: Message containing NDR and PDR widths in goals.
+        :rtype: str
+        """
+        return "ndr {ndr_in_goals}; pdr {pdr_in_goals}".format(
+            ndr_in_goals=self.ndr_interval.width_in_goals(relative_width_goal),
+            pdr_in_goals=self.pdr_interval.width_in_goals(relative_width_goal))
+
+    def __str__(self):
+        """Return string as tuple of named values."""
+        return "NDR={ndr!s};PDR={pdr!s}".format(
+            ndr=self.ndr_interval, pdr=self.pdr_interval)
+
+    def __repr__(self):
+        """Return string evaluable as a constructor call."""
+        return "NdrPdrResult(ndr_interval={ndr!r},pdr_interval={pdr!r})".format(
+            ndr=self.ndr_interval, pdr=self.pdr_interval)
---
+++ yardstick/tests/unit/network_services/helpers/vpp_helpers/test_ndr_pdr_result.py
@@ -0,0 +1,91 @@
+# Copyright (c) 2019 Viosoft Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import mock
+
+from yardstick.network_services.helpers.vpp_helpers.ndr_pdr_result import \
+    NdrPdrResult
+from yardstick.network_services.helpers.vpp_helpers.receive_rate_interval import \
+    ReceiveRateInterval
+from yardstick.network_services.helpers.vpp_helpers.receive_rate_measurement import \
+    ReceiveRateMeasurement
+
+
+class TestNdrPdrResult(unittest.TestCase):
+
+    def test___init__(self):
+        measured_low = ReceiveRateMeasurement(1, 4857361, 4857339, 84965)
+        measured_high = ReceiveRateMeasurement(1, 4977343, 4977320, 119959)
+        starting_interval = ReceiveRateInterval(measured_low, measured_high)
+        ndrpdr_result = NdrPdrResult(starting_interval, starting_interval)
+        self.assertIsInstance(ndrpdr_result.ndr_interval, ReceiveRateInterval)
+        self.assertIsInstance(ndrpdr_result.pdr_interval, ReceiveRateInterval)
+
+    def test___init__ndr_error(self):
+        starting_interval = mock.MagicMock()
+        measured_low = ReceiveRateMeasurement(1, 4857361, 4857339, 84965)
+        measured_high = ReceiveRateMeasurement(1, 4977343, 4977320, 119959)
+        end_interval = ReceiveRateInterval(measured_low, measured_high)
+        with self.assertRaises(TypeError) as raised:
+            NdrPdrResult(starting_interval, end_interval)
+        self.assertIn('ndr_interval, is not a ReceiveRateInterval: ',
+                      str(raised.exception))
+
+    def test___init__pdr_error(self):
+        measured_low = ReceiveRateMeasurement(1, 4857361, 4857339, 84965)
+        measured_high = ReceiveRateMeasurement(1, 4977343, 4977320, 119959)
+        starting_interval = ReceiveRateInterval(measured_low, measured_high)
+        end_interval = mock.MagicMock()
+        with self.assertRaises(TypeError) as raised:
+            NdrPdrResult(starting_interval, end_interval)
+        self.assertIn('pdr_interval, is not a ReceiveRateInterval: ',
+                      str(raised.exception))
+
+    def test_width_in_goals(self):
+        measured_low = ReceiveRateMeasurement(1, 4857361, 4857339, 84965)
+        measured_high = ReceiveRateMeasurement(1, 4977343, 4977320, 119959)
+        starting_interval = ReceiveRateInterval(measured_low, measured_high)
+        ndrpdr_result = NdrPdrResult(starting_interval, starting_interval)
+        self.assertEqual('ndr 4.86887; pdr 4.86887',
+                         ndrpdr_result.width_in_goals(0.005))
+
+    def test___str__(self):
+        measured_low = ReceiveRateMeasurement(1, 4857361, 4857339, 84965)
+        measured_high = ReceiveRateMeasurement(1, 4977343, 4977320, 119959)
+        starting_interval = ReceiveRateInterval(measured_low, measured_high)
+        ndrpdr_result = NdrPdrResult(starting_interval, starting_interval)
+        self.assertEqual(
+            'NDR=[d=1.0,Tr=4857361.0,Df=0.01749;d=1.0,Tr=4977343.0,Df=0.0241);'
+            'PDR=[d=1.0,Tr=4857361.0,Df=0.01749;d=1.0,Tr=4977343.0,Df=0.0241)',
+            ndrpdr_result.__str__())
+
+    def test___repr__(self):
+        measured_low = ReceiveRateMeasurement(1, 4857361, 4857339, 84965)
+        measured_high = ReceiveRateMeasurement(1, 4977343, 4977320, 119959)
+        starting_interval = ReceiveRateInterval(measured_low, measured_high)
+        ndrpdr_result = NdrPdrResult(starting_interval, starting_interval)
+        self.assertEqual(
+            'NdrPdrResult(ndr_interval=ReceiveRateInterval(measured_low=' \
+            'ReceiveRateMeasurement(duration=1.0,target_tr=4857361.0,' \
+            'transmit_count=4857339,loss_count=84965),measured_high=' \
+            'ReceiveRateMeasurement(duration=1.0,target_tr=4977343.0,' \
+            'transmit_count=4977320,loss_count=119959)),pdr_interval=' \
+            'ReceiveRateInterval(measured_low=ReceiveRateMeasurement' \
+            '(duration=1.0,target_tr=4857361.0,transmit_count=4857339,' \
+            'loss_count=84965),measured_high=ReceiveRateMeasurement' \
+            '(duration=1.0,target_tr=4977343.0,transmit_count=4977320,' \
+            'loss_count=119959)))',
+            ndrpdr_result.__repr__())
