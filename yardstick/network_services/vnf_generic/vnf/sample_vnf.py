9dfc98d985939e8cdb33c44662dd52e8a06422f9
Orest Voznyy <orestx.voznyy@intel.com>
2019-01-09 10:30:44 +0200

    Unify TG and VNF names in infrastructure files
    Update naming in related files, i.e. pod, docs, docstrings.
    Rename TGs and VNFs to follow a single naming convention, for all VNFs:
      name: trafficgen_0
      role: tg__0
    
      name: vnf_0
      role: vnf__0
    
    JIRA: YARDSTICK-1578
    
    Change-Id: I7596b915939f242349a99721b6088ae738dd277f
    Signed-off-by: Orest Voznyy <orestx.voznyy@intel.com>

--- yardstick/network_services/vnf_generic/vnf/sample_vnf.py
+++ yardstick/network_services/vnf_generic/vnf/sample_vnf.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016-2018 Intel Corporation
+# Copyright (c) 2016-2019 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -691,8 +691,8 @@ class SampleVNF(GenericVNF):
         scenarios:
         - type: NSPerf
           nodes:
-            tg__0: trafficgen_1.yardstick
-            vnf__0: vnf.yardstick
+            tg__0: trafficgen_0.yardstick
+            vnf__0: vnf_0.yardstick
           options:
             collectd:
               <options>  # COLLECTD priority 3
